3cea51c06ed601223d31b5ee7d1235f3a1c67e75
MOON JINJU <mjj920930@gmail.com>
2019-12-11 21:58:35 +0900

    feat: 플레이어의 베팅 금액을 입력 받는 기능

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람들의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String INPUT_BETTING_MONEY_MESSAGE = "의 배팅 금액은?";
+    private static final String NOT_A_NUMBER_MESSAGE = "숫자만 입력해주세요.";
 
     private InputView() {}
 
@@ -12,4 +14,15 @@ public class InputView {
         System.out.println(INPUT_PLAYER_NAMES_MESSAGE);
         return scanner.nextLine();
     }
+
+    public static double inputBettingMoney(String playerName) {
+        try {
+            System.out.println(playerName + INPUT_BETTING_MONEY_MESSAGE);
+            return scanner.nextDouble();
+        } catch (NumberFormatException e) {
+            System.out.println(NOT_A_NUMBER_MESSAGE);
+            return inputBettingMoney(playerName);
+        }
+    }
+
 }
