b1a7504450ce1d7ca69efea9cefdaef10871f6ae
kouz <lastpuzly@gmail.com>
2019-12-11 20:24:54 +0900

    feat: player 생성 기능 구현

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
+
+import domain.user.Player;
 import utils.InputHandler;
 import view.InputView;
 
 public class BlackJack {
+    private ArrayList<Player> players = new ArrayList<>();
+
     void play() {
+        setPlayers();
+    }
+
+    private void setPlayers() {
         for(String playerName : InputHandler.splitByComma(InputView.playerNames())) {
             int bettingMoney = InputView.bettingMoney(playerName);
+            Player player = new Player(playerName, bettingMoney);
+            players.add(player);
         }
     }
 }
