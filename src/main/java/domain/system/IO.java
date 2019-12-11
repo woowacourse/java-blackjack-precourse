30d85275b840549d9861c17f681846c00fdfbd27
BEOMJUN KIM <skyblue300a@gmail.com>
2019-12-11 18:45:30 +0900

    IO Class 코드 수정
    1. 코드를 최대한 간결하게 수정
    2. 필요없는 메소드 제거

--- src/main/java/domain/system/IO.java
+++ src/main/java/domain/system/IO.java
@@ -7,10 +7,6 @@ public class IO {
 
     private static final String INVALID_INPUT = null;
 
-    public static String getNextLine() {
-        return scan.nextLine();
-    }
-
     public static String getPlayersNames() {
         String playersNamePattern = "([a-zA-Z][a-zA-Z]{0,9}(,|$))+";
         String question = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
@@ -28,20 +24,14 @@ public class IO {
     private static String getInputUntilValid(String question, String validPattern) {
         String input = INVALID_INPUT;
 
-        while (input != INVALID_INPUT) {
-            input = getInput(question, validPattern);
+        while (!input.matches(validPattern)) {
+            input = getInput(question);
         }
         return input;
     }
 
-    private static String getInput(String question, String validPattern) {
-        String input;
-
+    private static String getInput(String question) {
         System.out.println(question);
-        input = scan.nextLine();
-        if (!input.matches(validPattern)) {
-            return INVALID_INPUT;
-        }
-        return input;
+        return scan.nextLine();
     }
 }
