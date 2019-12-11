fc452813bb836925c7fb8c97b58259c153afb9a8
kouz <lastpuzly@gmail.com>
2019-12-11 20:45:29 +0900

    feat: 카드뭉치 생성 기능 구현

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import domain.card.Card;
+import domain.card.CardFactory;
 import domain.user.Player;
 import utils.InputHandler;
 import view.InputView;
 
 public class BlackJack {
     private ArrayList<Player> players = new ArrayList<>();
+    private List<Card> deck = new ArrayList<>();
 
     void play() {
         setPlayers();
+        setFirstState();
     }
 
     private void setPlayers() {
@@ -18,4 +24,14 @@ public class BlackJack {
             players.add(player);
         }
     }
+
+    private void setFirstState() {
+        setDeck();
+    }
+
+    private void setDeck() {
+        deck = CardFactory.create();
+        Collections.shuffle(deck);
+    }
+
 }
