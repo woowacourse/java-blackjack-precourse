f5927549038db6e808419148c5218cd0d9413a2c
KimSeongGyu1 <KimSeongGyu1@github.com>
2019-12-11 17:39:05 +0900

    온전한 카드뭉치 생성 및 출력으로 확인 완료

--- src/main/java/domain/card/CardDeck.java
+++ src/main/java/domain/card/CardDeck.java
@@ -1,12 +1,19 @@
 package domain.card;
 
 import java.util.List;
-import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class CardDeck {
 	private List<Card> cards;
 	
 	public CardDeck() {
-		cards = new ArrayList<Card>();
+		cards = CardFactory.create();
+		System.out.println(this.toString());
+	}
+	
+	public String toString() {
+		return cards.stream()
+				.map(Card::toString)
+				.collect(Collectors.joining("\n"));
 	}
 }
