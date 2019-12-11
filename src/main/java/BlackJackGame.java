e530679a445f8905ae1bf81c96b5eee2a89a96c3
Andy H. Jung <commaniakr@gmail.com>
2019-12-11 22:52:41 +0900

    feat: 모두 n개의 카드 뽑기

--- src/main/java/BlackJackGame.java
+++ src/main/java/BlackJackGame.java
@@ -6,6 +6,7 @@ import domain.user.Player;
 import java.util.*;
 
 public class BlackJackGame {
+    private final int START_DRAW = 2;
     private final List<Player> players;
     private final Dealer dealer;
 
@@ -21,12 +22,28 @@ public class BlackJackGame {
         List<Card> newCards = new ArrayList<>();
         newCards.addAll(CardFactory.create());
         Collections.shuffle(newCards);
+        drawCards(newCards, START_DRAW);
+    }
+
+    private void drawCards(List<Card> newCards, int iteration) {
+        for(int i = 0; i < iteration; i++) {
+            drawOneCard(newCards);
+        }
+    }
+
+    private void drawOneCard(List<Card> newCards) {
         System.out.println(newCards.size());
-        //players.stream().forEach(x -> x.addCard(cards.get(random.nextInt(cards.size()))));
+        this.addCard(newCards, dealer);
         players.stream().forEach(x ->
-                x.addCard(newCards.remove(newCards.size()-1)));
+                this.addCard(newCards,x));
         System.out.println(newCards.size());
+    }
 
+    private void addCard(List<Card> newCards, Player player) {
+        player.addCard(newCards.remove(newCards.size()-1));
+    }
 
+    private void addCard(List<Card> newCards, Dealer dealer) {
+        dealer.addCard(newCards.remove(newCards.size()-1));
     }
 }
