73ca8f9e4fb4be0ab0c2c6965c58984ec442ec22
BEOMJUN KIM <skyblue300a@gmail.com>
2019-12-12 02:20:51 +0900

    메인 함수 업데이트

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,21 @@
+package domain;
+
+import domain.card.CardDeck;
+import domain.card.CardFactory;
+import domain.user.Dealer;
+import domain.user.Player;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Dealer dealer = new Dealer();
+        CardDeck cardDeck = new CardDeck(CardFactory.create());
+
+        List<Player> players = dealer.makePlayers();
+        if (!dealer.giveCardsFirst(cardDeck, players)) {    // 첫 패에 블랙잭이 있다면 종료
+            dealer.haveMoreCards(players, cardDeck);
+            dealer.calculateFinalEarn(players);
+        }
+    }
+}
