978fe0dd15cf9cf1175c05638acc2e6b2073ee70
BEOMJUN KIM <skyblue300a@gmail.com>
2019-12-11 18:10:12 +0900

    이름 입력 기능 구현

---
+++ src/main/java/domain/system/IO.java
@@ -0,0 +1,40 @@
+package domain.system;
+
+import java.util.Scanner;
+
+public class IO {
+    private static final Scanner scan = new Scanner(System.in);
+
+    private static final String INVALID_INPUT = null;
+
+    public static String getNextLine() {
+        return scan.nextLine();
+    }
+
+    public static String getPlayersNames() {
+        String playersNamePattern = "([a-zA-Z][a-zA-Z]{0,9}(,|$))+";
+        String question = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+
+        return getInputUntilValid(question, playersNamePattern);
+    }
+
+    private static String getInputUntilValid(String question, String validPattern) {
+        String input = INVALID_INPUT;
+
+        while (input != INVALID_INPUT) {
+            input = getInput(question, validPattern);
+        }
+        return input;
+    }
+
+    private static String getInput(String question, String validPattern) {
+        String input;
+
+        System.out.println(question);
+        input = scan.nextLine();
+        if (!input.matches(validPattern)) {
+            return INVALID_INPUT;
+        }
+        return input;
+    }
+}
