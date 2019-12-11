ffc83f7d866775283e80a58d297120256186f767
kouz <lastpuzly@gmail.com>
2019-12-11 20:58:13 +0900

    feat: 초기 게임 상태 세팅 구현, deck 자료구조 변경 (to Stack)
    deck의 사용 방식상 List 보단 Stack이 어울리는 자료구조라고 판단하여 Stack으로 변경합니다.

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -1,16 +1,21 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 import domain.card.Card;
 import domain.card.CardFactory;
+import domain.user.Dealer;
 import domain.user.Player;
 import utils.InputHandler;
 import view.InputView;
 
 public class BlackJack {
+    private static final int FIRST_PLAYER_CARD_COUNTS = 2;
+
     private ArrayList<Player> players = new ArrayList<>();
-    private List<Card> deck = new ArrayList<>();
+    private Stack<Card> deck = new Stack<>();
+    private Dealer dealer = new Dealer();
 
     void play() {
         setPlayers();
@@ -27,11 +32,28 @@ public class BlackJack {
 
     private void setFirstState() {
         setDeck();
+        distributeCards();
     }
 
     private void setDeck() {
-        deck = CardFactory.create();
-        Collections.shuffle(deck);
+        List<Card> cards = CardFactory.create();
+        Collections.shuffle(cards);
+        for(Card card : cards) {
+            deck.push(card);
+        }
+    }
+
+    private void distributeCards() {
+        dealer.addCard(deck.pop());
+        for(int i = 0; i < FIRST_PLAYER_CARD_COUNTS; i++) {
+            distributeCardsToPlayers();
+        }
+    }
+
+    private void distributeCardsToPlayers() {
+        for(Player player : players) {
+            player.addCard(deck.pop());
+        }
     }
 
 }
