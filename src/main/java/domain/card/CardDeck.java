52a35a04bd3a2dc4f7188b001720d3b11a653489
BEOMJUN KIM <skyblue300a@gmail.com>
2019-12-11 20:01:47 +0900

    카드 뽑기 기능 수정
    1. immutable한 객체를 수정하려고 했던 문제점 수정
    2. 카드 더미 총 숫자 수정

--- src/main/java/domain/card/CardDeck.java
+++ src/main/java/domain/card/CardDeck.java
@@ -1,28 +1,44 @@
 package domain.card;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 카드 덱을 관리하는 객체
  */
 public class CardDeck {
-    List<Card> cardDeck;
+    private static final int FIRST = 0;
+
+    private List<Card> cardDeck;
+
+    /**
+     * 카드 뽑는 순서를 정해주는 인스턴스 변수
+     */
+    private List<Integer> drawOrder;
 
     public CardDeck(List<Card> cards) {
         cardDeck = cards;
+        drawOrder = makeRandomDrawOrder();
     }
 
     /**
-     * 덱에서 랜덤한 카드를 한장 뽑는 메소드
-     *
-     * @return Card 객체
+     * @return 덱에서 뽑은 랜덤한 Card 객체
      */
     public Card drawCard() {
-        int randomIndex = new Random().nextInt(cardDeck.size());
+        Card card = cardDeck.get(drawOrder.get(FIRST));
 
-        Card card = cardDeck.get(randomIndex);
-        cardDeck.remove(randomIndex);
+        drawOrder.remove(FIRST);        // 뽑은 카드는 뽑는 순서에서 제거
         return card;
+    }
+
+    /**
+     * @return 0~51의 숫자를 랜덤으로 셔플한 List
+     */
+    private List<Integer> makeRandomDrawOrder() {
+        List<Integer> drawOrder = IntStream.range(0, cardDeck.size()).boxed().collect(Collectors.toList());
+
+        Collections.shuffle(drawOrder, new Random(System.nanoTime()));
+        return drawOrder;
     }
 }
