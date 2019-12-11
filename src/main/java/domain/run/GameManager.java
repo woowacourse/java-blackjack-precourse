abab5f9e9183f1b1207dedf42d7d68b990be024c
hayoung298 <dudgk298@gmail.com>
2019-12-11 23:35:13 +0900

    ,로 이름 분리하는 함수 추가
    splitName(), main함수 추가

--- src/main/java/domain/run/GameManager.java
+++ src/main/java/domain/run/GameManager.java
@@ -1,15 +1,27 @@
 package domain.run;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import domain.user.Player;
 
 public class GameManager {
 
 	final Scanner sc=new Scanner(System.in);
+	ArrayList<Player> players=new ArrayList<Player>();
 	
 	public void printInputName() {
 		System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
 		String names=sc.next();
 		splitName(names);
+	}
+
+	private void splitName(String names) {
+		for(String name : names.split(",")) {
+			System.out.println(name+"의 배팅금액은?");
+			players.add(new Player(name,sc.nextDouble()));
+		}
 	}
 
 
--- src/main/java/domain/run/Main.java
+++ src/main/java/domain/run/Main.java
@@ -2,4 +2,8 @@ package domain.run;
 
 public class Main {
 
+	public static void main(String args[]) {
+		GameManager game=new GameManager();
+		game.printInputName();
+	}
 }
