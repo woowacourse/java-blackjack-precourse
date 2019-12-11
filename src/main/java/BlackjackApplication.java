0899e443f9cefc362913db48ecf057d47632c189
MOON JINJU <mjj920930@gmail.com>
2019-12-11 22:40:59 +0900

    feat: 플레이어들의 이름을 쉼표를 기준으로 구분하고 플레이어들과 딜러의 객체를 생성하는 기능 추가

--- src/main/java/BlackjackApplication.java
+++ src/main/java/BlackjackApplication.java
@@ -1,7 +1,24 @@
 import View.InputView;
+import domain.user.GameMember;
+import domain.user.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlackjackApplication {
     public static void main(String[] args) {
         String playerNames = InputView.inputPlayerNames();
+        List<GameMember> gameMembers = createPlayersAndDealer(playerNames);
+
+    }
+
+    private static List<GameMember> createPlayersAndDealer(String playerNames) {
+        List<GameMember> gameMembers = new ArrayList<>();
+        String[] playerNamesSplit = playerNames.split(",");
+        for (String playerName : playerNamesSplit) {
+            Double bettingMoney = InputView.inputBettingMoney(playerName);
+            gameMembers.add(new Player(playerName, bettingMoney));
+        }
+        return gameMembers;
     }
 }
---
+++ src/main/java/domain/user/GameMember.java
@@ -0,0 +1,7 @@
+package domain.user;
+
+import domain.card.Card;
+
+public interface GameMember {
+    void addCard(Card card);
+}
