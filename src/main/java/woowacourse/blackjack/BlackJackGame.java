553798988a28fdec9e1bf6e02d390c6acb645ffe
kimjunseok <jnunseok@gmail.com>
2019-12-12 19:56:57 +0900

    [Update] java collection 사용

--- src/main/java/woowacourse/blackjack/BlackJackGame.java
+++ src/main/java/woowacourse/blackjack/BlackJackGame.java
@@ -1,16 +1,18 @@
 package woowacourse.blackjack;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class BlackJackGame {
     private Scanner sc = new Scanner(System.in);
 
     public void runGame() {
-        String [] playerNames = this.getPlayerNames();
+        List<String> playerNames = this.getPlayerNames();
     }
 
-    private String [] getPlayerNames() {
+    private List<String> getPlayerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return sc.nextLine().split(",");
+        return Arrays.asList(sc.nextLine().split(","));
     }
 }
