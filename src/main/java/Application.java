480d4e6440933e88da9fbd66b8b61d1385596781
toneyparky <qkrtmddhks95@gmail.com>
2019-12-12 14:56:05 +0900

    Complete: 프로그램 시작 기능 구현
    Add: Application.java

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+
+    public static void main(String[] args) {
+        playBlackJack();
+    }
+
+    public static void playBlackJack() {
+        // 여기서는 이제 controller에 있는 메서드들을 실행한다.
+    }
+}
