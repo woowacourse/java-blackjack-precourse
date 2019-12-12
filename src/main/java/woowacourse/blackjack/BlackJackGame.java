818b861f2df6c4a1ea002f2d254bcc44ed59ea07
kimjunseok <jnunseok@gmail.com>
2019-12-12 20:14:52 +0900

    [Add] 각 players betting 할 수 있는 함수 구현

--- src/main/java/woowacourse/blackjack/BlackJackGame.java
+++ src/main/java/woowacourse/blackjack/BlackJackGame.java
@@ -1,5 +1,6 @@
 package woowacourse.blackjack;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -9,10 +10,21 @@ public class BlackJackGame {
 
     public void runGame() {
         List<String> playerNames = this.getPlayerNames();
+        List<Double> bettingMoney = this.getBettingMoney(playerNames);
     }
 
     private List<String> getPlayerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         return Arrays.asList(sc.nextLine().split(","));
+    }
+
+    private List<Double> getBettingMoney(List<String> playerNames) {
+        List<Double> bettingMoney = new ArrayList<>();
+        for (String playerName : playerNames) {
+            System.out.println("" + playerName + "의 배팅 금액은?");
+            bettingMoney.add(sc.nextDouble());
+            System.out.println();
+        }
+        return bettingMoney;
     }
 }
