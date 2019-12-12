76e6257464cf7c773e3c4ddf0dcc1f5c0554784c
DB2 <10100aaaaa@gmail.com>
2019-12-12 19:21:08 +0900

    Dealer 클래스 는 Player 를 상속받도록 수정

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -8,14 +8,9 @@ import java.util.List;
 /**
  * 게임 딜러를 의미하는 객체
  */
-public class Dealer {
-    private final List<Card> cards = new ArrayList<>();
-
-    public Dealer() {}
-
-    public void addCard(Card card) {
-        cards.add(card);
+public class Dealer extends Player {
+    public Dealer() {
+        super("딜러", 0);
     }
-
     // TODO 추가 기능 구현
 }
