07b0f868ca77dce39e399969a89f715ca6e2ae08
sezikim <kdghj1002@gmail.com>
2019-12-13 00:19:50 +0900

    Add feat 사용자에게 배팅 금액을 입력받고, 플레이어 리스트를 만드는 기능

--- src/main/java/controller/InputController.java
+++ src/main/java/controller/InputController.java
@@ -2,15 +2,33 @@ package controller;
 
 import static view.InputView.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+
+import domain.user.Player;
 
 public class InputController {
 	private Scanner scanner;
 
 	public InputController() {
 		this.scanner = new Scanner(System.in);
+	}
+
+	public List<Player> getPlayers(List<String> playerNames) {
+		List<Player> players = new ArrayList<>();
+
+		for (String playerName : playerNames) {
+			players.add(new Player(playerName, getPlayerBettingMoney(playerName)));
+		}
+
+		return players;
+	}
+
+	private int getPlayerBettingMoney(String playerName) {
+		printAskPlayerBettingAmount(playerName);
+		return Integer.parseInt(getUserInput());
 	}
 
 	private List<String> getPlayerNames() {
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,8 +2,13 @@ package view;
 
 public class InputView {
 	private static final String ASK_PLAYER_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+	private static final String ASK_PLAYER_BETTING = "의 배팅 금액은?";
 
 	public static void printAskPlayerName() {
 		System.out.println(ASK_PLAYER_NAME);
+	}
+
+	public static void printAskPlayerBettingAmount(String playerName) {
+		System.out.println(playerName + ASK_PLAYER_BETTING);
 	}
 }
