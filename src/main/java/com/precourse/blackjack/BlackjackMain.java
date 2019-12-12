ea37e4b1201f74930cad2fe22acf2979a9dc8ac2
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-12-12 18:12:55 +0900

    feat: 게임 컨트롤러, 블랙잭 메인 클래스 추가

---
+++ src/main/java/com/precourse/blackjack/BlackjackMain.java
@@ -0,0 +1,24 @@
+/*
+ * BlackjackMain.java                        1.0.0   2019-12-12
+ *
+ * Copyright (c) 2019 Hyungju An.
+ * All rights reserved.
+ * Contact me for more information. a301dks@naver.com
+ */
+
+package com.precourse.blackjack;
+
+import java.io.IOException;
+
+/**
+ * 블랙잭 게임을 진행하는 메인 클래스입니다.
+ *
+ * @version 1.0.0
+ * @date 2019-12-12
+ * @author HyungjuAn
+ */
+public class BlackjackMain {
+	public static void main(String[] args) throws IOException {
+
+	}
+}
---
+++ src/main/java/com/precourse/blackjack/controller/GameController.java
@@ -0,0 +1,19 @@
+/*
+ * GameController.java                        1.0.0   2019-12-12
+ *
+ * Copyright (c) 2019 Hyungju An.
+ * All rights reserved.
+ * Contact me for more information. a301dks@naver.com
+ */
+
+package com.precourse.blackjack.controller;
+
+/**
+ * 게임 컨트롤러 클래스입니다.
+ *
+ * @version 1.0.0
+ * @date 2019-12-12
+ * @author HyungjuAn
+ */
+public class GameController {
+}
