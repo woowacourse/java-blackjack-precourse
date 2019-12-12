5f55a0b4f36b1ea456102e55133058beed5554e2
JunSeong <ebseud6135@naver.com>
2019-12-12 21:12:10 +0900

    무작위로 변하지않는 카드 분배 순서를 생성하는 기능 추가

---
+++ src/main/java/domain/distribution/RandomNumbersGenerator.java
@@ -0,0 +1,21 @@
+package domain.distribution;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RandomNumbersGenerator {
+
+    public static List<Integer> create(int minValue, int maxValue) {
+        List<Integer> cardOrders = createNumbersByRange(minValue, maxValue);
+        Collections.shuffle(cardOrders);
+        return Collections.unmodifiableList(cardOrders);
+    }
+
+    private static List<Integer> createNumbersByRange(int minValue, int maxValue) {
+        return IntStream.range(minValue, maxValue)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
