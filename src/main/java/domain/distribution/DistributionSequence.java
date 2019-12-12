28bf8fa870781b44b56cd64c12edbb428fefd57e
JunSeong <ebseud6135@naver.com>
2019-12-12 21:24:48 +0900

    셔플된 카드 분배 순서를 가지고 있는 클래스 생성 및 현재 순서를 알려주는 기능 추가

---
+++ src/main/java/domain/distribution/DistributionSequence.java
@@ -0,0 +1,26 @@
+package domain.distribution;
+
+import java.util.List;
+import java.util.Random;
+
+public class DistributionSequence {
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 52;
+    private static final int DECKS_NUMBER = 6;
+
+    private List<Integer> sequences;
+    private int index = 0;
+
+    public DistributionSequence() {
+        sequences = RandomNumbersGenerator.create(MIN_VALUE, calculateMaxValue());
+    }
+    
+    public int get() {
+        return sequences.get(index++ % MAX_VALUE);
+    }
+
+    private int calculateMaxValue() {
+        /* 6개의 덱 사용 - 카운팅 방지를 위한 커팅 */
+        return (MAX_VALUE * DECKS_NUMBER) - new Random().nextInt(MAX_VALUE);
+    }
+}
