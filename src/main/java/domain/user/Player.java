908b811bf0c3a543febc2261b93e352d76e49c39
gunhee lee <dlrjsgml42@naver.com>
2019-12-12 09:33:24 +0900

    [Player] make player Class extends Dealer Class.

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -1,25 +1,26 @@
 package domain.user;
 
 import domain.card.Card;
+import jdk.nashorn.internal.objects.annotations.Constructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 게임 참여자를 의미하는 객체
+ * Player는 게임 참여자를 의미하는 객체이다.
+ * Dealer 객체를 상속받아 addCard 메소드와 cardList를 가지고 있으며,
+ * Player는 별도로 이름, 배팅금액, 배팅 관련 메소드를 가진다.
+ *
+ * @author kafka
+ * @version 1.1
  */
-public class Player {
+public class Player extends Dealer{
     private final String name;
     private final double bettingMoney;
-    private final List<Card> cards = new ArrayList<>();
 
     public Player(String name, double bettingMoney) {
         this.name = name;
         this.bettingMoney = bettingMoney;
-    }
-
-    public void addCard(Card card) {
-        cards.add(card);
     }
 
     // TODO 추가 기능 구현
