2c5f800203e585d866e54d3d3c7822e39fe334d9
DB <rkdrbgks22@ks.ac.kr>
2019-12-12 12:44:08 +0900

    [UPDATE] Dealer 클래스를 Player 클래스에 상속시킴

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -8,13 +8,9 @@ import java.util.List;
 /**
  * 게임 딜러를 의미하는 객체
  */
-public class Dealer {
-    private final List<Card> cards = new ArrayList<>();
-
-    public Dealer() {}
-
-    public void addCard(Card card) {
-        cards.add(card);
+public class Dealer extends Player {
+    public Dealer(String name, double bettingMoney) {
+        super(name, bettingMoney);
     }
 
     // TODO 추가 기능 구현
