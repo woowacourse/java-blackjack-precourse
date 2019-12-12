50e08562ccbe22ea618c981f8a3b2a331f9cdb71
kimjunseok <jnunseok@gmail.com>
2019-12-12 19:49:53 +0900

    [ADD] player name 입력 받는 함수 구현

--- src/main/java/woowacourse/blackjack/BlackJackGame.java
+++ src/main/java/woowacourse/blackjack/BlackJackGame.java
@@ -1,7 +1,16 @@
 package woowacourse.blackjack;
 
+import java.util.Scanner;
+
 public class BlackJackGame {
+    private Scanner sc = new Scanner(System.in);
+
     public void runGame() {
-        
+        String [] playerNames = this.getPlayerNames();
+    }
+
+    private String [] getPlayerNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        return sc.nextLine().split(",");
     }
 }
