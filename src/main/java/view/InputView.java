336ac05506c89a6a697a1ee6bbd736fe7686e18d
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-12-12 17:18:01 +0900

    feat: 입출력을 담당하는 패키지와 클래스 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,23 @@
+/*
+ * InputView.java                        1.0.0   2019-12-12
+ *
+ * Copyright (c) 2019 Hyungju An.
+ * All rights reserved.
+ * Contact me for more information. a301dks@naver.com
+ */
+
+package view;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 입력을 담당하는 클래스입니다.
+ *
+ * @version 1.0.0
+ * @date 2019-12-12
+ * @author HyungjuAn
+ */
+public class InputView {
+	private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+}
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,19 @@
+/*
+ * OutputView.java                        1.0.0   2019-12-12
+ *
+ * Copyright (c) 2019 Hyungju An.
+ * All rights reserved.
+ * Contact me for more information. a301dks@naver.com
+ */
+
+package view;
+
+/**
+ * 출력을 담당하는 클래스입니다.
+ *
+ * @version 1.0.0
+ * @date 2019-12-12
+ * @author HyungjuAn
+ */
+public class OutputView {
+}
