74b4c607bb6b06fc31a03d79e7f544f0ffa3b409
icyMojito <dev.icymojito@gmail.com>
2019-12-13 22:09:23 +0900

    딜러와 플레이어에게 게임 카드를 2장씩 주는 기능

--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -1,16 +1,35 @@
 package domain;
 
+import domain.card.CardDistributor;
+import domain.user.Dealer;
 import domain.user.Player;
 import domain.user.PlayerInputData;
 
 import java.util.List;
 
 public class BlackJackGame {
+    private static final int FIRST_DISTRIBUTED_CARD_AMOUNT = 2;
     private static List<Player> playerList;
+    private static Dealer dealer;
+    private static CardDistributor cardDistributor;
 
     public static void main(String[] args) {
+        setGameParticipant();
+        distributeCardsToGameParticipant();
+    }
+
+    private static void setGameParticipant() {
         PlayerInputData inputData = new PlayerInputData();
         playerList = inputData.getPlayerList();
+        dealer = new Dealer();
+    }
+
+    private static void distributeCardsToGameParticipant() {
+        cardDistributor = new CardDistributor();
+        cardDistributor.giveCardToDealer(FIRST_DISTRIBUTED_CARD_AMOUNT, dealer);
+        for (Player player : playerList) {
+            cardDistributor.giveCardToPlayer(FIRST_DISTRIBUTED_CARD_AMOUNT, player);
+        }
     }
 
 }
---
+++ src/main/java/domain/card/CardDistributor.java
@@ -0,0 +1,35 @@
+package domain.card;
+
+import domain.user.Dealer;
+import domain.user.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CardDistributor {
+    private static ArrayList<Card> gameCards;
+
+    public CardDistributor() {
+        setGameCards();
+    }
+
+    private static void setGameCards() {
+        gameCards = new ArrayList<>(CardFactory.create());
+        Collections.shuffle(gameCards);
+    }
+
+    public void giveCardToDealer(int count, Dealer dealer) {
+        while (count-- > 0) {
+            dealer.addCard(gameCards.get(0));
+            gameCards.remove(0);
+        }
+    }
+
+    public void giveCardToPlayer(int count, Player player) {
+        while (count-- > 0) {
+            player.addCard(gameCards.get(0));
+            gameCards.remove(0);
+        }
+    }
+
+}
