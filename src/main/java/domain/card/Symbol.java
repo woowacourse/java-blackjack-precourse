6940feae2bfcfd35184a867cee39d967fa0ca8c0
gunhee lee <dlrjsgml42@naver.com>
2019-12-14 00:11:11 +0900

    [ETC] refactoring Enum(Symbol, Type) and Message Class.

--- src/main/java/domain/card/Symbol.java
+++ src/main/java/domain/card/Symbol.java
@@ -1,5 +1,9 @@
 package domain.card;
 
+/**
+ * Symbol Enum은 카드의 심볼(1~10, J/Q/K)을 나열한다.
+ * 이에 대해 점수와 이름(A,J,Q,K에 대한 예외적인 경우 고려)을 부여하고 getter를 제작해둔다.
+ */
 public enum Symbol {
     ACE(1, "A"),
     TWO(2, "2"),
--- src/main/java/domain/card/Type.java
+++ src/main/java/domain/card/Type.java
@@ -1,5 +1,9 @@
 package domain.card;
 
+/**
+ * Type Enum은 카드의 타입(스페이드/다이아몬드/하트/클럽)을 나열한다.
+ * 이에 대한 type을 문자열로 가지고 있어, getter를 통해 이를 구할 수 있다.
+ */
 public enum Type {
     SPADE("스페이드"),
     DIAMOND("다이아몬드"),
