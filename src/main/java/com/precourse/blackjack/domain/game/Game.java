281bce542d921a0eebf5652783a435a9205ea9a8
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-12-13 17:59:25 +0900

    feat: Game클래스의 변수, 생성자 추가

--- src/main/java/com/precourse/blackjack/domain/game/Game.java
+++ src/main/java/com/precourse/blackjack/domain/game/Game.java
@@ -8,10 +8,26 @@
 
 package com.precourse.blackjack.domain.game;
 
+import java.util.List;
+
+import com.precourse.blackjack.domain.card.Card;
+import com.precourse.blackjack.domain.card.CardFactory;
+import com.precourse.blackjack.domain.user.Dealer;
+import com.precourse.blackjack.domain.user.Player;
+
 /**
  * 블랙잭 게임을 의미하는 객체입니다.
  *
  * @author HyungjuAn
  */
 public class Game {
+	private final List<Card> cardDeck;
+	private final List<Player> players;
+	private final Dealer dealer;
+
+	public Game(List<Player> players) {
+		this.cardDeck = CardFactory.create();
+		this.players = players;
+		this.dealer = new Dealer();
+	}
 }
