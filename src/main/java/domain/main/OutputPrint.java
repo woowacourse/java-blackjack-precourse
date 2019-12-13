1c70844877ecaf3a6cba402b40ec76dd8df5ceec
SMJin <sstaw@naver.com>
2019-12-13 16:21:52 +0900

    [OutputPrint Class] dealerLessThan16, finalReturn 메소드 생성

--- src/main/java/domain/main/OutputPrint.java
+++ src/main/java/domain/main/OutputPrint.java
@@ -6,14 +6,6 @@ import java.util.List;
 
 public class OutputPrint {
 
-    private void print(String message) {
-        System.out.print(message);
-    }
-
-    private void println(String message) {
-        System.out.println(message);
-    }
-
     public void getPlayerNames() {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
     }
@@ -33,6 +25,22 @@ public class OutputPrint {
 
     public void wantMoreCards(String playerName) {
         println(playerName+"는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n");
+    }
+
+    public void dealerLessThan16() {
+        println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+    }
+
+    public void finalReturn() {
+        println("## 최종 수익");
+    }
+
+    private void print(String message) {
+        System.out.print(message);
+    }
+
+    private void println(String message) {
+        System.out.println(message);
     }
 
 }
