2468550561875b37797bd483d0dcc92da6d575db
lalize <printf@kakao.com>
2019-12-13 18:24:54 +0900

    feat: Symbol, Type 클래스 별명 추가

--- src/main/java/domain/card/Symbol.java
+++ src/main/java/domain/card/Symbol.java
@@ -1,27 +1,40 @@
 package domain.card;
 
 public enum Symbol {
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+	ACE(1, "A"),
+	TWO(2),
+	THREE(3),
+	FOUR(4),
+	FIVE(5),
+	SIX(6),
+	SEVEN(7),
+	EIGHT(8),
+	NINE(9),
+	TEN(10),
+	JACK(10, "J"),
+	QUEEN(10, "Q"),
+	KING(10, "K");
 
-    private int score;
+	private int score;
+	private String alias;
 
-    Symbol(int score) {
-        this.score = score;
-    }
+	Symbol(int score) {
+		this.score = score;
+	}
 
-    public int getScore() {
-        return score;
-    }
+	Symbol(int score, String alias) {
+		this.score = score;
+		this.alias = alias;
+	}
+
+	public String getAlias() {
+		if (alias != null) {
+			return alias;
+		}
+		return String.valueOf(score);
+	}
+
+	public int getScore() {
+		return score;
+	}
 }
