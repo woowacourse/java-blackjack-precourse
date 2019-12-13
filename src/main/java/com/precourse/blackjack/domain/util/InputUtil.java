849c29f4b6169012abf5bfff3e32b8bf412fea8d
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-12-13 18:31:40 +0900

    feat: 입력을 담당하는 InputUtil 클래스 생성

---
+++ src/main/java/com/precourse/blackjack/domain/util/InputUtil.java
@@ -0,0 +1,21 @@
+/*
+ * InputUtil.java                        1.0.0   2019-12-13
+ *
+ * Copyright (c) 2019 Hyungju An.
+ * All rights reserved.
+ * Contact me for more information. a301dks@naver.com
+ */
+
+package com.precourse.blackjack.domain.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 인풋 유틸리티 클래스입니다.
+ *
+ * @author HyungjuAn
+ */
+public class InputUtil {
+	private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+}
