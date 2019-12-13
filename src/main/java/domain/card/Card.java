a00fff66fa99be0f0f35b2e76bbf45b397d0a2f8
lalize <printf@kakao.com>
2019-12-13 22:01:36 +0900

    fix: 에이스 카드를 유리하게 계산하는 기능 추가

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -22,6 +22,10 @@ public class Card {
 		return symbol.getScore();
 	}
 
+	public boolean isAce() {
+		return symbol == Symbol.ACE;
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o)
--- src/main/java/domain/user/Gamer.java
+++ src/main/java/domain/user/Gamer.java
@@ -2,10 +2,15 @@ package domain.user;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import domain.card.Card;
 
 public class Gamer {
+	private static final int ZERO_SCORE = 0;
+	private static final int ACE_EXTRA_SCORE = 10;
+	private static final int OPTIMAL_SCORE = 21;
+
 	private final List<Card> cards = new ArrayList<>();
 
 	public void addCard(Card card) {
@@ -16,7 +21,21 @@ public class Gamer {
 		return cards;
 	}
 
+	private int calculateAce(Card card, int sum) {
+		if (!card.isAce()) {
+			return ZERO_SCORE;
+		}
+		if (sum + ACE_EXTRA_SCORE <= OPTIMAL_SCORE) {
+			return ACE_EXTRA_SCORE;
+		}
+		return ZERO_SCORE;
+	}
+
 	public int getSumOfCards() {
-		return cards.stream().mapToInt(Card::getScore).sum();
+		int sum = cards.stream().mapToInt(Card::getScore).sum();
+		for (Card card : cards) {
+			sum += calculateAce(card, sum);
+		}
+		return sum;
 	}
 }
