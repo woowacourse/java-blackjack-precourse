b7f9adc5974bc95179e8947403cafaf735c55a4c
MOON JINJU <mjj920930@gmail.com>
2019-12-13 14:49:55 +0900

    feat: User가 카드를 1장 더 받을지 여부를 입력받는 기능 추가

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,5 +1,7 @@
 package View;
 
+import domain.user.User;
+
 import java.util.Scanner;
 
 public class InputView {
@@ -7,6 +9,7 @@ public class InputView {
     private static final String INPUT_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람들의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String INPUT_BETTING_MONEY_MESSAGE = "의 배팅 금액은?";
     private static final String NOT_A_NUMBER_MESSAGE = "숫자만 입력해주세요.";
+    private static final String INPUT_IF_GET_CARD_MESSAGE = "님 카드를 1장 더 받으시겠습니까?";
 
     private InputView() {}
 
@@ -23,6 +26,11 @@ public class InputView {
             System.out.println(NOT_A_NUMBER_MESSAGE);
             return inputBettingMoney(playerName);
         }
+    }
+
+    public static String inputIfGetCard(User user) {
+        System.out.println(user.getName() + INPUT_IF_GET_CARD_MESSAGE);
+        return scanner.nextLine();
     }
 
 }
