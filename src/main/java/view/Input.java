4bac840aade0dcd0c4b2eb7ca2984de40896eeeb
DB2 <10100aaaaa@gmail.com>
2019-12-13 14:31:49 +0900

    Input 불필요콤마 확인하는 기능 추가 및 테스트

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -1,16 +1,30 @@
 package view;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Input {
     private static final String COMMA = ",";
 
-    Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
     public Input() {
     }
 
     public String input() {
         return scanner.nextLine();
+    }
+
+    public List<String> asGamers() {
+        return Collections.emptyList();
+    }
+
+    public boolean containsDoubleComma(String input) {
+        return input.contains(COMMA + COMMA);
+    }
+
+    public boolean endWithComma(String input) {
+        return input.endsWith(COMMA);
     }
 }
---
+++ src/test/java/view/InputTest.java
@@ -0,0 +1,27 @@
+package view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InputTest {
+    @Test
+    public void inputInitTest() {
+        Input input = new Input();
+        assertThat(input).isNotNull();
+    }
+
+    @Test
+    public void containsDoubleCommaTest() {
+        Input input = new Input();
+        assertThat(input.containsDoubleComma("자바,안드,아이,,냄비,마스")).isTrue();
+        assertThat(input.containsDoubleComma("자바,안드,아이,냄비,마스")).isFalse();
+    }
+
+    @Test
+    public void endWithCommaTest() {
+        Input input = new Input();
+        assertThat(input.endWithComma("자바,")).isTrue();
+        assertThat(input.endWithComma("자바,코틀")).isFalse();
+    }
+}
