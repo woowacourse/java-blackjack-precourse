3f957293e9dd91afd9feb4000a7d007acfbfcd40
SMJin <sstaw@naver.com>
2019-12-13 16:03:41 +0900

    [OutputPrint Class] print, println 메소드 생성

--- src/main/java/domain/main/OutputPrint.java
+++ src/main/java/domain/main/OutputPrint.java
@@ -1,4 +1,13 @@
 package domain.main;
 
 public class OutputPrint {
+
+    private void print(String message) {
+        System.out.print(message);
+    }
+
+    private void println(String message) {
+        System.out.println(message);
+    }
+
 }
