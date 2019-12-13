ace084a48b319ee79030421b1a005f4b50d734a1
gunhee lee <dlrjsgml42@naver.com>
2019-12-13 21:18:39 +0900

    [Player/Dealer] make Player extends Dealer.

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -1,6 +1,7 @@
 package domain.user;
 
 import domain.card.Card;
+import domain.game.BlackJack;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,18 +9,10 @@ import java.util.List;
 /**
  * 게임 딜러를 의미하는 객체
  */
-public class Dealer {
-    protected final List<Card> cards = new ArrayList<>();
-    /**
-     * 딜러는 돈을 배팅하거나 버는 일이 없으므로, money는 0으로 설정되어 변경되지 않는다.
-     * 하지만 만약 유저가 잃은 돈을 딜러가 받아 수익으로 올리는 등의 로직이 필요하다면,
-     * 이 변수를 바탕으로 재설계할 수 있다.
-     */
-    protected double money = 0;
-
+public class Dealer{
+    private final List<Card> cards = new ArrayList<>();
     public Dealer() {
     }
-
     public void addCard(Card card) {
         cards.add(card);
     }
@@ -30,39 +23,35 @@ public class Dealer {
             score += card.getScore();
         }
         for(Card card : cards) {
-            score += card.getAceScore(score);
+            score += getAceScore(score, card);
         }
         return score;
     }
-    public String getOneCardString(){
-        return "딜러: " + cards.get(0).getCardName();
-    }
-    public String getCardString() {
-        List<String> nameList = new ArrayList<String>();
-        String cardString = "딜러 카드: ";
-        for(Card card : cards) {
-            nameList.add(card.getCardName());
-        }
-        return cardString + String.join(",",nameList);
-    }
 
+
+    public int getCardListSize() {
+        return cards.size();
+    }
     public String getScoreString() {
         return " - 결과: " + Integer.toString(getScore());
     }
 
-    public void addMoney(int money){
-        this.money += money;
-    }
 
-    /**
-     * getMoneyString은 최종 수익을 계산해주는 메서드이다.
-     * 딜러 객체는 배팅 기능이 없으므로, 이 메서드는 단순한 값만 출력하게 설계했다.
-     * 만약 유저가 돈을 잃을 때 그만큼 그 돈이 딜러에게 가도록 재설계한다면,
-     * 이 메서드를 다시 작성할 필요가 생긴다.
-     *
-     * @return
-     */
-    public String getMoneyString() {
-        return "딜러: "+ Double.toString(money);
+    public int getAceScore(int score, Card card){
+        if(score <= BlackJack.MAX_SCORE - BlackJack.ACE_BONUS_SCORE && card.isAceCard()){
+            return BlackJack.ACE_BONUS_SCORE;
+        }
+        return 0;
+    }
+    public String getCardStringWithName() {
+        return "딜러 " + getCardString();
+    }
+    public String getCardString() {
+        List<String> nameList = new ArrayList<String>();
+        String cardString = "카드: ";
+        for(Card card : cards) {
+            nameList.add(card.getCardName());
+        }
+        return cardString + String.join(",",nameList);
     }
 }
--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -1,39 +1,58 @@
 package domain.user;
 
 import domain.card.Card;
+import domain.game.BlackJack;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Player는 게임 참여자를 의미하는 객체이다.
- * Dealer 객체를 상속받아 addCard 메소드와 cardList를 가지고 있으며,
- * Player는 별도로 이름, 배팅금액, 배팅 관련 메소드를 가진다.
+ * 카드를 더하고 배팅을 하는 기능을 가지고 있다.
+ *
+ * 로직상 별도의 money 변수가 있다면, 배팅에 따른 이익/손실을 계산하기에 용이할 것으로 보인다.
+ * 그러나 요구사항 상 인스턴스 필드를 추가할 수 없으므로,
+ * 또한 베팅머니라는 변수의 값 변경이 불가(final) 하므로
+ * 실제 구현은 승패 여부에 따라 결과를 반환하는 함수의 추가 정도로 마친다.
  *
  * @author kafka
- * @version 1.1
+ * @version 1.2
  */
 public class Player extends Dealer{
+
     private final String name;
     private final double bettingMoney;
 
     public Player(String name, double bettingMoney) {
         this.name = name;
         this.bettingMoney = bettingMoney;
-        money = 0;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getResultMoney(int dealerScore) {
+        if(isBlackJackInFirstHand()){
+            return bettingMoney / 2.0;
+        }
+        if(dealerScore > BlackJack.MAX_SCORE || (getScore() <= BlackJack.MAX_SCORE && dealerScore < getScore())){
+            return bettingMoney;
+        }
+        if(getScore() > BlackJack.MAX_SCORE || getScore() < dealerScore){
+            return bettingMoney * -1.0;
+        }
+        return 0.0;
+    }
+    public String getResultMoneyString(int dealerScore){
+        return name+": "+Integer.toString((int)getResultMoney(dealerScore));
+    }
+
+    private boolean isBlackJackInFirstHand() {
+        return getCardListSize() == 2 && getScore() == BlackJack.MAX_SCORE;
     }
 
     @Override
-    public String getCardString() {
-        List<String> nameList = new ArrayList<String>();
-        String cardString = name+"카드: ";
-        for(Card card : cards) {
-            nameList.add(card.getCardName());
-        }
-        return cardString + String.join(",",nameList);
+    public String getCardStringWithName() {
+        return name + getCardString();
     }
-    @Override
-    public String getMoneyString() {
-        return name+": "+Double.toString((money - bettingMoney));
-    }
+
 }
