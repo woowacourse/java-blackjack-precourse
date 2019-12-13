e15c0a1041f4f0f1881cd68076a52ef9f03072c8
unknown <gptjs409@naver.com>
2019-12-13 20:57:02 +0900

    [기능구현] User.java BlackJack(21)과 Bust(21초과) 여부 기능

--- src/main/java/domain/user/User.java
+++ src/main/java/domain/user/User.java
@@ -1,5 +1,24 @@
 package domain.user;
 
-public class User {
+import domain.card.Card;
 
+public abstract class User {
+	int profit = 0;
+	int sumNumbers = 0;
+	
+    abstract void addCard(Card card);
+	
+    boolean blackJackYN(int sumNumbers) {
+    	if (sumNumbers == 21) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    boolean bustYN(int sumNumbers) {
+    	if (sumNumbers > 21) {
+    		return true;
+    	}
+    	return false;
+    }
 }
