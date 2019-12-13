08eba75993d5f693de8edc3bbdb356c846ae913b
ikswary <ikswary@naver.com>
2019-12-13 18:22:16 +0900

    [ADD] drawFromCardDeck 메소드 작성

--- src/main/java/domain/game/BlackJack.java
+++ src/main/java/domain/game/BlackJack.java
@@ -3,6 +3,7 @@ package domain.game;
 import domain.card.Card;
 import domain.user.Dealer;
 import domain.user.Player;
+import sources.BlackJackPlayingSource;
 
 import java.util.List;
 
@@ -15,5 +16,12 @@ public class BlackJack {
 		this.cardDeck = cardDeck;
 		this.players = players;
 		this.dealer = new Dealer();
+	}
+
+	private void drawFromCardDeck() {
+		for ( Player player : this.players ) {
+			player.addCard(BlackJackPlayingSource.popRandomCard(this.cardDeck));
+		}
+		dealer.addCard(BlackJackPlayingSource.popRandomCard(this.cardDeck));
 	}
 }
