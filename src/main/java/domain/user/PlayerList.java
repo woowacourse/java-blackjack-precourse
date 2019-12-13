9dea0a022c382ef5825e67ed58f2c7551444cbcc
SMJin <sstaw@naver.com>
2019-12-13 19:26:53 +0900

    [PlayerList Class] 생성자 생성

--- src/main/java/domain/user/PlayerList.java
+++ src/main/java/domain/user/PlayerList.java
@@ -1,4 +1,14 @@
 package domain.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerList {
+
+    private List<Player> playerList = new ArrayList<Player>();
+
+    public void addPlayer(Player player) {
+        playerList.add(player);
+    }
+
 }
