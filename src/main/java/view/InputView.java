dd5b52b0286709b86936fa70365bb77d16180524
lalize <printf@kakao.com>
2019-12-13 17:15:10 +0900

    style: 코드 정렬

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class InputView {
 	private static final Scanner SCANNER = new Scanner(System.in);
-	private static final String GET_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
 	private static final String NAMES_DELIMITER = ",";
+	private static final String GET_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
 	private static final String GET_BETTING_MONEY_MESSAGE = "의 베팅 금액은?";
 
 	public static String[] getPlayerNames() {
