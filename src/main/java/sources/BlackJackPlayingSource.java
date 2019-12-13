514a04ebeb781e9a2b14a069eb7ed5f839a41098
ikswary <ikswary@naver.com>
2019-12-13 18:21:52 +0900

    [FIX] popRandomCard 메소드 static, public하게 변경

--- src/main/java/sources/BlackJackPlayingSource.java
+++ src/main/java/sources/BlackJackPlayingSource.java
@@ -1,17 +1,12 @@
 package sources;
 
 import domain.card.Card;
-import domain.user.Player;
 import utils.UtilityMethods;
 
 import java.util.List;
 
 public class BlackJackPlayingSource {
-	public void startRound(List<Player> players, List<Card> cardDeck) {
-
-	}
-
-	private Card popRandomCard(List<Card> cards) {
+	public static Card popRandomCard(List<Card> cards) {
 		int popIndex = UtilityMethods.generateRandomNumber(cards.size());
 		return cards.remove(popIndex);
 	}
