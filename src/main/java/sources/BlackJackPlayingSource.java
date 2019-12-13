175af4e8391889119f1d61d41d589fc1a3ed098e
ikswary <ikswary@naver.com>
2019-12-13 16:01:30 +0900

    [ADD] popRandomCard 작성
    RandomCard를 결정하기위한 generateRandomNumber 메소드 작성

--- src/main/java/sources/BlackJackPlayingSource.java
+++ src/main/java/sources/BlackJackPlayingSource.java
@@ -1,7 +1,22 @@
 package sources;
 
+import domain.card.Card;
+import domain.card.CardFactory;
+
+import java.util.List;
+
 public class BlackJackPlayingSource {
 	public void startBlackJack(){
+		List<Card> cardDeck = CardFactory.create();
+	}
 
+	private Card popRandomCard(List<Card> cards){
+		int popIndex = generateRandomNumber(cards.size());
+		return cards.remove(popIndex);
+	}
+
+	private int generateRandomNumber(int limit){
+		int randomNumber = (int) (Math.random()*limit);
+		return  randomNumber;
 	}
 }
