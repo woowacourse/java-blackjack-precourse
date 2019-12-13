72fbd39eb7171347777cf19b6eac7dd1d2309680
SMJin <sstaw@naver.com>
2019-12-13 19:10:08 +0900

    [InputScanner Class] 援ы쁽

--- src/main/java/domain/main/InputScanner.java
+++ src/main/java/domain/main/InputScanner.java
@@ -1,4 +1,21 @@
 package domain.main;
 
+import java.util.Scanner;
+
 public class InputScanner {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public String stringLine() {
+        return scanner.nextLine();
+    }
+
+    public String string() {
+        return scanner.next();
+    }
+
+    public int integer() {
+        return scanner.nextInt();
+    }
+
 }
--- src/main/java/domain/main/Main.java
+++ src/main/java/domain/main/Main.java
@@ -9,11 +9,16 @@ import java.util.List;
 public class Main {
 
     private static CardFactory cardFactory = new CardFactory();
+    private static OutputPrint outputPrint = new OutputPrint();
 
     public static void main(String[] args) {
         List<Card> cards = new ArrayList<Card>();
         cards = cardFactory.create();
-        System.out.print(cards);
+//        System.out.print(cards);
+        outputPrint.getPlayerNames();
+
+
+
     }
 
 }
