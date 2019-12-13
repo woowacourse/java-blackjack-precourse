154e7eabec1b5bef1b05777fed3d740f2ef731cf
seonuk <sunwook317@gmail.com>
2019-12-13 18:51:24 +0900

    플레이어 이름, 베팅 금액 유저 인터페이스 구현

---
+++ src/main/java/domain/GameUI.java
@@ -0,0 +1,69 @@
+package domain;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameUI {
+
+    Scanner sc = new Scanner(System.in);
+    String[] players;
+
+
+    public void play() {
+        createPlayer();
+    }
+
+    public void createPlayer() {
+        players = inputPlayer();
+        for (String p : players) {
+            int b = inputBetting(p);
+
+        }
+    }
+
+    public String[] inputPlayer() {
+        String INPUTPLAYER = "게임에 참여할 사람의 이름을 입력하세요 (쉼표로 분리)";
+        String[] player;
+        String input;
+
+        do {
+            System.out.println(INPUTPLAYER);
+            input = sc.nextLine();
+            players = input.split(",");
+
+        } while (!(players.length >= 2));        //두 명 이상 입력이 안된 경우
+
+        for (String p : players)
+        System.out.println(p);
+        return players;
+    }
+
+    public int inputBetting(String name) {
+        int betting = 0;
+        boolean success = false;
+
+        System.out.println(name+"의 베팅금액은?");
+        do {
+            try {
+                betting = sc.nextInt();
+                if (betting <= 0) {
+                    throw new Exception();
+                }
+                success = true;
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력하세요!");
+                sc = new Scanner(System.in);
+
+            } catch (Exception e) {
+                System.out.println("더 큰 수를 입력하세요");
+                sc = new Scanner(System.in);
+            }
+        } while (!success);
+
+        return betting;
+    }
+
+
+}
