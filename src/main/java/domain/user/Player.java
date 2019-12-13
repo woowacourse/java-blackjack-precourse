881070603af72af6561eb0bc852ffacd1dfb851d
lalize <printf@kakao.com>
2019-12-13 17:26:04 +0900

    feat: Player 클래스 이름 유효성 검사 추가

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -9,13 +9,25 @@ import java.util.List;
  * 게임 참여자를 의미하는 객체
  */
 public class Player {
+	private static final String WHITE_SPACE = " ";
+
 	private final String name;
 	private final double bettingMoney;
 	private final List<Card> cards = new ArrayList<>();
 
 	public Player(String name, double bettingMoney) {
+		validateName(name);
 		this.name = name;
 		this.bettingMoney = bettingMoney;
+	}
+
+	public static void validateName(String name) {
+		if (name.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		if (name.contains(WHITE_SPACE)) {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	public void addCard(Card card) {
