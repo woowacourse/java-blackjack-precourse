fb3aa8d19b961f58f5ff4e73296bc906db2a4412
ParkSanggi <roqkfwkehlwk@naver.com>
2019-12-14 01:02:15 +0900

    feat : 9. 딜러와 모든 플레이어에게 카드를 2장씩 분배한다.

--- src/main/java/domain/manager/PlayManager.java
+++ src/main/java/domain/manager/PlayManager.java
@@ -1,14 +1,18 @@
 package domain.manager;
 
+import domain.card.CardFactory;
+import domain.card.Deck;
 import domain.user.Dealer;
 import domain.user.Gamer;
 import domain.user.Player;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class PlayManager {
     public final List<Gamer> gamers = new ArrayList<>();
     private final int dealerIndex = 0;
+    private final Deck deck = new Deck(CardFactory.create());
 
     public PlayManager(List<String> playerNameList, List<Integer> bettingMoneyList) {
         gamers.add(new Dealer());
@@ -18,6 +22,17 @@ public class PlayManager {
     private void getPlayers(List<String> playerNameList, List<Integer> bettingMoneyList) {
         for (int i = 0; i < playerNameList.size(); i++) {
             gamers.add(new Player(playerNameList.get(i), bettingMoneyList.get(i)));
+        }
+    }
+
+    public void PlayGame() {
+        setBasicCards();
+    }
+
+    private void setBasicCards() {
+        int basicCardCount = 2;
+        for (int i = 0; i < basicCardCount; i++) {
+            dealCards();
         }
     }
 }
