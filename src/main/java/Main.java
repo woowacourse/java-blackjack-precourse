ddbba68d67cbfcf6d5bad0de46fc66a56f23dcc0
DB2 <10100aaaaa@gmail.com>
2019-12-13 14:57:57 +0900

    Main BlackJack 클래스 추가

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import controller.BlackJack;
+
+public class Main {
+    public static void main(String[] args) {
+        BlackJack blackJack = new BlackJack();
+        blackJack.start();
+    }
+}
