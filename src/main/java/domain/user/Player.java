943b2aac5d58a322b9cc6df2f14daa0d5530e51a
lalize <printf@kakao.com>
2019-12-13 17:22:15 +0900

    feat: Player 클래스 getter 메소드 추가

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -9,19 +9,24 @@ import java.util.List;
  * 게임 참여자를 의미하는 객체
  */
 public class Player {
-    private final String name;
-    private final double bettingMoney;
-    private final List<Card> cards = new ArrayList<>();
+	private final String name;
+	private final double bettingMoney;
+	private final List<Card> cards = new ArrayList<>();
 
-    public Player(String name, double bettingMoney) {
-        this.name = name;
-        this.bettingMoney = bettingMoney;
-    }
+	public Player(String name, double bettingMoney) {
+		this.name = name;
+		this.bettingMoney = bettingMoney;
+	}
 
-    public void addCard(Card card) {
-        cards.add(card);
-    }
+	public void addCard(Card card) {
+		cards.add(card);
+	}
 
-    // TODO 추가 기능 구현
+	public String getName() {
+		return name;
+	}
 
+	public double getBettingMoney() {
+		return bettingMoney;
+	}
 }
