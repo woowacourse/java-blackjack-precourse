6071b39adaad45cfab5321a6b33cf6ef34b7866f
SMJin <sstaw@naver.com>
2019-12-13 16:04:51 +0900

    [OutputPrint Class] getBettingMoney 메소드 생성

--- src/main/java/domain/main/OutputPrint.java
+++ src/main/java/domain/main/OutputPrint.java
@@ -14,4 +14,8 @@ public class OutputPrint {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
     }
 
+    public void getBettingMoney(String playerName) {
+        println(playerName + "의 배팅 금액은?");
+    }
+
 }
