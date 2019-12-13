405d3cacb55a8c28e40eed08aebc2071a261d4f6
SangBoLee <sangbo0229@gmail.com>
2019-12-13 15:13:40 +0900

    플레이어 이름과 배팅금액을 이용하여 플레이어 인스턴스 리스트 만들기

--- src/main/java/domain/game/Game.java
+++ src/main/java/domain/game/Game.java
@@ -2,12 +2,14 @@ package domain.game;
 
 import java.util.ArrayList;
 
+import domain.user.Player;
 import domain.view.ViewInput;
 
 public class Game {
 	private static Game blackJack = new Game();
 	
 	private ArrayList<String> playerName = new ArrayList<String>();
+	private ArrayList<Player> players = new ArrayList<Player>();
 	
 	public static Game getInstance() {
 		return blackJack;
@@ -15,6 +17,7 @@ public class Game {
 
 	public void run() {
 		makePlayerName(ViewInput.getPlayerNames());
+		makePlayer();
 	}
 	
 	public String nameIsValid(String name) throws IllegalArgumentException {
@@ -31,17 +34,18 @@ public class Game {
 		for (String player : names) {
 			playerName.add(player.trim());
 		}
-		
-		getAllBettingPrice();
 	}
 	
-	public void getAllBettingPrice() {
-		for (String player : playerName) {
-			int money = ViewInput.getBettingPrice(player);
+	public void makePlayer() {
+		for (String name : playerName) {
+			Player player = new Player(name, ViewInput.getBettingPrice(name));
+			
+			players.add(player);
 		}
 	}
 	
 	public int moneyIsValid(int money) throws IllegalArgumentException {
 		return money;
 	}
+	
  }
