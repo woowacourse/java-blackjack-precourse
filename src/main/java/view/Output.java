6674504a5fa9c1c56d8027d2bef4205cf1130b00
DB2 <10100aaaaa@gmail.com>
2019-12-13 16:28:10 +0900

    Output 베팅금액 물어보는 Ui 추가

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -4,4 +4,8 @@ public class Output {
     public static void showGamePlayerInput() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
     }
+
+    public static void showBettingMoneyInput(String playerName) {
+        System.out.println(playerName + "의 배팅금액은?");
+    }
 }
