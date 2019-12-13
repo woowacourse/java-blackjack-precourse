c5a2b4a181e161a3223b68fddb1e78bcd070da2c
MOON JINJU <mjj920930@gmail.com>
2019-12-13 17:31:04 +0900

    feat: Dealer의 턴을 play하는 로직 구현

--- src/main/java/Controller/BlackjackGame.java
+++ src/main/java/Controller/BlackjackGame.java
@@ -11,6 +11,9 @@ import Exception.DeckHasNoCardException;
 import java.util.List;
 
 public class BlackjackGame {
+    public static final int DEALER_MINIMUM_SCORE_TO_PASS = 17;
+    public static final String DEALER_SCORE_IS_UNDER_MINIMUM_MESSAGE = "딜러의 점수가 17미만이므로 1장의 카드를 더 받습니다.";
+
     private List<User> users;
     private Deck deck;
 
@@ -60,6 +63,10 @@ public class BlackjackGame {
     }
 
     private void playTurnForOneDealer(Dealer dealer) {
-
+        while (dealer.getScoreOfCards() < DEALER_MINIMUM_SCORE_TO_PASS) {
+            System.out.println(DEALER_SCORE_IS_UNDER_MINIMUM_MESSAGE);
+            dealer.addCard(deck.draw());
+        }
+        OutputView.printCardsOfOneUser(dealer);
     }
 }
