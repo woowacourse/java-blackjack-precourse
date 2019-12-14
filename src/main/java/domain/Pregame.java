465c1139d62eddd0178ae27a6a45f7713f29abc8
Devhyein <jhn7121@naver.com>
2019-12-14 15:59:46 +0900

    player객체 생성

--- src/main/java/domain/Pregame.java
+++ src/main/java/domain/Pregame.java
@@ -2,6 +2,7 @@ package domain;
 
 import domain.user.Player;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Pregame {
@@ -18,12 +19,25 @@ public class Pregame {
     public void inputPlayerName(){
         String totalInputName;
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표 기준으로 구분) : ");
+        System.out.println("참여할 플레이어의 이름을 입력하세요.(이름은 쉼표 기준으로 구분) : ");
         totalInputName = sc.next();
         name = totalInputName.split(",");
+        if (!checkFormAboutName()) {
+            inputPlayerName();
+        }
+    }
+
+    private boolean checkFormAboutName() {
+        if(name.length < 2 || name.length > 8) {
+            System.out.println("player는 2~8명으로 설정해주세요.");
+            return false;
+        }
+        return true;
     }
 
     public void createPlayer() {
-
+        for (int i = 0; i < name.length; i++) {
+            player[i] = new Player(name[i], 0);
+        }
     }
 }
