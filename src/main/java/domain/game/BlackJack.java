24b3f7c15659ac09f52d218ab6c751e4332bfa44
gunhee lee <dlrjsgml42@naver.com>
2019-12-14 17:05:49 +0900

    [BlackJack] move draw method to Deck class.

--- src/main/java/domain/game/BlackJack.java
+++ src/main/java/domain/game/BlackJack.java
@@ -2,6 +2,7 @@ package domain.game;
 
 import domain.card.Card;
 import domain.card.CardFactory;
+import domain.card.Deck;
 import domain.user.Dealer;
 import domain.user.Player;
 import domain.util.ConstMessage;
@@ -48,17 +49,9 @@ public class BlackJack {
     private Dealer dealer;
 
     /**
-     * cardList는 카드 한 덱(52장)의 뭉치이다.
-     * cardFactory의 create 메소드를 통해 섞인 52장의 카드가 입력된다.
+     * deck은 52장의 카드 묶음을 생성하고 카드를 뽑는 것을 관리하는 객체이다.
      */
-    private List<Card> cardList;
-
-    /**
-     * cardIterator는 덱을 순회하는 번지값 저장 변수이다.
-     * 우리는 섞여있는 52장의 카드 리스트를 가지고 있으며, 이는 변경 불가하다.(뽑은 카드 pop 불가)
-     * 그렇기 때문에, 대신 선형으로 순회하며 카드를 전달하는 식으로 중복을 피하고 랜덤한 뽑기를 구현한다.
-     */
-    private int cardIterator;
+    private Deck deck;
 
     /**
      * 정적이고 보호되는 클래스 BlackJackHolder는,
@@ -81,31 +74,9 @@ public class BlackJack {
      * initBlackJack은 블랙잭 수행을 위한 유저, 카드, 플레이어들의 생성과 초기화를 담당한다.
      */
     private void initBlackJack() {
-        cardList = CardFactory.create();
-        cardIterator = 0;
+        deck = new Deck();
         dealer = new Dealer();
         createPlayerList();
-    }
-
-    /**
-     * drawCard 메서드는 카드를 한 장 뽑아 반환하고, iterator 값을 올려준다.
-     * 카드의 리스트는 생성시 셔플되므로, 이를 선형으로 순회하게 되면
-     * 중복 없이 랜덤한 카드가 뽑히는 것을 보장할 수 있다.
-     * <p>
-     * 카드가 다 떨어지는 극단적인 경우(예를 들어 20명 이상의 플레이어가 3장씩 뽑는다던가 하는 경우)를 고려하여
-     * 카드가 떨어지면 예외를 던지도록 구현하였다.
-     * 다 떨어졌을 때 새로운 덱을 create하는 것도 고려하였으나,
-     * 덱이 다 떨어지는 상황은 예외적 상황으로 보는 것이 더 올바르다고 판단하였다.
-     *
-     * @return 뽑힌 카드를 반환해준다.
-     * @throws AssertionError 뽑을 카드가 없는 경우, 논리적 에러를 생성한다.
-     */
-    private Card drawCard() {
-        if (cardIterator >= cardList.size()) {
-            System.out.print(ConstMessage.ERROR_CARD_EMPTY);
-            throw new AssertionError();
-        }
-        return cardList.get(cardIterator++);
     }
 
     /**
@@ -212,12 +183,12 @@ public class BlackJack {
      */
     private void firstDraw() {
         printNameInFirstDraw();
-        dealer.addCard(drawCard());
+        dealer.addCard(deck.drawCard());
         System.out.println(dealer.getCardStringWithName());
-        dealer.addCard(drawCard());
+        dealer.addCard(deck.drawCard());
         for (Player player : playerList) {
-            player.addCard(drawCard());
-            player.addCard(drawCard());
+            player.addCard(deck.drawCard());
+            player.addCard(deck.drawCard());
             System.out.println(player.getCardStringWithName());
         }
         System.out.print("\n");
@@ -293,7 +264,7 @@ public class BlackJack {
      */
     private void dealerDraw() {
         while (dealer.checkDrawMore()) {
-            dealer.addCard(drawCard());
+            dealer.addCard(deck.drawCard());
             System.out.println(ConstMessage.DRAW_DEALER);
         }
     }
@@ -306,7 +277,7 @@ public class BlackJack {
      */
     private void playerDraw(Player player) {
         while (player.checkDrawMore()) {
-            player.addCard(drawCard());
+            player.addCard(deck.drawCard());
             System.out.println(player.getCardStringWithName());
         }
     }
