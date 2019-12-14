ca41ec037cad25fe8529d1f57c4a7b36a0f3798d
chws <rebeccachoosir@gmail.com>
2019-12-15 00:45:02 +0900

    [FEATURE] 기능-각 플레이어의 배팅 금액 입력 받기

--- src/main/java/domain/manager/GameManager.java
+++ src/main/java/domain/manager/GameManager.java
@@ -9,7 +9,6 @@ public class GameManager {
 
     private static final String delimiter = ",";
     private static final Scanner SC = new Scanner(System.in);
-    private static final int numOfPlayers;
 
     private List<Player> players;
 
@@ -21,7 +20,7 @@ public class GameManager {
 
     public void startGame() {
         List<String> playerNames = Arrays.asList(setPlayerNames());//player 이름 입력
-        //각 player의 배팅 금액 입력
+        List<Integer> bettingMoney = setBettingMoney(playerNames);//각 player의 배팅 금액 입력
         //딜러, 플레이어 각각 두장의 카드 발급
         //현재 상황 출력
     }
@@ -32,6 +31,17 @@ public class GameManager {
         String input = sc.nextLine();
         String[] playerNamesArray = input.split(delimiter);
         return playerNamesArray;
+    }
+
+    public List<Integer> setBettingMoney(List<String> playerNames){
+        Scanner sc = SC;
+        List<Integer> bettingMoney = new List<Integer>;
+        for(String tmp: playerNames){
+            System.out.println(tmp + "의 배팅 금액은?");
+            Integer input = sc.nextInt();
+            bettingMoney.add(input);
+        }
+        return bettingMoney;
     }
 
 
