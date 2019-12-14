d14e92d9efb1bbb2640f3f353532c26922a424e9
moonyoungCHAE <xpf_fl@naver.com>
2019-12-15 01:30:37 +0900

    카드 생성

--- src/main/java/domain/BlackjackManager.java
+++ src/main/java/domain/BlackjackManager.java
@@ -1,6 +1,10 @@
 package domain;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import domain.card.Card;
+import domain.card.CardFactory;
 import domain.user.Player;
+import sun.print.BackgroundLookupListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,13 +14,20 @@ import java.util.List;
  */
 public class BlackjackManager {
     GamePrinter gamePrinter = new GamePrinter();
+    CardFactory cardFactory = new CardFactory();
     List<Player> players = new ArrayList<>();
+    List<Card> cards;
 
-    void makePlayer(){
+
+    void makePlayer() {
         String[] playerName = gamePrinter.getPlayerNameFromUser();
         for(int i = 0; i < playerName.length; i++){
             int bettingMoney = gamePrinter.getBettingMoneyFromUser(playerName[i]);
             players.add(new Player(playerName[i], bettingMoney));
         }
+    }
+
+    void makeCard(){
+        cards = cardFactory.create();
     }
 }
