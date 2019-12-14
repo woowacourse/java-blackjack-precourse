9271cf5930b694c7f23358dcfd515a50fec5b86a
Devhyein <jhn7121@naver.com>
2019-12-14 15:59:18 +0900

    호출되는 메서드 변경

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -3,6 +3,6 @@ package domain;
 public class Main {
     public static void main(String[] args) {
         Pregame pregame = new Pregame();
-        pregame.inputPlayerName();
+        pregame.run();
     }
 }
