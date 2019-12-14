8e338c5dcbf6ae3c07c5d1225b8896fad84d668b
woonjang <woonjangahn@gmail.com>
2019-12-14 21:10:04 +0900

    기능: 사용자 입력 관련 메소드 예외처리

--- src/main/java/ui/Input.java
+++ src/main/java/ui/Input.java
@@ -1,26 +1,67 @@
 package ui;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
+import exception.InvalidInputException;
+
 public class Input {
-
-    // 테스트용
-    private Scanner scanner = new Scanner("mika,john\n200\n500\nn\nn");
-
-    // private Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
     public String[] getNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return scanner.nextLine().split(",");
+        try {
+            String answer = scanner.nextLine();
+            validateNames(answer);
+            String[] split = answer.split(",");
+            validateNames(split);
+            return split;
+        } catch (Exception e) {
+            System.out.println("에러: " + e.getMessage());
+            return getNames();
+        }
+    }
+
+    private void validateNames(String input) throws Exception {
+        if (input.isEmpty() || input.endsWith(",") || input.contains(",,")) {
+            throw new InvalidInputException("잘못된 형식의 입력입니다.");
+        }
+    }
+
+    private void validateNames(String[] split) throws Exception {
+        if (split.length != new HashSet<String>(Arrays.asList(split)).size()) {
+            throw new InvalidInputException("잘못된 형식의 입력입니다.");
+        }
+        for (String name : split) {
+            validateNames(name);
+        }
     }
 
     public double getMoney(String name) {
         System.out.println("\n" + name + "의 배팅 금액은?");
-        return Double.parseDouble(scanner.nextLine());
+        try {
+            double answer = Double.parseDouble(scanner.nextLine());
+            validatePositiveAmount(answer);
+            return answer;
+        } catch (Exception e) {
+            System.out.println("에러: " + e.getMessage());
+            return getMoney(name);
+        }
+    }
+
+    private void validatePositiveAmount(double amount) throws Exception {
+        if (amount < 0) {
+            throw new InvalidInputException("잘못된 형식의 입력입니다.");
+        }
     }
 
     public boolean wantsMoreCard(String name) {
+        String answer;
         System.out.println("\n" + name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
-        return scanner.nextLine().equals("y");
+        do {
+            answer = scanner.nextLine();
+        } while (!answer.matches("[yn]"));
+        return answer.equals("y");
     }
 }
