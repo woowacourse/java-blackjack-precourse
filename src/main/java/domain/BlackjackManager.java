5e780caf28a078bc246c48429fcccad199ee9ac5
moonyoungCHAE <xpf_fl@naver.com>
2019-12-14 21:56:51 +0900

    Player 객체 생성

--- src/main/java/domain/BlackjackManager.java
+++ src/main/java/domain/BlackjackManager.java
@@ -1,4 +1,22 @@
 package domain;
 
+import domain.user.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 게임 전반을 관리하는 객체
+ */
 public class BlackjackManager {
+    GamePrinter gamePrinter = new GamePrinter();
+    List<Player> players = new ArrayList<>();
+
+    void makePlayer(){
+        String[] playerName = gamePrinter.getPlayerNameFromUser();
+        for(int i = 0; i < playerName.length; i++){
+            int bettingMoney = gamePrinter.getBettingMoneyFromUser(playerName[i]);
+            players.add(new Player(playerName[i], bettingMoney));
+        }
+    }
 }
