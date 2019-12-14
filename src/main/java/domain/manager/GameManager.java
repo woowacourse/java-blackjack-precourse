6d9ccb2e4cd1baca8137efbcca12aa0f54b9dd48
chws <rebeccachoosir@gmail.com>
2019-12-15 00:33:48 +0900

    [FEATURE] 플레이어 쉼표로 구분하여 입력

---
+++ src/main/java/domain/manager/GameManager.java
@@ -0,0 +1,43 @@
+package domain.manager;
+
+import domain.user.*;
+import domain.card.*;
+
+import java.util.*;
+
+public class GameManager {
+
+    private static final String delimiter = ",";
+    private static final Scanner SC = new Scanner(System.in);
+    private static final int numOfPlayers;
+
+    private List<Player> players;
+
+    public static void main(String[] args) {
+        GameManager game = new GameManager();
+        game.startGame();
+        game.playGame();
+    }
+
+    public void startGame() {
+        List<String> playerNames = Arrays.asList(setPlayerNames());//player 이름 입력
+        //각 player의 배팅 금액 입력
+        //딜러, 플레이어 각각 두장의 카드 발급
+        //현재 상황 출력
+    }
+
+    public String[] setPlayerNames(){
+        Scanner sc = SC;
+        System.out.println("게임에 참여할 사람들의 이름을 입력하세요. (쉼표 기준으로 분리함)");
+        String input = sc.nextLine();
+        String[] playerNamesArray = input.split(delimiter);
+        return playerNamesArray;
+    }
+
+
+    public void playGame() {
+        //각 player가 카드를 더 받을지
+        //딜러가 카드를 더 받을
+    }
+
+}
