1974caca3a583b4416165890856cc1ad83638758
lalize <printf@kakao.com>
2019-12-14 16:39:36 +0900

    feat: 플레이어를 생성하는 기능 추가

--- src/main/java/controller/BlackjackController.java
+++ src/main/java/controller/BlackjackController.java
@@ -1,8 +1,29 @@
 package controller;
 
-public class BlackjackController {
-	public BlackjackController() {
+import java.util.ArrayList;
+import java.util.List;
 
+import domain.card.Card;
+import domain.card.CardDeck;
+import domain.user.Dealer;
+import domain.user.Player;
+import view.InputView;
+
+public class BlackjackController {
+	private final CardDeck cardDeck = new CardDeck();
+	private final Dealer dealer = new Dealer();
+	private final List<Player> players = new ArrayList<>();
+
+	public BlackjackController() {
+		initPlayers();
+	}
+
+	private void initPlayers() {
+		String[] names = InputView.getPlayerNames();
+		for (String name : names) {
+			int bettingMoney = InputView.getBettingMoney(name);
+			players.add(new Player(name, bettingMoney));
+		}
 	}
 
 	public void run() {
