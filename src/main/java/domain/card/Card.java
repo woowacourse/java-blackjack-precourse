1fdbccf0c32a6d4b029d36d4fe96c97d8c5e2633
Devhyein <jhn7121@naver.com>
2019-12-15 00:40:06 +0900

    toString()했을 때 출력 형식 변경

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -17,6 +17,10 @@ public class Card {
 
     // TODO Card 관련 추가 기능 구현
 
+    public int getScore() {
+        return symbol.getScore();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -33,9 +37,13 @@ public class Card {
 
     @Override
     public String toString() {
-        return "Card{" +
+       /* return "Card{" +
                 "symbol=" + symbol +
                 ", type=" + type +
                 '}';
+                */
+        return symbol.getScore() + type.toString();
     }
+
+
 }
