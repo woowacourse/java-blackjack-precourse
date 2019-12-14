86df23207afc96941975a9911ef57d97ab6f3589
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-14 16:40:37 +0900

    feat: 추가 카드 여부 입력 구현

--- src/main/java/controller/InputController.java
+++ src/main/java/controller/InputController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import view.InputView;
@@ -7,6 +8,7 @@ import view.InputView;
 public class InputController {
 	private static String ERROR_MESSAGE = "유효하지 않은 입력입니다.";
 	private static final int NAME_MIN_LEN = 1;
+	private static String YES_NO[] = {"y", "n"};
 	InputView inputView;
 
 	public List<String> getPlayerNameList() {
@@ -33,6 +35,18 @@ public class InputController {
 		return bettingMoney;
 	}
 
+	public boolean getYesOrNo(String name) {
+		String draw;
+		try {
+			draw = inputView.getDrawYesOrNo(name, YES_NO);
+			checkValidDraw(draw);
+		} catch (Exception e) {
+			System.out.println(ERROR_MESSAGE);
+			return getYesOrNo(name);
+		}
+		return draw.equals(YES_NO[0]);
+	}
+
 	private void checkValidNameList(List<String> playerNameList) throws Exception {
 		boolean checkResult = playerNameList.stream()
 			.map(name -> isValidName(name))
@@ -45,6 +59,12 @@ public class InputController {
 
 	private void checkValidBettingMoney(double bettingMoney) throws Exception {
 		if (bettingMoney <= 0) {
+			throw new Exception();
+		}
+	}
+
+	private void checkValidDraw(String draw) throws Exception {
+		if (Arrays.asList(YES_NO).contains(draw) == false) {
 			throw new Exception();
 		}
 	}
