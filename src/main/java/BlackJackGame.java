936ef453d3be1e92d6656e8876278621da9dc8d3
hyesun.choi <gptjs409@naver.com>
2019-12-14 21:41:05 +0900

    [기능구현] BlackJackGame.java PlayerNamingChecker.java 플레이어의 배팅금액을 입력받는 기능

--- src/main/java/BlackJackGame.java
+++ src/main/java/BlackJackGame.java
@@ -3,8 +3,9 @@ import domain.user.User;
 
 public class BlackJackGame {
     private List<User> users = new ArrayList<User>();
+    Map<String, Integer> playerInfo = new HashMap<String, Integer>();
     Scanner scanner = new Scanner(System.in);
-    PlayerNamingChecker playerNamingChecker = new PlayerNamingChecker();
+
 
     public static void main(String[] args) {
         BlackJackGame blackJackGame = new BlackJackGame();
@@ -16,8 +17,10 @@ public class BlackJackGame {
     }
 
     private void startGame() {
-        Map<String, Integer> playerInfo = new HashMap<String, Integer>();
-        playerInfo = playerNaming();
+        playerNaming();
+        for (String playerName : playerInfo.keySet()) {
+            playerInfo.put(playerName, playerSetBettingMoney(playerName));
+        }
     }
 
     private void proceedGame() {
@@ -28,16 +31,25 @@ public class BlackJackGame {
 
     }
 
-    private Map<String, Integer> playerNaming() {
-        Map<String, Integer> players = new HashMap<String, Integer>();
+    private void playerNaming() {
         String playerNames = "";
+        PlayerNamingChecker playerNamingChecker = new PlayerNamingChecker();
         do {
             System.out.println("게임에 참여할 사람의 이름을 입력하세요. (쉼표 기준으로 분리, 중복은 권장하지 않습니다.)");
             playerNames = scanner.nextLine().trim();
         } while (!playerNamingChecker.playerNamingCheck(playerNames));
         for (String player : playerNames.split(",")) {
-            players.put(player.trim(), 0);
+            playerInfo.put(player.trim(), 0);
         }
-        return players;
+    }
+
+    private int playerSetBettingMoney(String playerName) {
+        String bettingMoney = "";
+        PlayerBettingMoneyChecker playerBettingMoneyChecker = new PlayerBettingMoneyChecker();
+        do {
+            System.out.println(playerName + "의 배팅 금액은?(10원단위로 쉼표없이 입력해주세요)");
+            bettingMoney = scanner.nextLine().trim();
+        } while (!playerBettingMoneyChecker.playerBettingMoneyCheck(bettingMoney));
+        return Integer.parseInt(bettingMoney);
     }
 }
---
+++ src/main/java/PlayerBettingMoneyChecker.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class PlayerBettingMoneyChecker {
+    boolean playerBettingMoneyCheck(String bettingMoney) {
+        if (bettingMoney.isEmpty()) {
+            return false;
+        }
+        if (!bettingMoneyIsNumber(bettingMoney)) {
+            return false;
+        }
+        if (!bettingMoneyCheckValue(Integer.parseInt(bettingMoney)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean bettingMoneyIsNumber (String bettingMoney) {
+        Set<Boolean> checkBlankSet = new HashSet<Boolean>();
+        for (int i = 0; i < bettingMoney.length(); i++) {
+            checkBlankSet.add(charIsNumber(bettingMoney.charAt(i)));
+        }
+        if(checkBlankSet.contains(true)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean charIsNumber (char number) {
+        if (number >= '0' || number <= '9') {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean bettingMoneyCheckValue (int bettingMoney) {
+        if (bettingMoney < 0 || bettingMoney > Integer.MAX_VALUE) {
+            return false;
+        } else if (bettingMoney % 10 != 0) {
+            return false;
+        }
+        return false;
+    }
+}
