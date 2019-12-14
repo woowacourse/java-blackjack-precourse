de51a5cea0b54d021b7bab4b3842360543849862
choiyeonseok <smr603@snu.ac.kr>
2019-12-15 04:45:52 +0900

    기능 : 카드더미 생성 및 처음 카드 두장 배분

--- src/main/java/domain/game/BlackJackGame.java
+++ src/main/java/domain/game/BlackJackGame.java
@@ -4,15 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import domain.user.Dealer;
 import domain.user.Player;
 
 public class BlackJackGame {
+	private static final int INITIAL_CARDS = 2;
+	
+	private CardShoe cardShoe;
+	private Dealer dealer;
 	private List<Player> playerList = new ArrayList<>();
 	private String[] playerNames;
+	
 	Scanner input = new Scanner(System.in);
+	
+	public BlackJackGame() {
+		cardShoe = new CardShoe();
+		dealer = new Dealer();
+	}
 	
 	public void play() {
 		inputPlayerInfos();
+		initialDeal();
 	}
 	
 	private void inputPlayerInfos() {
@@ -23,6 +35,14 @@ public class BlackJackGame {
 			System.out.println(name + "의 배팅 금액은?");
 			double bettingMoney = input.nextDouble();
 			playerList.add(new Player(name, bettingMoney));
+		}
+	}
+	
+	private void initialDeal() {
+		for (int i = 0; i < INITIAL_CARDS; i++) {
+			dealer.addCard(cardShoe.getOneCard());
+			playerList.stream()
+					.forEach(player -> player.addCard(cardShoe.getOneCard()));
 		}
 	}
 	
---
+++ src/main/java/domain/game/CardShoe.java
@@ -0,0 +1,26 @@
+package domain.game;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import domain.card.Card;
+import domain.card.CardFactory;
+
+public class CardShoe {
+	private static final int TOP_OF_DUMMY = 0;
+	private List<Card> cardDummy = new ArrayList<>(CardFactory.create());
+	
+	public CardShoe() {
+		shuffleCardDummy();
+	}
+	
+	public void shuffleCardDummy() {
+		Collections.shuffle(cardDummy);
+	}
+	
+	public Card getOneCard() {
+		return cardDummy.remove(TOP_OF_DUMMY);
+	}
+	
+}
