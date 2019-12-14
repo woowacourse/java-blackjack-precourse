6202987b0d56edabb85dd467178698fa3a097a0a
ParkSanggi <roqkfwkehlwk@naver.com>
2019-12-14 19:12:07 +0900

    feat : 12. 사용되지 않은 카드들 중 한장을 무작위로 뽑는다.

--- src/main/java/domain/card/Deck.java
+++ src/main/java/domain/card/Deck.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class Deck {
     private List<Card> cards;
-    private HashMap<Integer, Boolean> cardsOnTable = new HashMap<Integer, Boolean>();
+    public HashMap<Integer, Boolean> cardsOnTable = new HashMap<Integer, Boolean>();
 
     public Deck(List<Card> cards) {
         this.cards = cards;
