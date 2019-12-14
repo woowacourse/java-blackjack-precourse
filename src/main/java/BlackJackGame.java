a0ad4d7157fa319a5d008b6eeda6bea5a13bc94a
hyesun.choi <gptjs409@naver.com>
2019-12-14 20:20:35 +0900

    [기능구현] BlackJackGame.java Game을 단계별로 나눔

---
+++ src/main/java/BlackJackGame.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
+import domain.user.User;
+
+public class BlackJackGame {
+    private List<User> users = new ArrayList<User>();
+
+    public static void main(String[] args) {
+        BlackJackGame blackJackGame = new BlackJackGame();
+        blackJackGame.playGame();
+    }
+
+    private void playGame() {
+
+    }
+
+    private void startGame() {
+
+    }
+
+    private void proceedGame() {
+
+    }
+
+    private void endGame() {
+
+    }
+}
