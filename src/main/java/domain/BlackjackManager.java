7475c06af7d10af9b1487011c6c3ebb5dd2a8da6
moonyoungCHAE <xpf_fl@naver.com>
2019-12-15 02:33:42 +0900

    참여자들에게 부여하는 카드의 중복체크

--- src/main/java/domain/BlackjackManager.java
+++ src/main/java/domain/BlackjackManager.java
@@ -2,9 +2,11 @@ package domain;
 
 import domain.card.Card;
 import domain.card.CardFactory;
+import domain.user.Dealer;
 import domain.user.Participant;
 import domain.user.Player;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,22 +18,39 @@ public class BlackjackManager {
     CardFactory cardFactory = new CardFactory();
     List<Player> players = new ArrayList<>();
     List<Card> cards;
-
+    List<Card> addedCards;
+    Dealer dealer = new Dealer();
 
     void makePlayer() {
         String[] playerName = gamePrinter.getPlayerNameFromUser();
-        for(int i = 0; i < playerName.length; i++){
+        for (int i = 0; i < playerName.length; i++) {
             int bettingMoney = gamePrinter.getBettingMoneyFromUser(playerName[i]);
             players.add(new Player(playerName[i], bettingMoney));
         }
     }
 
-    void makeCard(){
+    void makeCard() {
         cards = cardFactory.create();
     }
 
-    void giveCard(Participant participant){
+    void giveCardToParticipant(Participant participant) {
         int cardIndex = (int) (Math.random() * cards.size());
-        participant.addCard(cards.get(cardIndex));
+        boolean availableToGive = true;
+        for (int i = 0; i < addedCards.size(); i++) {
+            cardOverlap(cards.get(cardIndex), addedCards.get(i), availableToGive);
+        }
+
+        if (availableToGive) {
+            participant.addCard(cards.get(cardIndex));
+            addedCards.add(cards.get(cardIndex));
+            return;
+        }
+        giveCardToParticipant(participant);
+    }
+
+    // 새로 추가할 카드가 이미 추가된 카드인지 확인한다.
+    void cardOverlap(Card newCard, Card addedCard, boolean availableToGive) {
+        if (newCard.equals(addedCard))
+            availableToGive = false;
     }
 }
