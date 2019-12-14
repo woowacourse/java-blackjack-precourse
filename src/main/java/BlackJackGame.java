5a7b2f5a4a3371cb7ad51a477623182a14d16c73
hyesun.choi <gptjs409@naver.com>
2019-12-14 21:04:53 +0900

    [기능구현] BlackJackGame.java PlayerNamingChecker.java 플레이어의 이름을 입력받는 기능

--- src/main/java/BlackJackGame.java
+++ src/main/java/BlackJackGame.java
@@ -1,9 +1,10 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 import domain.user.User;
 
 public class BlackJackGame {
     private List<User> users = new ArrayList<User>();
+    Scanner scanner = new Scanner(System.in);
+    PlayerNamingChecker playerNamingChecker = new PlayerNamingChecker();
 
     public static void main(String[] args) {
         BlackJackGame blackJackGame = new BlackJackGame();
@@ -15,7 +16,8 @@ public class BlackJackGame {
     }
 
     private void startGame() {
-
+        Map<String, Integer> playerInfo = new HashMap<String, Integer>();
+        playerInfo = playerNaming();
     }
 
     private void proceedGame() {
@@ -24,5 +26,18 @@ public class BlackJackGame {
 
     private void endGame() {
 
+    }
+
+    private Map<String, Integer> playerNaming() {
+        Map<String, Integer> players = new HashMap<String, Integer>();
+        String playerNames = "";
+        do {
+            System.out.println("게임에 참여할 사람의 이름을 입력하세요. (쉼표 기준으로 분리, 중복은 권장하지 않습니다.)");
+            playerNames = scanner.nextLine().trim();
+        } while (!playerNamingChecker.playerNamingCheck(playerNames));
+        for (String player : playerNames.split(",")) {
+            players.put(player.trim(), 0);
+        }
+        return players;
     }
 }
---
+++ src/main/java/PlayerNamingChecker.java
@@ -0,0 +1,23 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class PlayerNamingChecker {
+
+    boolean playerNamingCheck(String playerNames) {
+        if(playerNamingCheckBlank(playerNames)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean playerNamingCheckBlank(String playerNames) {
+        Set<Boolean> checkBlankSet = new HashSet<Boolean>();
+        for (String playerName : playerNames.split(",")) {
+            checkBlankSet.add(playerName.trim().isEmpty());
+        }
+        if(checkBlankSet.contains(true)) {
+            return true;
+        }
+        return false;
+    }
+}
