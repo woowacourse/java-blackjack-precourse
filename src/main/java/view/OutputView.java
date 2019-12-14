7a23bb31716b5f34d59543f1db2ed638a64dc957
lalize <printf@kakao.com>
2019-12-14 18:21:01 +0900

    refactor: printCards 메소드 분리
    concatNameAndCards를 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,14 +8,19 @@ import domain.card.Card;
 public class OutputView {
 	private static final String CARD_NAMES_DELIMITER = ", ";
 	private static final String DEALER_ADD_CARD_MESSAGE = "는 16이하라 한 장의 카드를 더 받았습니다.";
+	private static final String CONCAT_DELIMITER = ": ";
 
-	public static void printCards(String name, List<Card> cards) {
+	private static String concatNameAndCards(String name, List<Card> cards) {
 		String cardNames = String.join(CARD_NAMES_DELIMITER,
 				cards.stream()
 						.map(Card::getName)
 						.collect(Collectors.toList())
 		);
-		System.out.println(name + ": " + cardNames);
+		return name + CONCAT_DELIMITER + cardNames;
+	}
+
+	public static void printCards(String name, List<Card> cards) {
+		System.out.println(concatNameAndCards(name, cards));
 	}
 
 	public static void printDealerAddCard(String name) {
