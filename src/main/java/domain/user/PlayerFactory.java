c49cd9c171a0dd97a8b0e6395201f3c1cc7a7c7f
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-14 19:55:51 +0900

    refacor: Gambler 클래스 추가, Dealer와 Player는 이를 상속

--- src/main/java/domain/user/PlayerFactory.java
+++ src/main/java/domain/user/PlayerFactory.java
@@ -5,8 +5,8 @@ import java.util.Collections;
 import java.util.List;
 
 public class PlayerFactory {
-	public static List<Player> create(List<String>names,List<Double>bettings){
-		List<Player> players=new ArrayList<>();
+	public static List<Gambler> create(List<String>names,List<Double>bettings){
+		List<Gambler> players=new ArrayList<>();
 		for(int i=0;i<names.size();i++){
 			players.add(new Player(names.get(i),bettings.get(i)));
 		}
