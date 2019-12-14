f6f88fc4b40343da90611a20d40be2e8c4a97632
ParkSanggi <roqkfwkehlwk@naver.com>
2019-12-14 20:55:13 +0900

    feat : 17. 플레이어가 가지고 있는 카드들의 정보를 출력한다.

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -4,6 +4,7 @@ import domain.card.Card;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringJoiner;
 
 /**
  * 게임 참여자를 의미하는 객체
@@ -17,6 +18,7 @@ public class Player extends Gamer {
         this.bettingMoney = bettingMoney;
     }
 
-    // TODO 추가 기능 구현
-
+    public String toString() {
+        return name + " : " + cardsToString(getCards());
+    }
 }
