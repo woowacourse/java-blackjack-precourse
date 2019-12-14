473c51111fb8113a044176c7fe349a69ec2d042e
lalize <printf@kakao.com>
2019-12-14 23:43:34 +0900

    refactor: WHITE_SPACE -> BLANK로 이름 수정

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -4,10 +4,10 @@ package domain.user;
  * 게임 참여자를 의미하는 객체
  */
 public class Player extends User {
-	private static final String WHITE_SPACE = " ";
+	private static final String BLANK = " ";
 	private static final int MIN_BETTING_MONEY = 1;
 	private static final String NAME_EMPTY_ERROR = "이름이 비어있다.";
-	private static final String NAME_WHITE_SPACE_ERROR = "이름에 공백이 존재한다.";
+	private static final String NAME_BLANK_ERROR = "이름에 공백이 존재한다.";
 	private static final String BETTING_MONEY_ERROR = "베팅 금액이 " + MIN_BETTING_MONEY + " 보다 작다.";
 
 	private final String name;
@@ -24,8 +24,8 @@ public class Player extends User {
 		if (name.isEmpty()) {
 			throw new IllegalArgumentException(NAME_EMPTY_ERROR);
 		}
-		if (name.contains(WHITE_SPACE)) {
-			throw new IllegalArgumentException(NAME_WHITE_SPACE_ERROR);
+		if (name.contains(BLANK)) {
+			throw new IllegalArgumentException(NAME_BLANK_ERROR);
 		}
 	}
 
