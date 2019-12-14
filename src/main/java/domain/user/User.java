b0373eac5fb21fa6246a1fa088e302f8ed1b5270
hyesun.choi <gptjs409@naver.com>
2019-12-14 16:29:29 +0900

    [기능구현] User.java 유저별 card정보를 보여주는 기능

--- src/main/java/domain/user/User.java
+++ src/main/java/domain/user/User.java
@@ -1,13 +1,14 @@
 package domain.user;
 
 import domain.card.Card;
+import java.util.List;
 
 public abstract class User {
 	int profit = 0;
 	int sumNumbers = 0;
 	
     abstract void addCard(Card card);
-	
+
     boolean blackJackYN(int sumNumbers) {
     	if (sumNumbers == 21) {
     		return true;
@@ -21,4 +22,15 @@ public abstract class User {
     	}
     	return false;
     }
+
+	void userCardInfo(List<Card> cards) {
+		for (int i = 0; i < cards.size() - 2; i++) {
+			System.out.print(cards.get(i).cardInfo() + ", ");
+		}
+		System.out.print(cards.get(cards.size() - 1).cardInfo());
+	}
+
+	void userCardInfo(List<Card> cards, String dealer) {
+		System.out.print(cards.get(0).cardInfo());
+	}
 }
