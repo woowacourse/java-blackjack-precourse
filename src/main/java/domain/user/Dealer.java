f2a9a38dbea76aa4145ce32eeb70f9744d72a8df
ParkSanggi <roqkfwkehlwk@naver.com>
2019-12-14 20:50:13 +0900

    feat : 16. 딜러 손에 있는 카드들 중 히드 카드를 제외한 모든 카드들의 정보를 문자열로 반환한다.

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -17,4 +17,13 @@ public class Dealer extends Gamer {
     public String toString() {
         return "딜러 : " + cardsToString(getCards());
     }
+
+    public String cardsToString(List<Card> cards) {
+        StringJoiner stringJoiner = new StringJoiner(", ");
+        int hiddenCardIndex = 0;
+        for (int i = hiddenCardIndex + 1; i < cards.size(); i++) {
+            stringJoiner.add(cards.get(i).toString());
+        }
+        return stringJoiner.toString();
+    }
 }
