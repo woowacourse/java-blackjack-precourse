d4817085267c0ceeac6f24e91f6a558c0913b256
jiwon_lala <s26788761@naver.com>
2019-12-14 17:29:26 +0900

    feat : 딜러가 카드를 추가로 받는 기준인 16 상수화 와 +a 구현
    private final int DEALER_CRITERIA = 16;
    //딜러가 카드를 추가로 받거나 멈추는 기준인 16을 상수화.
    
    Player 클래스와 중복되는 함수 구현
    public int getScore();
    public boolean cardsContainsAce();
    private int getCardsPoint();
    public boolean isWinner(int maxValue);

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -9,6 +9,8 @@ import java.util.List;
  * 게임 딜러를 의미하는 객체
  */
 public class Dealer {
+    private final int DEALER_CRITERIA = 16;
+
     private final List<Card> cards = new ArrayList<>();
 
     public Dealer() {}
@@ -17,5 +19,40 @@ public class Dealer {
         cards.add(card);
     }
 
-    // TODO 추가 기능 구현
+    public int getScore() {
+        int score = getCardsPoint();
+
+        if (cardsContainsAce() && (score+10) <= 21) {
+            score += 10;
+        }
+        return score;
+    }
+
+    public boolean cardsContainsAce() {
+        for (Card card : cards) {
+            if (card.isAce()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int getCardsPoint() {
+        int point = 0;
+        for (Card card : cards) {
+            point += card.getSymbolValue();
+        }
+        return point;
+    }
+
+    public boolean isWinner(int maxValue) {
+        if (getScore() > 21) {
+            return false;
+        }
+        if (getScore() >= maxValue) {
+            return true;
+        }
+        return false;
+    }
+
 }
