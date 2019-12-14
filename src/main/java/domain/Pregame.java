99472ce0210bcd2c3811d56196f0e204f1ea7b8e
Devhyein <jhn7121@naver.com>
2019-12-14 15:47:06 +0900

    게임 player 이름입력받기

--- src/main/java/domain/Pregame.java
+++ src/main/java/domain/Pregame.java
@@ -1,7 +1,29 @@
 package domain;
 
-/**
- * Created by gram on 2019-12-14.
- */
+import domain.user.Player;
+
+import java.util.Scanner;
+
 public class Pregame {
+    Scanner sc = new Scanner(System.in);
+    String[] name;
+    Player[] player;
+
+    public void run() {
+        inputPlayerName();
+        player = new Player[name.length];
+        createPlayer();
+    }
+
+    public void inputPlayerName(){
+        String totalInputName;
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표 기준으로 구분) : ");
+        totalInputName = sc.next();
+        name = totalInputName.split(",");
+    }
+
+    public void createPlayer() {
+
+    }
 }
