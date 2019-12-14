d71cd515ba664d6b839aa890e86474f969fb5890
lalize <printf@kakao.com>
2019-12-14 19:19:00 +0900

    feat: Profit 클래스 추가

--- src/main/java/domain/profit/Profit.java
+++ src/main/java/domain/profit/Profit.java
@@ -1,4 +1,11 @@
 package domain.profit;
 
 public class Profit {
+	private final String name;
+	private final double profit;
+
+	public Profit(String name, double profit) {
+		this.name = name;
+		this.profit = profit;
+	}
 }
