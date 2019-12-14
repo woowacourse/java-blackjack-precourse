37b3a91dda6a8c6ef763f5a10a73cd92cdd5e0ef
junk-sound <wom2277@naver.com>
2019-12-14 16:40:39 +0900

    기능: '게임에 참여할 플레이어들을 정한다' 중 플레이어들의 이름을 입력받는 기능.

--- src/main/java/ui/Console.java
+++ src/main/java/ui/Console.java
@@ -1,8 +1,34 @@
 package ui;
 
+import common.BlackjackConfig;
+
+import java.util.Scanner;
+
 public class Console implements UserInterface {
+
+    private Scanner scanner;
+
+    public Console(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
     @Override
     public String[] extractNames() {
+        String input = scanner.nextLine();
+        try {
+            return parseNames(input);
+        } catch (RuntimeException e) {
+
+        }
+
         return new String[0];
+    }
+
+    private String[] parseNames(String input) {
+        input = input.trim();
+
+        String[] names = input.split(BlackjackConfig.STANDARD_FOR_PARSE);
+
+        return names;
     }
 }
