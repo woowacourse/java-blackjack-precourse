5849425563831cedb6da103dafa86b6fc0948c0c
Jinhong Kim <deepred.kim@gmail.com>
2019-12-14 15:26:41 +0900

    style: space to tab

--- src/main/java/com/woowahan/card/CardFactory.java
+++ src/main/java/com/woowahan/card/CardFactory.java
@@ -8,19 +8,19 @@ import java.util.List;
  * 트럼프 카드 전체 생성을 담당하는 객체
  */
 public class CardFactory {
-    public static List<Card> create() {
-        List<Card> cards = new ArrayList<>();
-        Symbol[] symbols = Symbol.values();
-        for (Symbol symbol : symbols) {
-            createByType(cards, symbol);
-        }
-        return Collections.unmodifiableList(cards);
-    }
+	public static List<Card> create() {
+		List<Card> cards = new ArrayList<>();
+		Symbol[] symbols = Symbol.values();
+		for (Symbol symbol : symbols) {
+			createByType(cards, symbol);
+		}
+		return Collections.unmodifiableList(cards);
+	}
 
-    private static void createByType(List<Card> cards, Symbol symbol) {
-        Type[] types = Type.values();
-        for (Type type : types) {
-            cards.add(new Card(symbol, type));
-        }
-    }
+	private static void createByType(List<Card> cards, Symbol symbol) {
+		Type[] types = Type.values();
+		for (Type type : types) {
+			cards.add(new Card(symbol, type));
+		}
+	}
 }
