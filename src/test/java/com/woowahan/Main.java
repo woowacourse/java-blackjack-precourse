766b380b63dc4a275c882f5bb56fe3aaff2c7d6b
Jinhong Kim <deepred.kim@gmail.com>
2019-12-14 17:13:41 +0900

    test: 테스트 클래스 추가

--- src/test/java/com/woowahan/Main.java
+++
@@ -1,7 +0,0 @@
-package com.woowahan;
-
-public class Main {
-	public static void main(String[] args) {
-
-	}
-}
---
+++ src/test/java/com/woowahan/blackjack/BlackjackTest.java
@@ -0,0 +1,8 @@
+package com.woowahan.blackjack;
+
+public class BlackjackTest {
+	public static void main(String[] args) {
+		Blackjack blackjack = new Blackjack();
+		blackjack.run();
+	}
+}
