c86a7d0690ecd48e197647b8eaee3edc7e5cd28a
lalize <printf@kakao.com>
2019-12-14 16:51:34 +0900

    feat: 딜러가 카드를 나눠주는 기능 추가

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -1,9 +1,15 @@
 package domain.user;
 
+import domain.card.CardDeck;
+
 /**
  * 게임 딜러를 의미하는 객체
  */
 public class Dealer extends User {
 	public Dealer() {
+	}
+
+	public void giveCard(CardDeck cardDeck, User user) {
+		user.addCard(cardDeck.draw());
 	}
 }
