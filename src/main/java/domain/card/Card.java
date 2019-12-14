35557af131ea2e09f106298f23050199059ef167
YerinCho <forjustice9839@gmail.com>
2019-12-15 01:29:56 +0900

    Card의 toString 수정에 의해 Symbol,Type 수정

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -11,31 +11,29 @@ public class Card {
     private final Type type;
 
     public Card(Symbol symbol, Type type) {
-        this.symbol = symbol;
-        this.type = type;
+	this.symbol = symbol;
+	this.type = type;
     }
 
     // TODO Card 관련 추가 기능 구현
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Card card = (Card) o;
-        return symbol == card.symbol &&
-                type == card.type;
+	if (this == o)
+	    return true;
+	if (o == null || getClass() != o.getClass())
+	    return false;
+	Card card = (Card) o;
+	return symbol == card.symbol && type == card.type;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(symbol, type);
+	return Objects.hash(symbol, type);
     }
 
     @Override
     public String toString() {
-        return "Card{" +
-                "symbol=" + symbol +
-                ", type=" + type +
-                '}';
+	return symbol.getSymbol() + " " + type.getTypeName();
     }
 }
--- src/main/java/domain/card/Type.java
+++ src/main/java/domain/card/Type.java
@@ -1,8 +1,18 @@
 package domain.card;
 
 public enum Type {
-    SPADE,
-    DIAMOND,
-    HEART,
-    CLUB
+    SPADE("스페이드"),
+    DIAMOND("다이아몬드"),
+    HEART("하트"),
+    CLUB("클로버");
+    
+    private String Typename;
+    
+    Type(String Typename){
+	this.Typename=Typename;
+    }
+    
+    public String getTypeName() {
+	return Typename;
+    }
 }
