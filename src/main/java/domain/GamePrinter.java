35c28739bb9ac3a87b3fb182e20d1158412801b2
moonyoungCHAE <xpf_fl@naver.com>
2019-12-14 21:24:15 +0900

    플레이어 이름 입력에 대한 예외처리(아무 이름도 입력하지 않은 경우, 쉼표 뒤에 이름을 입력하지 않은 경우)

--- src/main/java/domain/GamePrinter.java
+++ src/main/java/domain/GamePrinter.java
@@ -8,14 +8,26 @@ import java.util.Scanner;
 public class GamePrinter {
     Scanner scanner = new Scanner(System.in);
 
-    void getPlayerNameFromUser(){
-        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리");
-        String playerNameFromUser = scanner.nextLine();
+    void getPlayerNameFromUser() {
+        String playerNameFromUser;
+        do {
+            System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+            playerNameFromUser = scanner.nextLine();
+        } while (!checkPlayerName(playerNameFromUser));
         String[] splitedPlyerName = splitPlayerName(playerNameFromUser);
     }
 
     String[] splitPlayerName(String playerNameFromUser) {
         String[] splitedPlayerName = playerNameFromUser.split(",");
         return splitedPlayerName;
+    }
+
+    Boolean checkPlayerName(String playerNameFromUser) {
+        if (playerNameFromUser.length() == 0
+                || playerNameFromUser.charAt(playerNameFromUser.length() - 1) == ',') {
+            System.out.println("이름을 잘못 입력했습니다.");
+            return false;
+        }
+        return true;
     }
 }
