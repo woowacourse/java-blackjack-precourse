02e015eeb0db992fa2369ec30802c1cbeb85af2e
lalize <printf@kakao.com>
2019-12-14 16:18:49 +0900

    refactor: gamer -> user 변수 이름 수정

--- src/main/java/domain/rule/Rule.java
+++ src/main/java/domain/rule/Rule.java
@@ -6,7 +6,7 @@ public class Rule {
 	private static final int FIRST_DRAW_CARD_SIZE = 2;
 	private static final int BEST_SCORE = 21;
 
-	public static boolean isBlackjack(User gamer) {
-		return gamer.isCardSize(FIRST_DRAW_CARD_SIZE) && gamer.isScore(BEST_SCORE);
+	public static boolean isBlackjack(User user) {
+		return user.isCardSize(FIRST_DRAW_CARD_SIZE) && user.isScore(BEST_SCORE);
 	}
 }
