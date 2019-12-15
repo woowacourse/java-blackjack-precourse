31863acdc7e200b04dc6bf585a077c24294de780
Won Chan Oh <woncoh1@gmail.com>
2019-12-15 13:57:09 +0900

    [추가] List<Player>를 감싸는 Players 일급 컬렉션

---
+++ src/main/java/domain/user/Players.java
@@ -0,0 +1,11 @@
+package domain.user;
+
+import java.util.*;
+
+public class Players {
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        this.players = players;
+    }
+}
