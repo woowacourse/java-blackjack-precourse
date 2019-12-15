0478e16e6290577aa7a275a7fb28b0c6bc149568
minuyim <minu_yim@naver.com>
2019-12-16 00:13:59 +0900

    feat : NameInput 기능 추가

---
+++ src/main/java/utils/NameInput.java
@@ -0,0 +1,49 @@
+package utils;
+
+public class NameInput {
+	private static final int MIN_LENGTH = 0;
+	private static final int MAX_LENGTH = 7;
+	private String[] names;
+	
+	public NameInput() {
+		System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+		input();
+	}
+	
+	public String[] getNames() {
+		return this.names;
+	}
+	
+	private void input() {
+		String[] output = UserInput.input().split("," , -1);
+		
+		try {
+			validateNamesLength(output);
+			validateLength(output);
+		} catch (IllegalArgumentException e) {
+			input();
+		}
+		
+		this.names = output;
+	}
+	
+	private void validateNamesLength(String[] strings) {
+		for (String string : strings) {
+			validateNameLength(string);
+		}
+	}
+	
+	private void validateNameLength(String string) {
+		if (string.length() <= MIN_LENGTH) {
+			System.out.println("이름을 " + MIN_LENGTH + "자 보다 많이 입력하세요.");
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	private void validateLength(String[] strings) {
+		if (strings.length > MAX_LENGTH) {
+			System.out.println("최대 인원 수는 " + MAX_LENGTH + "명입니다. 다시 입력하세요.");
+			throw new IllegalArgumentException();
+		}
+	}
+}
---
+++ src/main/java/utils/UserInput.java
@@ -0,0 +1,12 @@
+package utils;
+
+import java.util.Scanner;
+
+public class UserInput {
+	public static String input() {
+		Scanner scanner = new Scanner(System.in);
+		String string = scanner.nextLine();
+		
+		return string;
+	}
+}
