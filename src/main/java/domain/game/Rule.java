49f307bfbcd39cc4e2d1b5acc737a0ae0d90b4b1
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-15 20:23:20 +0900

    style: 띄어쓰기 수정

--- src/main/java/domain/game/Rule.java
+++ src/main/java/domain/game/Rule.java
@@ -1,8 +1,9 @@
 package domain.game;
 
 public class Rule {
-	static private final int BASIC_DRAW = 2;
-	static private final int BLACKJACK_POINT = 21;
+	static final int BASIC_DRAW = 2;
+	static final int BLACKJACK_POINT = 21;
+	static final int DEALER_DRAW_POINT = 17;
 	private static final String OUT_OF_CARDS_MESSAGE = "카드가 부족합니다.";
 
 	public static int getBasicDraw() {
@@ -11,6 +12,10 @@ public class Rule {
 
 	public static int getBlackjackPoint() {
 		return BLACKJACK_POINT;
+	}
+
+	public static int getDealerDrawPoint() {
+		return DEALER_DRAW_POINT;
 	}
 
 	public static String getOutOfCardsMessage() {
--- src/main/java/domain/user/PlayerFactory.java
+++ src/main/java/domain/user/PlayerFactory.java
@@ -5,11 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class PlayerFactory {
-	public static List<Gambler> create(List<String>names,List<Double>bettings){
-		List<Gambler> players=new ArrayList<>();
-		for(int i=0;i<names.size();i++){
-			players.add(new Player(names.get(i),bettings.get(i)));
+	public static List<Gambler> create(List<String> names, List<Double> bettings) {
+		List<Gambler> players = new ArrayList<>();
+		for (int i = 0; i < names.size(); i++) {
+			players.add(new Player(names.get(i), bettings.get(i)));
 		}
-		return Collections.unmodifiableList(players);
+		return players;
 	}
 }
