18191dbed4580b9dcc90b881d1bf61c405d52d45
lalize <printf@kakao.com>
2019-12-16 01:54:51 +0900

    refactor: Rule 클래스 상수 FIRST_DRAW_CARD_SIZE -> BLACKJACK_CARD_SIZE 이름 수정

--- src/main/java/domain/rule/Rule.java
+++ src/main/java/domain/rule/Rule.java
@@ -3,12 +3,12 @@ package domain.rule;
 import domain.user.User;
 
 public class Rule {
-	private static final int FIRST_DRAW_CARD_SIZE = 2;
+	private static final int BLACKJACK_CARD_SIZE = 2;
 	private static final int BEST_SCORE = 21;
 	private static final int DEALER_STAY_SCORE = 17;
 
 	public static boolean isBlackjack(User user) {
-		return user.isCardSize(FIRST_DRAW_CARD_SIZE) && user.isScore(BEST_SCORE);
+		return user.isCardSize(BLACKJACK_CARD_SIZE) && user.isScore(BEST_SCORE);
 	}
 
 	private static boolean isBust(User user) {
