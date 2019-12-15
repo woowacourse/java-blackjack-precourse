fd013ad411cc236903738238b2e617c1ac9b5bed
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-15 15:17:19 +0900

    feat: 패의 합을 계산하는 동작 2가지 추가 (Ace: 1, 11)

--- src/main/java/domain/user/Gambler.java
+++ src/main/java/domain/user/Gambler.java
@@ -1,4 +1,34 @@
 package domain.user;
 
-public class Gambler {
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import domain.card.Card;
+
+public abstract class Gambler {
+	private List<Card> cards;
+
+	public abstract void addCard(Card card);
+
+	public int sumCardsMin() {
+		List<Integer> cardScores = cards.stream()
+			.flatMapToInt(card -> card.getSymbol().getScore())
+			.sorted().collect(Collectors.toList());
+		int sum = cardScores.stream().reduce(0, Integer::sum);
+
+		return sum;
+	}
+
+	public int sumCardsMax() {
+		int sum = sumCardsMin();
+		List<Integer> aces = cards.stream()
+			.flatMapToInt(card -> card.getSymbol().getScore())
+			.filter(score -> (score == 1))
+			.collect(Collectors.toList());
+		for (Iterator<Integer> iterator = aces.iterator(); sum < 21 && iterator.hasNext(); ) {
+			sum += 10;
+		}
+		return sum;
+	}
 }
