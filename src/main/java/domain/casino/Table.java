5079bd4ae7f63db258cf6b23fef892d4bcabcefb
KongsonPark <sonypark0204+wootech@gmail.com>
2019-12-15 16:09:41 +0900

    [feat] Table 클래스: 딜러와 플레이어에게 카드 지급하는 로직 구현
    - Table 클래스에 적용 후 테스트

--- src/main/java/domain/casino/Table.java
+++ src/main/java/domain/casino/Table.java
@@ -3,14 +3,23 @@ package domain.casino;
 import java.util.ArrayList;
 import java.util.List;
 
+import domain.card.Card;
+import domain.card.CardFactory;
+import domain.card.CardSupplier;
+import domain.user.Dealer;
 import domain.user.Player;
 import domain.view.InputView;
 
 public class Table {
+	private static List<Card> cards = CardFactory.create();
 	private List<Player> playerList;
+	private Dealer dealer = new Dealer();
+	private CardSupplier cardSupplier = new CardSupplier(cards);
+
 
 	public void playBlackjack() {
 		getPlayers();
+		distributeCards();
 	}
 
 	// 플레이어 객체 생성 : 배팅 금액, 이름 등록
@@ -22,6 +31,24 @@ public class Table {
 			bettigMoney = InputView.getBettingMoney(playerNames[i]);
 			Player player = new Player(playerNames[i], bettigMoney);
 			playerList.add(player);
+		}
+	}
+
+	// 카드 지급 : 딜러, 플레이어
+	private void distributeCards() {
+		for (int i = 0; i < 2; i++) {
+			distributeCardsToDealer();
+			distributeCardsToPlayers();
+		}
+	}
+
+	private void distributeCardsToDealer() {
+		dealer.addCard(cardSupplier.getDeal());
+	}
+
+	private void distributeCardsToPlayers() {
+		for (Player player : playerList) {
+			player.addCard(cardSupplier.getDeal());
 		}
 	}
 
