7ba8826e68fbec31e1d4a626f9db53ebcd7d74ec
barrett24 <barrett2440@gmail.com>
2019-12-16 00:53:50 +0900

    Update - Player의 카드 점수계산, 버스트 또는 블랙잭 판정 기능

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -1,14 +1,22 @@
 package domain.user;
 
 import domain.card.Card;
+import domain.card.CardsOnGame;
 
+import java.lang.management.BufferPoolMXBean;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 게임 참여자를 의미하는 객체
  */
 public class Player {
+    private static final int ACE_CARD_BONUS_SCORE = 10;
+    private static final int BLACK_JACK = 21;
+    private static final int BURST = 22;
+    private static final int AFTER_BURST_SCORE = 0;
+
     private final String name;
     private final double bettingMoney;
     private final List<Card> cards = new ArrayList<>();
@@ -22,6 +30,38 @@ public class Player {
         cards.add(card);
     }
 
-    // TODO 추가 기능 구현
+    public String getName() {
+        return name;
+    }
 
+    public double getBettingMoney() {
+        return bettingMoney;
+    }
+
+    public int getCountedScore() {
+        return cards.stream()
+                .map(Card::getScore)
+                .reduce(Integer::sum)
+                .filter(x -> isBurst(x))
+                .orElse(AFTER_BURST_SCORE);
+    }
+
+    public int getCountedScoreWithAceBonus() {
+        return Optional.of(getCountedScore() + ACE_CARD_BONUS_SCORE)
+                .filter(x -> isPlayerHaveAce())
+                .filter(x -> isBurst(x))
+                .orElse(getCountedScore());
+    }
+
+    public boolean isPlayerHaveAce() {
+        return cards.stream().anyMatch(Card::checkAce);
+    }
+
+    public boolean isBurst(int score) {
+        return  score < BURST;
+    }
+
+    public boolean isBlackJack() {
+        return cards.size() == 2 && getCountedScoreWithAceBonus() == BLACK_JACK;
+    }
 }
