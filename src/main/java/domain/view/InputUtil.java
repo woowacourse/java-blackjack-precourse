3b5d03221071b8998952dac19fda3a3fa1d050d0
Tak Hyungmin <milkho11@naver.com>
2019-12-15 22:05:21 +0900

    기능: 사용자의 카드 더받기 입력 받기
    InputUtil에 위 기능 추가

--- src/main/java/domain/view/InputUtil.java
+++ src/main/java/domain/view/InputUtil.java
@@ -10,6 +10,9 @@ public class InputUtil {
     static final String EMPTY_SPACE = "";
     static final String ONE = "1";
     static final String ELEVEN = "11";
+    // ENUM 활용해서 리팩토링해보기
+    static final String YES = "y";
+    static final String NO = "n";
 
     public static String[] inputName() {
         return SCANNER.nextLine().replace(SPACE, EMPTY_SPACE).split(COMMA);
@@ -27,7 +30,17 @@ public class InputUtil {
         if (inputAceUse.equals(ELEVEN)) {
             return Integer.parseInt(ELEVEN);
         }
+        throw new InputMismatchException("1, 11 중에 입력하셨어야죠! 판 엎습니다!");
+    }
 
-        throw new InputMismatchException("잘못된 값을 입력했어요!");
+    public static boolean inputAddCardQuestion() {
+        String inputAddCardQuestion = SCANNER.nextLine().trim();
+        if(inputAddCardQuestion.equals(YES)){
+            return true;
+        }
+        if(inputAddCardQuestion.equals(NO)){
+            return false;
+        }
+        throw new InputMismatchException("y, n 중에 입력하셨어야죠! 판 엎습니다!");
     }
 }
