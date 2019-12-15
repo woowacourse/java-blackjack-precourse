979522ebc742c878f9ef12cac725c7f67ec73c95
김예림 <kiel0103@naver.com>
2019-12-16 04:42:39 +0900

    게임참여자 이름 입력받기 기능 작성

--- src/main/java/domain/ui/UserInterfaceMachine.java
+++ src/main/java/domain/ui/UserInterfaceMachine.java
@@ -1,18 +1,45 @@
 package domain.ui;
 
 import domain.user.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class UserInterfaceMachine {
     private Scanner scanner = new Scanner(System.in);
 
-    public String[] scanNames() {
-        return null;
+    public List<String> scanNames() {
+        final String StringGetNamesHeader = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+        ArrayList<String> names;
+
+        System.out.println(StringGetNamesHeader);
+        String input=scanner.nextLine();
+        names = new ArrayList<String>(Arrays.asList(input.split(",")));
+
+        while (!isCorrectNames(names)) {
+            System.out.println(names);
+            printNameError();
+            System.out.println(StringGetNamesHeader);
+            names = new ArrayList<String>(Arrays.asList(scanner.nextLine().split(",")));
+        }
+
+        return names;
+    }
+
+    private boolean isCorrectNames(List<String> names) {
+        return true;
     }
 
     private boolean isCorrectName() {
         return true;
+    }
+
+    private void printNameError () {
+        System.out.println("잘못된 이름 입력입니다.");
+        System.out.println(" * 이름에는 특수문자를 사용할 수 없습니다.");
+        System.out.println(" * 이름과 이름 사이를 \',\'로 분리해주세요.");
+        System.out.println(" * ex)YelimKim,JongSeongLee,Rebeca");
     }
 
     public int scanBetAmountOfPlayer(String playerName) {
