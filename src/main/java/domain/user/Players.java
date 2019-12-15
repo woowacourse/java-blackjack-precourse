5ded37d89d534293618eb3cb9d7a9c868b67b2e2
Won Chan Oh <woncoh1@gmail.com>
2019-12-15 17:34:38 +0900

    [추가] Players에 카드들 추가하는 기능

--- src/main/java/domain/user/Players.java
+++ src/main/java/domain/user/Players.java
@@ -1,6 +1,8 @@
 package domain.user;
 
-import java.util.*;
+import domain.card.Deck;
+
+import java.util.List;
 
 public class Players {
     private static final int MINIMUM_NUMBER_OF_PLAYERS = 1;
@@ -15,6 +17,12 @@ public class Players {
     private void validateSize(List<Player> players) {
         if (players.size() < MINIMUM_NUMBER_OF_PLAYERS) {
             throw new IllegalArgumentException("플레이어는 최소 1명 있어야 합니다.");
+        }
+    }
+
+    public void addCards(Deck deck) {
+        for (Player player : this.players) {
+            player.addCard(deck.pop());
         }
     }
 }
