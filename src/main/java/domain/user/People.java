d94c9d615dda2cab021313b66a0dcf5fea7537f3
HongBin kim <fusis1@naver.com>
2019-12-15 19:39:34 +0900

    add Poeple

---
+++ src/main/java/domain/user/People.java
@@ -0,0 +1,22 @@
+package domain.user;
+
+import java.util.ArrayList;
+import java.util.List;
+import domain.card.Card;
+
+public class People {
+    private final List<Card> cards = new ArrayList<>();
+
+    public People() { }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int getTotalNumber(){
+        int result = 0;
+        for(Card card : cards) {
+        }
+        return 1;
+    }
+}
