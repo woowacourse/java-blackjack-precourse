9364d53d24769c20370f50632f2d90f556f0281f
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-15 15:38:41 +0900

    refactor: 진입지점 정리

--- src/main/java/BlackJackGame.java
+++ src/main/java/BlackJackGame.java
@@ -1,35 +1,10 @@
 import domain.game.BlackJack;
 
 public class BlackJackGame {
-	private static BlackJack blackJack;
+	private BlackJack blackJack;
 
 	public static void main(String[] args) {
-		BlackJackGame blackJackGame = new BlackJackGame();
-		blackJack = blackJackGame.setUp();
-
-		if (blackJackGame.firstRound()) {
-			blackJackGame.finish();
-			return;
-		}
-
-		blackJackGame.secondRound();
-		blackJackGame.finish();
-	}
-
-	private BlackJack setUp() {
-		BlackJack blackJack = new BlackJack();
-		blackJack.setUp();
-		return blackJack;
-	}
-
-	private boolean firstRound() {
-		// blackJack
-	}
-
-	private void secondRound(){
-
-	}
-	private void finish(){
-		
+		BlackJack blackJack=new BlackJack();
+		blackJack.run();
 	}
 }
--- src/main/java/domain/game/BlackJack.java
+++ src/main/java/domain/game/BlackJack.java
@@ -2,14 +2,52 @@ package domain.game;
 
 import java.util.List;
 
-import domain.user.Gambler;
+import controller.InputController;
+import view.InputView;
 
 public class BlackJack {
-	public void run(){
-		PlayerPhase playerPhase =new PlayerPhase();
-		List<Gambler> gamblerList=playerPhase.gatherPlayers();
+	private InputController inputController;
+	private InputView inputView;
+	private Match match;
+	private boolean firstDone;
 
-		CasinoPhase casinoPhase=new CasinoPhase();
+	public BlackJack() {
+		inputView = new InputView();
+		inputController = new InputController(inputView);
+	}
+
+	public void run() {
+		setUp();
+
+		firstRound();
+		if (firstDone) {
+			finish();
+			return;
+		}
+
+		secondRound();
+		finish();
+	}
+
+	private void setUp() {
+		List<String> playerNames = inputController.getPlayerNames();
+		List<Double> bettings = inputController.getBettings(playerNames);
+		match = new Match(playerNames, bettings);
+	}
+
+	private void firstRound() {
+		match.drawAll(Rule.getBasicDraw());
+		match.findBlackJackWinner(Rule.getBlackjackPoint());
+		if (match.hasWinners()) {
+			firstDone = true;
+		}
+	}
+
+	private void secondRound() {
 		
+	}
+
+	private void finish() {
+		if()
 	}
 }
