15687af3fc9530db02aa6ec2539c639a44e16b20
fucct KIM <dqrd123@gmail.com>
2019-12-16 08:57:19 +0900

    Dealer, Player class 의 super class 인 User class 생성

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,19 +1,25 @@
 package domain;
 
+import domain.user.Dealer;
 import domain.user.Player;
+import domain.user.User;
 
 import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args){
-        Player[] players;
+        User[] players;
+        User dealer = new Dealer();
         players = makePlayer();
+        for(User sex : players){
+            System.out.println(sex);
+        }
     }
     public static Player[] makePlayer(){
         Scanner s = new Scanner(System.in);
         String userInput;
         String playerNames[];
-        System.out.print("게임에 참여할 사람의 이름을 입력하세요." +
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요." +
                 "(쉼표 기준으로 분리)");
         userInput = s.nextLine();
         if(!checkUserInput(userInput)){
--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -8,14 +8,8 @@ import java.util.List;
 /**
  * 게임 딜러를 의미하는 객체
  */
-public class Dealer {
-    private final List<Card> cards = new ArrayList<>();
-
+public class Dealer extends User{
     public Dealer() {}
-
-    public void addCard(Card card) {
-        cards.add(card);
-    }
 
     // TODO 추가 기능 구현
 }
