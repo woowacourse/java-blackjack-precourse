e844e444dd3b90f5c73f6d565c8bfb56ac9d52bc
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-12-15 17:43:27 +0900

    refactor: 예외 메시지 문자열 하드코딩 수정

--- src/main/java/com/precourse/blackjack/domain/util/InputUtil.java
+++ src/main/java/com/precourse/blackjack/domain/util/InputUtil.java
@@ -24,6 +24,10 @@ public class InputUtil {
 	private static final String SPACE = " ";
 	private static final String EMPTY = "";
 	private static final String COMMA = ",";
+	private static final String PLAYER_NAME_BLANK = "Player name is blank";
+	private static final String PLAYER_NAME_DUPLICATION = "Player name is duplicate";
+	private static final String PLAYER_WRONG_COUNT = "The number of players is wrong";
+	private static final String BETTING_WRONG_MONEY = "Betting money is less than minimum";
 	private static final int SPLIT_LIMIT = -1;
 	private static final int MINIMUM_PLAYER_NUMBER = 1;
 	private static final int MAXIMUM_PLAYER_NUMBER = 7;
@@ -52,19 +56,19 @@ public class InputUtil {
 
 	private static void checkNameEmpty(List<String> playersName) throws IOException {
 		if (playersName.contains(EMPTY)) {
-			throw new IOException("Player's name is empty");
+			throw new IOException(PLAYER_NAME_BLANK);
 		}
 	}
 
 	private static void checkNameDuplicate(List<String> playersName) throws IOException {
 		if (playersName.stream().distinct().count() != playersName.size()) {
-			throw new IOException("Player's name is duplicated");
+			throw new IOException(PLAYER_NAME_DUPLICATION);
 		}
 	}
 
 	private static void checkNameCount(List<String> playersName) throws IOException {
 		if ((playersName.size() < MINIMUM_PLAYER_NUMBER) || (playersName.size() > MAXIMUM_PLAYER_NUMBER)) {
-			throw new IOException("The number of players is wrong");
+			throw new IOException(PLAYER_WRONG_COUNT);
 		}
 	}
 
@@ -82,7 +86,7 @@ public class InputUtil {
 
 	private static void checkMinimumBettingMoney(double playerBettingMoney) throws IOException {
 		if (playerBettingMoney < MINIMUM_BETTING_MONEY) {
-			throw new IOException("Betting money is less than minimum");
+			throw new IOException(BETTING_WRONG_MONEY);
 		}
 	}
 }
