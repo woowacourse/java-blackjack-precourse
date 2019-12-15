77ca8e2c83243e2d36226ef67c79ac1db3368030
begaonnuri <begaonnuri@gmail.com>
2019-12-15 18:25:10 +0900

    feat: 플레이어를 추가하는 기능 구현

--- src/main/java/system/BlackjackSystem.java
+++ src/main/java/system/BlackjackSystem.java
@@ -1,10 +1,15 @@
 package system;
 
+import domain.user.Player;
 import view.InputView;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BlackjackSystem {
+    private List<Player> playerList = new LinkedList<>();
+
     public void run() {
         setGame();
     }
@@ -12,9 +17,22 @@ public class BlackjackSystem {
     private void setGame() {
         String names = InputView.inputPlayerName();
         StringTokenizer st = splitPlayerName(names);
+        setPlayer(st);
     }
 
     private StringTokenizer splitPlayerName(String names) {
         return new StringTokenizer(names, ",");
+    }
+
+    private void setPlayer(StringTokenizer st) {
+        while (st.hasMoreTokens()) {
+            String playerName = st.nextToken();
+            double bettingMoney = InputView.inputPlayerMoney(playerName);
+            addPlayer(new Player(playerName, bettingMoney));
+        }
+    }
+
+    private void addPlayer(Player player) {
+        playerList.add(player);
     }
 }
