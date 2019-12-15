5f9c6f95e4969562e75fb59503733ab6ce52bc83
KongsonPark <sonypark0204+wootech@gmail.com>
2019-12-15 15:45:01 +0900

    [feat] 게임에 참여할 플레이어 입력 로직 구현
    - inputPlayerNames(): 쉼표 기준으로 게임 참가자 이름을 입력받는 함수
    - isNull(): 입력값이 null 인지 판별하는 함수
    - isInvalidInput(): 유효하지 않은 입력값 판별하는 함수

---
+++ src/main/java/domain/view/InputView.java
@@ -0,0 +1,36 @@
+package domain.view;
+
+import java.util.Scanner;
+
+public class InputView {
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static String[] inputPlayerNames() {
+		String[] playerNames;
+
+		do {
+			System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+			String input = scanner.nextLine().trim();
+			playerNames = input.split(",");
+		} while (isNull(playerNames) || isInvalidInput(playerNames));
+		return playerNames;
+	}
+
+	private static boolean isNull(String[] players) {
+		for (String player : players) {
+			if (player.isEmpty()) {
+				System.out.println("한 명 이상의 플레이어를 입력해주세요.");
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static boolean isInvalidInput(String[] players) {
+		if (players.length == 0) {
+			System.out.println("입력값 형식이 잘못 되었습니다.");
+			return true;
+		}
+		return false;
+	}
+}
