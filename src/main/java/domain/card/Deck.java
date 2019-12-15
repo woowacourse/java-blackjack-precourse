3c2287d440a5eb4f837c4f4720ab554a417b759c
Won Chan Oh <woncoh1@gmail.com>
2019-12-15 16:49:22 +0900

    [추가] 카드 Deck 만들어, 카드 섞고, 제일 윗장의 카드 빼는 기능

---
+++ src/main/java/domain/card/Deck.java
@@ -0,0 +1,25 @@
+package domain.card;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private static List<Card> cards;
+
+    public Deck() {
+        List<Card> cards = new ArrayList<>();
+        for (Card card : CardFactory.create()) {
+            cards.add(card);
+        }
+        this.cards = cards;
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card pop() {
+        return this.cards.remove(0);
+    }
+}
