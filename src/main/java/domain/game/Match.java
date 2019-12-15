d569d74eedbd2350f29be3eb2943c545263686c9
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-15 15:39:53 +0900

    feat: Match 클래스에 패를 뽑는 기능 추가

--- src/main/java/domain/game/Match.java
+++ src/main/java/domain/game/Match.java
@@ -1,24 +1,59 @@
 package domain.game;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import domain.card.Deck;
 import domain.user.Dealer;
 import domain.user.Gambler;
-import domain.user.Player;
 import domain.user.PlayerFactory;
 
 public class Match {
 	private Deck deck;
 	private List<Gambler> playerList;
 	private Gambler dealer;
-	private Rule rule;
+	private List<Gambler> winnerList;
 
-	public Match(List<String>players, List<Double>bettings){
-		playerList= PlayerFactory.create(players,bettings);
-		dealer=new Dealer();
-		deck=new Deck();
+	public Match(List<String> players, List<Double> bettings) {
+		playerList = PlayerFactory.create(players, bettings);
+		dealer = new Dealer();
+		winnerList = new ArrayList<>();
+		deck = new Deck();
 		deck.shuffle();
-		rule=new Rule();
+	}
+
+	public void drawAll(int count) {
+		try {
+			drawCards(Arrays.asList(dealer), count);
+			drawCards(playerList, count);
+		} catch (Exception e) {
+			System.out.println(Rule.getOutOfCardsMessage());
+		}
+	}
+
+	private void drawCards(List<Gambler> gamblers, int count) throws Exception {
+		for (Gambler gambler : gamblers
+		) {
+			drawCard(gambler, count);
+		}
+		return;
+	}
+
+	private void drawCard(Gambler gambler, int count) throws Exception {
+		for (int i = 0; i < count; i++) {
+			gambler.addCard(deck.draw());
+		}
+	}
+
+	public void findBlackJackWinner(int blackjackPoint) {
+		winnerList = playerList.stream()
+			.filter(player -> (player.sumCardsMax() == blackjackPoint))
+			.collect(Collectors.toList());
+	}
+
+	public boolean hasWinners() {
+		return winnerList.size() > 0;
 	}
 }
