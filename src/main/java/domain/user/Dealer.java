d79ac097dc1dbe23208eae4737bd76952d194f0a
김효찬 <kom1230@hanyang.ac.kr>
2019-12-16 00:55:15 +0900

    [ADD] Dealer 클래스 안에 점수가 각 기준을 넘었는지 확인하는 메소드 추가

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -3,19 +3,49 @@ package domain.user;
 import domain.card.Card;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import static domain.game.BlackJackGame.*;
+
 /**
- * 게임 딜러를 의미하는 객체
+ * Dealer
+ *
+ * @author hyochan
+ * @version 0.0.1
+ * @since 2019-12-14
  */
+
 public class Dealer {
     private final List<Card> cards = new ArrayList<>();
 
-    public Dealer() {}
+    public Dealer() {
+    }
 
     public void addCard(Card card) {
         cards.add(card);
     }
 
-    // TODO 추가 기능 구현
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public boolean isBlackJack() {
+        if (getCardScore() == MAX_Score && getCards().size() == 2) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isUnderMaxScore() {
+        return MAX_Score >= getCardScore();
+    }
+
+    public boolean isOverMaxScore() {
+        return MAX_Score < getCardScore();
+    }
+
+    public boolean isUnderDealerMinScore() {
+        return DEALER_MIN_SCORE > getCardScore();
+    }
 }
