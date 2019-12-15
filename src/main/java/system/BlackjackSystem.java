0907083879f5d5fccfc59d31eec6464459c4cc75
begaonnuri <begaonnuri@gmail.com>
2019-12-15 17:40:54 +0900

    feat: 입력받은 문자열을 쉼표 기준으로 분리하는 기능 구현

--- src/main/java/system/BlackjackSystem.java
+++ src/main/java/system/BlackjackSystem.java
@@ -1,6 +1,20 @@
 package system;
 
+import view.InputView;
+
+import java.util.StringTokenizer;
+
 public class BlackjackSystem {
     public void run() {
+        setGame();
+    }
+
+    private void setGame() {
+        String names = InputView.inputPlayerName();
+        StringTokenizer st = splitPlayerName(names);
+    }
+
+    private StringTokenizer splitPlayerName(String names) {
+        return new StringTokenizer(names, ",");
     }
 }
