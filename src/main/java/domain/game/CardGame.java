5cf933ef3402d54e4ab925f52484b5a0ff5fa4e4
aegis1920 <aegis1920@gmail.com>
2019-12-15 23:30:28 +0900

    [docs] fix : 이름을 입력받는 메소드, [feat] add : 쉼표로 분리하는 메소드, 공백 삭제 메소드, 공백 체크 메소드

--- src/main/java/domain/game/CardGame.java
+++ src/main/java/domain/game/CardGame.java
@@ -1,20 +1,53 @@
 package domain.game;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class CardGame {
 
-    public static void run(){
-        Scanner sc = new Scanner(System.in);
+    // 후에 rafactoring
+    static Scanner sc = new Scanner(System.in);
+
+    public static void insertPlayerNameStr(){
 
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        String playerStr = sc.nextLine();
 
-        //
-        String joinPlayerStr = sc.nextLine();
+        String[] playerNameArr = dividePlayerNameStrToRest(playerStr);
+        List<String> playerNameList = deleteSpacePlayerNameArr(playerNameArr);
 
-        String[] joinPlayerNameArr = joinPlayerStr.split(",");
-        int[] joinPlyaerMoneyArr = new int[joinPlayerNameArr.length];
+        while(!checkPlayerNameList(playerNameList)){
+            System.out.println("이름에 공백이 있습니다! 다시 입력해주세요");
+            playerStr = sc.nextLine();
+            playerNameArr = dividePlayerNameStrToRest(playerStr);
+            playerNameList = deleteSpacePlayerNameArr(playerNameArr);
+        }
 
+        System.out.println(playerNameList.toString());
+
+    }
+
+    public static String[] dividePlayerNameStrToRest(String playerStr){
+        String[] playerNameArr = playerStr.split(",");
+        return playerNameArr;
+    }
+
+    public static List<String> deleteSpacePlayerNameArr(String[] playerNameArr){
+        List<String> playerNameList = Arrays.asList(playerNameArr).stream()
+                .map(element -> element.replace(" ", ""))
+                .collect(Collectors.toList());
+
+        return playerNameList;
+    }
+
+    public static boolean checkPlayerNameList(List<String> playerNameList){
+        return playerNameList.stream().allMatch(element -> !element.equals(""));
+    }
+
+    public static void run(){
+        insertPlayerNameStr();
     }
 
 }
