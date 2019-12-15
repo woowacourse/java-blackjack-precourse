d73da5c565285027d96cb157d4622083000b7fd0
KongsonPark <sonypark0204+wootech@gmail.com>
2019-12-15 16:34:38 +0900

    [feat] Palyer 블랙잭 판별 로직 구현
    - Delear에서 구현한 것과 동일
    - 추후에 공통 로직으로 묶을 예정

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -1,14 +1,9 @@
 package domain.user;
 
 import domain.card.Card;
-import domain.card.CardSupplier;
-import domain.card.KoreanSymbol;
 import domain.card.Symbol;
-import domain.view.InputView;
-import domain.view.OutputView;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -42,4 +37,49 @@ public class Player {
 		return cards;
 	}
 
+	public boolean bust() {
+		if (sumCardScore() > 21) {
+			return true;
+		}
+		return false;
+	}
+
+	public int sumCardScore() {
+		if (containAce()) {
+			int sum_ace_eleven = sumScore() + 10;
+			if (sum_ace_eleven <= 21) {
+				return sum_ace_eleven;
+			}
+		}
+		return sumScore();
+	}
+
+	private int sumScore() {
+		int sum = 0;
+
+		for (Card card : cards) {
+			sum += card.getScore();
+		}
+		return sum;
+	}
+
+	public boolean isBlackJack() {
+		if (cards.size() == 2 && containAce() && sumCardScore() == 21) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean containAce() {
+		for (Card card : cards) {
+			if (isAce(card)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean isAce(Card card) {
+		return card.getSymbol().equals(Symbol.ACE);
+	}
 }
