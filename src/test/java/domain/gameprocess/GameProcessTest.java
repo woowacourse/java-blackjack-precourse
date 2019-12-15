37fb5023709e4920f08a873bf77dd0cdf500f809
김예림 <kiel0103@naver.com>
2019-12-16 04:45:44 +0900

    GameProcessTest 코드 수정 : 이름 입력 System.in 대체코드 추가

--- src/test/java/domain/gameprocess/GameProcessTest.java
+++ src/test/java/domain/gameprocess/GameProcessTest.java
@@ -2,12 +2,17 @@ package domain.gameprocess;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class GameProcessTest {
     @Test
     void play() {
+        String input = "yelim,helim";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
         new GameProcess().play();
     }
 }
