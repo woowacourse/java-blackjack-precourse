863bd01e2a78549e137505090ed3f4dfd55cb649
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-15 16:41:29 +0900

    style: 띄어쓰기등 정리

--- src/main/java/domain/card/Deck.java
+++ src/main/java/domain/card/Deck.java
@@ -6,17 +6,18 @@ public class Deck {
 	private List<Card> cards;
 	private DrawSequence drawSequence;
 
-	public Deck(){
-		cards=CardFactory.create();
-		drawSequence =new DrawSequence(cards.size());
+	public Deck() {
+		cards = CardFactory.create();
+		drawSequence = new DrawSequence(cards.size());
+	}
+	public void shuffle(){
 		drawSequence.shuffle();
 	}
-
-	public Card draw() throws Exception{
-		if(drawSequence.hasNext()==false){
+	public Card draw() throws Exception {
+		if (drawSequence.hasNext() == false) {
 			throw new Exception();
 		}
-		int index= drawSequence.next();
+		int index = drawSequence.next();
 		return cards.get(index);
 	}
 }
