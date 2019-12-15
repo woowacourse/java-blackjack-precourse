4e46ca7a11e9beddfbbf37f6f042de6e196fabd8
Tak Hyungmin <milkho11@naver.com>
2019-12-15 15:09:27 +0900

    기능: 플레이어 이름 입력받기
    InputUtil에 이름 입력 기능 추가

---
+++ src/main/java/domain/view/InputUtil.java
@@ -0,0 +1,11 @@
+package domain.view;
+
+import java.util.Scanner;
+
+public class InputUtil {
+    static final Scanner SCANNER = new Scanner(System.in);
+
+    public static String[] inputName() {
+        return SCANNER.nextLine().replace(" ", "").split(",");
+    }
+}
