d27859c4f4e41b0fb4df139f3ae7cf8527fb7d0d
Won Chan Oh <woncoh1@gmail.com>
2019-12-15 14:37:47 +0900

    [추가] Players 클래스 예외처리하는 기능

--- src/main/java/domain/user/Players.java
+++ src/main/java/domain/user/Players.java
@@ -3,9 +3,18 @@ package domain.user;
 import java.util.*;
 
 public class Players {
+    private static final int MINIMUM_NUMBER_OF_PLAYERS = 1;
+
     private final List<Player> players;
 
     public Players(List<Player> players) {
+        validateSize(players);
         this.players = players;
+    }
+
+    private void validateSize(List<Player> players) {
+        if (players.size() < MINIMUM_NUMBER_OF_PLAYERS) {
+            throw new IllegalArgumentException("플레이어는 최소 1명 있어야 합니다.");
+        }
     }
 }
