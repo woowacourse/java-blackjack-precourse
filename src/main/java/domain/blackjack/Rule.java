41b8b0b316bc8b3df6f904601b5fe4e060db6e94
Min-92 <0201.mj.lee@gmail.com>
2019-12-17 04:36:31 +0900

    FEAT : 점수 판정 기능 생성
    - 점수 판정 기능 생성
    - Ace 소지 확인 메소드 생성
    - 21 이상인지 확인하는 메소드 생성

--- src/main/java/domain/blackjack/Rule.java
+++ src/main/java/domain/blackjack/Rule.java
@@ -1,16 +1,20 @@
 package domain.blackjack;
 
 import domain.card.Card;
+import domain.card.CardFactory;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rule {
+
+  private static final List<Card> Aces = new ArrayList<>(CardFactory.createAces());
 
   public static Boolean isBlackJack(List<Card> cards) {
     int firstCardScore = cards.get(0).getScore();
     int secondCardScore = cards.get(1).getScore();
 
-    if ((firstCardScore == 1 && secondCardScore == 10) || (firstCardScore == 10
-        && secondCardScore == 1)) {
+    if ((firstCardScore == 11 && secondCardScore == 10) || (firstCardScore == 10
+        && secondCardScore == 11)) {
       return true;
     }
 
@@ -27,7 +31,39 @@ public class Rule {
     return false;
   }
 
+  public static Boolean isOverTwentyOne(List<Card> cards) {
+    int score = 0;
+
+    for (int i = 0; i < cards.size(); i++) {
+      score += cards.get(i).getScore();
+    }
+
+    if (score > 21) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private static boolean hasAce(List<Card> cards) {
+    if (cards.contains(Aces.get(0)) || cards.contains(Aces.get(1)) || cards.contains(Aces.get(2))
+        || cards.contains(Aces.get(3))) {
+      return true;
+    }
+    return false;
+  }
+
   public static int getScore(List<Card> cards) {
-    return 1;
+    int score = 0;
+
+    for (int i = 0; i < cards.size(); i++) {
+      score += cards.get(i).getScore();
+    }
+
+    if (hasAce(cards) && isOverTwentyOne(cards)) {
+      score -= 10;
+    }
+
+    return score;
   }
 }
