0e260f183b463206c68ca3ce6701bf851cffe8b1
JH <surfing2003@naver.com>
2019-12-17 01:05:38 +0900

    카드 뽑는 과정 수정 중간

---
+++ src/main/java/domain/main/GamePlay.java
@@ -0,0 +1,34 @@
+package domain.main;
+
+import domain.card.Card;
+import domain.card.CardFactory;
+import domain.card.Symbol;
+import domain.card.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GamePlay {
+    private static final int NUMBER_OF_CARDS = 52;
+
+    public static List<Card> cardList = CardFactory.create();
+    public List<Integer> usedCardsIndex = new ArrayList<Integer>();
+    public static int drawCardIndex;
+
+    public static int getNewCardIndex() {
+        return (int) (Math.random() * NUMBER_OF_CARDS);
+    }
+
+    public static boolean checkNewCardIndex(List<Integer> usedCardsIndex, int drawCardsIndex) {
+        return usedCardsIndex.contains(drawCardsIndex);
+    }
+
+    public static void addNewCardIndex(List<Integer> usedCardsIndex,) {
+
+    }
+
+    public static void addNewCardForUser(int drawCardIndex){
+
+    }
+
+}
--- src/main/java/domain/main/Main.java
+++ src/main/java/domain/main/Main.java
@@ -10,16 +10,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Card> aa = CardFactory.create();
-        Card bb = new Card(Symbol.ACE, Type.SPADE);
-        System.out.println(aa.indexOf(bb));
-        System.out.println(aa.size());
-        List<Integer> used_card = new ArrayList<Integer>();
 
-        int drawCard = (int) (Math.random() * 52);
-
-        if (!used_card.contains(drawCard)) {
-            used_card.add(drawCard);
-        }
     }
 }
