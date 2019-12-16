60f62bf0e8e7ab839845da68fe6e27fafe5d2b59
ikswary <ikswary@naver.com>
2019-12-16 16:30:52 +0900

    [CORRECT] getPlayersName, getPlayersBetting 메소드 이름 수정
    메소드가 수행하는 일을 명시적으로 표현하도록 수정

--- src/main/java/services/BlackJackUIService.java
+++ src/main/java/services/BlackJackUIService.java
@@ -5,12 +5,12 @@ import controller.BlackJackController;
 import java.util.List;
 
 public class BlackJackUIService {
-	public static List<String> getPlayersName() {
+	public static List<String> printMessageToGetName() {
 		System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
 		return BlackJackController.getInputOfPlayersName();
 	}
 
-	public static double getPlayersBetting(String player) {
+	public static double printMessageToGetBetting(String player) {
 		System.out.println(player + "의 배팅 금액은?");
 		return BlackJackController.getBetting();
 	}
@@ -21,6 +21,11 @@ public class BlackJackUIService {
 
 	public static void printWrongInputMessage() {
 		System.out.println("양수를 입력해야 합니다");
+	}
+
+	public static void printInputForMoreCard(String player) {
+		System.out.println(player + "는 한장의 카드를 더 받으시겠습니까? y or n");
+
 	}
 }
 
