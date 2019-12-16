47f42e6d4455e257ba751dc93a6dd45a1b7bdfa2
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-16 21:35:49 +0900

    refactor: 상수 public으로 변경

--- src/main/java/domain/game/Rule.java
+++ src/main/java/domain/game/Rule.java
@@ -1,29 +1,11 @@
 package domain.game;
 
 public class Rule {
-	static final int BASIC_DRAW = 2;
-	static final int BLACKJACK_POINT = 21;
-	static final int DEALER_DRAW_POINT = 17;
-	static final int DEALER_OPEN_COUNT = 1;
-	private static final String OUT_OF_CARDS_MESSAGE = "카드가 부족합니다.";
-
-	public static int getBasicDraw() {
-		return BASIC_DRAW;
-	}
-
-	public static int getBlackjackPoint() {
-		return BLACKJACK_POINT;
-	}
-
-	public static int getDealerDrawPoint() {
-		return DEALER_DRAW_POINT;
-	}
-
-	public static String getOutOfCardsMessage() {
-		return OUT_OF_CARDS_MESSAGE;
-	}
-
-	public static int getOpenCount() {
-		return DEALER_OPEN_COUNT;
-	}
+	public static final int BASIC_DRAW = 2;
+	public static final int BLACKJACK_POINT = 21;
+	public static final int DEALER_DRAW_POINT = 16;
+	public static final int DEALER_OPEN_COUNT = 1;
+	public static final String OUT_OF_CARDS_MESSAGE = "카드가 부족합니다.";
+	public static final double RATIO_LOSE =-1;
+	public static final double RATIO_WIN_BLACKJACK =1.5;
 }
