78dd2dedeae4a8763d55e456b1b384fe9bef4368
begaonnuri <begaonnuri@gmail.com>
2019-12-16 23:22:00 +0900

    feat: 딜러에게 두 장의 카드를 지급하는 기능, 플레이어에게 두 장의 카드를 지급하는 기능 구현

--- src/main/java/system/BlackjackSystem.java
+++ src/main/java/system/BlackjackSystem.java
@@ -1,17 +1,27 @@
 package system;
 
+import domain.card.Card;
+import domain.card.CardFactory;
+import domain.user.Dealer;
 import domain.user.Player;
 import view.InputView;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class BlackjackSystem {
-    private List<Player> playerList = new LinkedList<>();
+    private static int CARD_COUNT = 52;
+    private static int DISTRIBUTE_CARD_COUNT = 2;
+
+    private List<Player> playerList = new ArrayList<>();
+    private List<Card> cardList = new ArrayList<>();
+    private Dealer dealer = new Dealer();
+    private int remainCardMount = CARD_COUNT;
 
     public void run() {
         setGame();
+        distributeCard();
     }
 
     private void setGame() {
@@ -36,6 +46,28 @@ public class BlackjackSystem {
         playerList.add(player);
     }
     
+    private void distributeCard() {
+        cardList.addAll(CardFactory.create());
+        for (int i = 0; i < DISTRIBUTE_CARD_COUNT; i++) {
+            giveCard(dealer);
+            giveCard(playerList);
+        }
+    }
+
+    private void giveCard(Dealer dealer) {
+        int randomNumber = getRandomNumber(remainCardMount--);
+        dealer.addCard(cardList.get(randomNumber));
+        cardList.remove(randomNumber);
+    }
+
+    private void giveCard(List<Player> playerList) {
+        for (Player p : playerList) {
+            int randomNumber = getRandomNumber(remainCardMount--);
+            p.addCard(cardList.get(randomNumber));
+            cardList.remove(randomNumber);
+        }
+    }
+
     private int getRandomNumber(int range) {
         return (int) (Math.random() * range);
     }
