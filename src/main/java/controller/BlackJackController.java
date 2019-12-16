c7dd4c3139bd0fb14346de8dc7e3ac522f7290ff
ikswary <ikswary@naver.com>
2019-12-16 14:59:02 +0900

    [IMPROVE] 플레이어 입력과 베팅 금액 입력 예외처리 작성

--- src/main/java/controller/BlackJackController.java
+++ src/main/java/controller/BlackJackController.java
@@ -3,7 +3,7 @@ package controller;
 import domain.card.Card;
 import domain.user.Player;
 import exceptions.EmptyInputException;
-import exceptions.NegativeNumberException;
+import exceptions.WrongNumberException;
 import services.BlackJackUIService;
 import utils.UtilityMethods;
 
@@ -12,23 +12,21 @@ import java.util.Scanner;
 
 public class BlackJackController {
 	public static List<String> getInputOfPlayersName() {
-		String names = null;
-
-		while (names == null) {
-			names = catchEmptyInput();
+		Scanner scanner = new Scanner(System.in);
+		String names = scanner.nextLine();
+		while (names.equals("")) {
+			names = catchEmptyInput(names);
 		}
+		scanner.close();
 		return UtilityMethods.sliceStringToList(names);
 	}
 
-	private static String catchEmptyInput() {
-		Scanner scanner = new Scanner(System.in);
-		String names = scanner.nextLine();
+	private static String catchEmptyInput(String names) {
 		try {
 			validateNames(names);
 		} catch (EmptyInputException e) {
 			BlackJackUIService.printEmptyInputMessage();
 		}
-		scanner.close();
 		return names;
 	}
 
@@ -38,40 +36,41 @@ public class BlackJackController {
 		}
 	}
 
-	public static List<Double> getEachBettings() {
-	}
-
-	public static double getBetting(String player) {
-		System.out.println(player + "의 배팅 금액은?");
+	public static double getBetting() {
 		Scanner scanner = new Scanner(System.in);
 		double betting = scanner.nextDouble();
-		try {
-			validateBetting(betting);
-		} catch (NegativeNumberException e) {
-			System.out.println("양수를 입력해야 합니다");
+		while (betting <= 0){
+			catchWrongInput(betting);
 		}
 		scanner.close();
 		return betting;
 	}
 
+	private static double catchWrongInput(double betting) {
+		try {
+			validateBetting(betting);
+		} catch (EmptyInputException e) {
+			BlackJackUIService.printEmptyInputMessage();
+		}
+		return betting;
+	}
 
-
-	private static void validateBetting(double betting) throws NegativeNumberException {
-		if (betting < 0) {
-			throw new NegativeNumberException();
+	private static void validateBetting(double betting) throws WrongNumberException {
+		if (betting <= 0) {
+			throw new WrongNumberException();
 		}
 	}
 
+	public static Card popRandomCard(List<Card> cards) {
+		int popIndex = UtilityMethods.generateRandomNumber(cards.size());
+		return cards.remove(popIndex);
+	}
+	/*
 	private void drawFromCardDeck() {
 		for (Player player : this.players) {
 			player.addCard(popRandomCard(this.cardDeck));
 		}
 		dealer.addCard(popRandomCard(this.cardDeck));
 	}
-
-
-	public Card popRandomCard(List<Card> cards) {
-		int popIndex = UtilityMethods.generateRandomNumber(cards.size());
-		return cards.remove(popIndex);
-	}
+	*/
 }
