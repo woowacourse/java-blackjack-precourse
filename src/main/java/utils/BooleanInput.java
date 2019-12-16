132da7c63392020ffe98cdcd4e8b85d9993df411
minuyim <minu_yim@naver.com>
2019-12-16 20:59:35 +0900

    feat : 추가 카드 지급 여부를 묻는 BooleanInput 기능 추가

---
+++ src/main/java/utils/BooleanInput.java
@@ -0,0 +1,39 @@
+package utils;
+
+public class BooleanInput {
+	private static final String AGREE_CHAR = "y";
+	private static final String DISAGREE_CHAR = "n";
+	private boolean agree;
+	
+	public BooleanInput(String string) {
+		System.out.println(string + "님은 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+		input();
+	}
+	
+	public boolean getAgree() {
+		return this.agree;
+	}
+
+	private void input() {
+		String agreeString = UserInput.input();
+		
+		try {
+			validate(agreeString);
+			this.agree = stringtobool(agreeString);
+		} catch (IllegalArgumentException e) {
+			System.out.println("올바른 문자를 입력하세요.");
+			input();
+		}
+	}
+	
+	private boolean stringtobool(String string) {
+		return string.equals(AGREE_CHAR);
+	}
+	
+	private void validate(String agreeString) {
+		if (!agreeString.equals(AGREE_CHAR)  
+				&& !agreeString.equals(DISAGREE_CHAR)) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
