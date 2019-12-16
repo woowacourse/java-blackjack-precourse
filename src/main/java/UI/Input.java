ceb57ae7dc145b3b9e7c6c9a36562e78fe327d18
barrett24 <barrett2440@gmail.com>
2019-12-17 03:37:14 +0900

    Add - 최종 정산을 제외한 Input UI 구현

---
+++ src/main/java/UI/Input.java
@@ -0,0 +1,69 @@
+package UI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Input {
+    private static final String YES_RESPONSE = "y";
+    private static final String NO_RESPONSE = "n";
+    private static final String NAME_DIVIDER = ",";
+    private static final int MIN_PLAYER = 2;
+    private static final int MAX_PLAYER = 8;
+
+    private Scanner scanner = new Scanner(System.in);
+
+    private String userResponse() {
+        return scanner.nextLine();
+    }
+
+    public List<String> getPlayerEntry() {
+        Output.displayForGetPlayerNames();
+        return Optional.of(userResponse())
+                .filter(this::isNotEmptyName)
+                .map(this::nameSpliter)
+                .orElseGet(this::getPlayerEntry);
+    }
+
+    public double getBettingMoney(String playerName) {
+        Output.displayForGetPlayerBettingMoney(playerName);
+        return Optional.of(userResponse())
+                .flatMap(this::stringToDoubleForMoney)
+                .orElseGet(() -> getBettingMoney(playerName));
+    }
+
+    public boolean pickExtraCard(String playerName) {
+        Output.displayForAskPickMoreCard(playerName);
+        return Optional.of(userResponse())
+                .filter(this::isCorrectYesOrNo)
+                .map(x -> x.equals(YES_RESPONSE))
+                .orElseGet(() -> pickExtraCard(playerName));
+    }
+
+    public boolean isNotEmptyName(String playerNames) {
+        return !playerNames.contains(NAME_DIVIDER + NAME_DIVIDER)
+                || !playerNames.endsWith(NAME_DIVIDER);
+    }
+
+    public List<String> nameSpliter(String playerNames) {
+        return Arrays.asList(userResponse().split(NAME_DIVIDER));
+    }
+
+    public Optional<Double> stringToDoubleForMoney(String bettingMoney) {
+        try {
+            return Optional.of(Double.parseDouble(bettingMoney));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+    public boolean isCorrectYesOrNo(String response) {
+        return response.equals(YES_RESPONSE)
+                || response.equals(NO_RESPONSE);
+    }
+
+    public boolean responseChange(String response) {
+        return response.equals(YES_RESPONSE);
+    }
+}
