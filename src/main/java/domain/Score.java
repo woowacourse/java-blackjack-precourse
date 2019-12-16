7b42bddaf993e7785f4ad68f828c6e2652ab313a
YebinK <yebin825@gmail.com>
2019-12-16 21:35:24 +0900

    이 때 플레이어와 딜러 카드 모두 21이면 플레이어는 배팅 금액을 돌려받는다.

--- src/main/java/domain/Score.java
+++ src/main/java/domain/Score.java
@@ -26,22 +26,21 @@ public class Score {
         dealer.setProfit(bettingMoneySum);
     }
 
-
     public void blackJack(List<Player> winners, int dealerScore) {
         if (!winners.isEmpty()) returnBettingMoney(winners);
-        if (dealerScore != BLACK_JACK) blackJackScore(winners);
+        if (dealerScore != BLACK_JACK) blackJackWinners(winners);
     }
 
-    private void returnBettingMoney(List<Player> winners) { //우승자 배팅금액 돌려줌
+    private void returnBettingMoney(List<Player> winners) { //플레이어와 딜러 모두 21: 배팅금액 돌려줌
         for (Player player : winners) {
             player.setProfit(0);
             bettingMoneySum -= player.getBettingMoney();
         }
     }
 
-    private void blackJackScore(List<Player> winners) { //블랙잭으로 우승한 플레이어 1.5배 돌려줌
+    private void blackJackWinners(List<Player> winners) { //블랙잭 플레이어에게 1.5배로 돌려줌
         for (Player player : winners)
-            player.setProfit(player.getBettingMoney() * 1.5);
+            player.setProfit(player.getProfit() + player.getBettingMoney() * 1.5);
     }
 
 //    private void divideBettingMoney() //배팅금 우승자+딜러가 나눔
