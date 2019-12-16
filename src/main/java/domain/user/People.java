3fa8b061029ef47d8b5a71576633c4e7c56c5fca
HongBin kim <fusis1@naver.com>
2019-12-16 12:04:08 +0900

    [ADD] 블랙잭 판단 함수, 카드 합 함수 구현

--- src/main/java/domain/user/People.java
+++ src/main/java/domain/user/People.java
@@ -8,6 +8,13 @@ import java.util.stream.Collectors;
 import domain.card.Card;
 
 public class People {
+    final static int ZERO = 0;
+    final static int BLACKJACK_CARDS = 2;
+    final static int ONE = 1;
+    final static int TEN = 10;
+    final static int ELEVEN = 11;
+    final static int BLACKJACK_NUMBER = 21;
+
     private final List<Card> cards = new ArrayList<>();
 
     public People() {
@@ -18,16 +25,57 @@ public class People {
     }
 
     public int getTotalNumber() {
-        int result = 0;
+        ArrayList<Integer> numberList = new ArrayList<>();
         for (Card card : cards) {
-            result += card.getSymbol().getScore();
+            numberList.add(card.getSymbol().getScore());
+        }
+
+        return calculateNumber(numberList);
+    }
+
+    public String getCardList() {
+        return cards.
+                stream()
+                .map((x) -> {return x.getCardString();})
+                .collect(Collectors.joining(","));
+    }
+
+    public boolean isBlackJack() {
+        if ((cards.size() == BLACKJACK_CARDS) && blackJackCheck())
+            return true;
+        return false;
+    }
+
+    private int calculateNumber(ArrayList<Integer> numberList) {
+        int aceCount = ZERO;
+        int result = ZERO;
+        for (Integer number : numberList) {
+            aceCount += isAce(number.intValue());
+            result += number.intValue();
+        }
+        for (int i = 0; i < aceCount; i++) {
+            result = addCalculateBurst(result, TEN);
         }
         return result;
     }
 
-    public String getCardList(){
-        return cards.stream()
-            .map((x) -> {return x.getCardString();})
-            .collect(Collectors.joining(","));
+    private int addCalculateBurst(int result, int ten) {
+        if (result + ten < BLACKJACK_NUMBER) {
+            return result + ten;
+        }
+        return result;
+    }
+
+    private int isAce(int number) {
+        if (number == ONE)
+            return ONE;
+        return ZERO;
+    }
+
+    private boolean blackJackCheck() {
+        if (((cards.get(ZERO).getSymbol().getScore() + cards.get(ONE).getSymbol().getScore()) == ELEVEN)
+                && (cards.get(ZERO).getSymbol().getScore() == ONE || cards.get(ONE).getSymbol().getScore() == TEN))
+            return true;
+        return false;
     }
 }
