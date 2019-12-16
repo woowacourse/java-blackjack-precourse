c8f7126bc5b72ff8c7e95d34cd615a4af2c0036c
jb-oh <jb.oh@icloud.com>
2019-12-17 04:06:13 +0900

    카드를 한 장 나눠주는 메소드 추가

--- src/main/java/domain/GameProcessor.java
+++ src/main/java/domain/GameProcessor.java
@@ -11,20 +11,21 @@ import java.util.List;
 
 public class GameProcessor {
     private static ArrayList<Player> playersArray = new ArrayList<Player>();
-    static List<Card> shuffledCards = createCard();
 
-    static void addPlayers(ArrayList<String> playerNamesArray) {
+    static ArrayList<Player> addPlayers(ArrayList<String> playerNamesArray) {
         for (String playerName : playerNamesArray) {
             Double playerMoney = InputProcessor.getPlayerMoney(playerName);
             playersArray.add(new Player(playerName, playerMoney));
         }
-        for (int i = 0; i < playersArray.size(); i++) {
-            System.out.println(playersArray.get(i).getName() + ", " + (int)playersArray.get(i).getBettingMoney());
-        }
+        //for (int i = 0; i < playersArray.size(); i++) {
+        //    System.out.println(playersArray.get(i).getName() + ", " + (int)playersArray.get(i).getBettingMoney());
+        //}
+        return playersArray;
     }
 
-    static void createDealer() {
+    static Dealer createDealer() {
         Dealer dealer = new Dealer();
+        return dealer;
     }
 
     static List<Card> createCard() {
@@ -32,5 +33,11 @@ public class GameProcessor {
         List<Card> cardsArray = new ArrayList(cardFactory.create());
         Collections.shuffle(cardsArray);
         return cardsArray;
+    }
+
+    static Card dealCard(List<Card> cardsArray) {
+        Card dealtCard = cardsArray.get(0);
+        cardsArray.remove(0);
+        return dealtCard;
     }
 }
