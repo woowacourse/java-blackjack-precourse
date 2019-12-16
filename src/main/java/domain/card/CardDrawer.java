52c4b0c48c847239f8680d850fef2ad005229e11
minuyim <minu_yim@naver.com>
2019-12-16 14:51:57 +0900

    feat : 카드 덱에서 카드 한 장을 꺼내는 CardDrawer 기능 추가

---
+++ src/main/java/domain/card/CardDrawer.java
@@ -0,0 +1,23 @@
+package domain.card;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CardDrawer {
+	private static final int TOTAL_CARD_NUMBER = 52;
+	private static Random random = new Random();
+	private static List<Card> cardDeck = CardFactory.create();
+	private static List<Integer> drawnCard = new ArrayList<>();
+
+	public static Card draw() {
+		int idx = random.nextInt(TOTAL_CARD_NUMBER);
+		
+		if (drawnCard.contains(idx)) {
+			draw();
+		}
+		drawnCard.add(idx);
+		
+		return cardDeck.get(idx);
+	}
+}
