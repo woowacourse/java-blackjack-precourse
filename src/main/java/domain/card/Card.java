88c7d4347f6f5f5b0c4bd24285840312615f5ef5
ikswary <ikswary@naver.com>
2019-12-17 03:35:43 +0900

    [CORRECT] 컨벤션에 따른 정리

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -6,48 +6,55 @@ import java.util.Objects;
  * 카드 한장을 의미하는 객체
  */
 public class Card {
-    private final Symbol symbol;
-    private final Type type;
+	private final Symbol symbol;
+	private final Type type;
 
-    public Card(Symbol symbol, Type type) {
-        this.symbol = symbol;
-        this.type = type;
-    }
+	public Card(Symbol symbol, Type type) {
+		this.symbol = symbol;
+		this.type = type;
+	}
 
-    // TODO Card 관련 추가 기능 구현
-    public int getSymbolScore() {
-        return this.symbol.getScore();
-    }
+	// TODO Card 관련 추가 기능 구현
+	public int getSymbolScore() {
+		return this.symbol.getScore();
+	}
 
-    public Boolean isSymbolAce() {
-        if (this.symbol == Symbol.ACE) {
-            return true;
+	public Boolean isSymbolAce() {
+		if (this.symbol == Symbol.ACE) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Card card = (Card) o;
+		return symbol == card.symbol &&
+			type == card.type;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(symbol, type);
+	}
+
+	@Override
+	public String toString() {
+		if (this.symbol == Symbol.ACE) {
+			return " A" + this.type;
+		} else if (this.symbol == Symbol.KING) {
+		    return " K" + this.type;
+        } else if (this.symbol == Symbol.QUEEN) {
+		    return " Q" + this.type;
+        } else if (this.symbol == Symbol.JACK) {
+		    return " J" + this.type;
         }
-        return false;
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Card card = (Card) o;
-        return symbol == card.symbol &&
-                 type == card.type;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(symbol, type);
-    }
-
-    @Override
-    public String toString() {
-        if (this.symbol == Symbol.ACE){
-            return " A" + this.type;
-        }
-        return " " + this.symbol.getScore() + this.type;
-    }
-}
+		return " " + this.symbol.getScore() + this.type;
+		}
+	}
