73099177c73a76823d974ed75425bd7f3c613389
YebinK <yebin825@gmail.com>
2019-12-17 06:34:44 +0900

    출력 시 SPADE -> 스페이드 로 변환.

--- src/main/java/domain/card/Type.java
+++ src/main/java/domain/card/Type.java
@@ -1,8 +1,19 @@
 package domain.card;
 
 public enum Type {
-    SPADE,
-    DIAMOND,
-    HEART,
-    CLUB
+    SPADE("스페이드"),
+    DIAMOND("다이아몬드"),
+    HEART("하트"),
+    CLUB("클럽");
+
+    private String kor;
+
+    Type(String kor) {
+        this.kor = kor;
+    }
+
+    public String toString() {
+        return kor;
+    }
+
 }
