4c8d5985b5c2e9e730a417b3b7a3c87a4e403cdb
Min-92 <0201.mj.lee@gmail.com>
2019-12-16 22:42:26 +0900

    Fix : arrayList 오류 수정
    - ArrayList 생성자 추가

--- src/main/java/domain/blackjack/Table.java
+++ src/main/java/domain/blackjack/Table.java
@@ -9,7 +9,7 @@ public class Table {
 
   private Deck deck;
   private Dealer dealer;
-  private ArrayList<Player> players;
+  private ArrayList<Player> players = new ArrayList<>();
 
   public Table() {
     this.dealer = new Dealer();
