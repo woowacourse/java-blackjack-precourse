df256ea14cd802f7be4cf9c221845c8989a436ce
ikswary <ikswary@naver.com>
2019-12-16 13:35:34 +0900

    [IMPROVE] getPlayersName() 메소드가 empty input을 핸들할 수 있게 수정
    이를 위해 validateNames, catchEmptyInput 메소드 작성

--- src/main/java/controller/BlackJackController.java
+++ src/main/java/controller/BlackJackController.java
@@ -4,23 +4,38 @@ import domain.card.Card;
 import domain.user.Player;
 import exceptions.EmptyInputException;
 import exceptions.NegativeNumberException;
+import services.BlackJackUIService;
 import utils.UtilityMethods;
 
 import java.util.List;
 import java.util.Scanner;
 
 public class BlackJackController {
-	public static List<String> getPlayersName() {
-		System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+	public static List<String> getInputOfPlayersName() {
+		String names = null;
+
+		while (names == null) {
+			names = catchEmptyInput();
+		}
+		return UtilityMethods.sliceStringToList(names);
+	}
+
+	private static String catchEmptyInput() {
 		Scanner scanner = new Scanner(System.in);
 		String names = scanner.nextLine();
 		try {
 			validateNames(names);
 		} catch (EmptyInputException e) {
-			System.out.println("플레이어를 입력하여야 합니다");
+			BlackJackUIService.printEmptyInputMessage();
 		}
 		scanner.close();
-		return UtilityMethods.sliceStringToList(names);
+		return names;
+	}
+
+	private static void validateNames(String names) throws EmptyInputException {
+		if (names.equals("")) {
+			throw new EmptyInputException();
+		}
 	}
 
 	public static List<Double> getEachBettings() {
@@ -39,30 +54,21 @@ public class BlackJackController {
 		return betting;
 	}
 
-	public void playGames() {
 
-	}
 
-	private static String validateNames(String names) throws EmptyInputException {
-		if (names.equals("")) {
-			throw new EmptyInputException();
-		}
-		return names;
-	}
-
-	private static double validateBetting(double betting) throws NegativeNumberException {
+	private static void validateBetting(double betting) throws NegativeNumberException {
 		if (betting < 0) {
 			throw new NegativeNumberException();
 		}
-		return betting;
-
-		private void drawFromCardDeck () {
-			for (Player player : this.players) {
-				player.addCard(popRandomCard(this.cardDeck));
-			}
-			dealer.addCard(popRandomCard(this.cardDeck));
-		}
 	}
+
+	private void drawFromCardDeck() {
+		for (Player player : this.players) {
+			player.addCard(popRandomCard(this.cardDeck));
+		}
+		dealer.addCard(popRandomCard(this.cardDeck));
+	}
+
 
 	public Card popRandomCard(List<Card> cards) {
 		int popIndex = UtilityMethods.generateRandomNumber(cards.size());
