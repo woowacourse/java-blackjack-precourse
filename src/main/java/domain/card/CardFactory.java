f79d31eac9013a73542c09bc257f940d9537d581
Min-92 <0201.mj.lee@gmail.com>
2019-12-17 04:35:45 +0900

    REFACTOR : Ace 점수 변경
    - Ace 점수 11로 변경
    - Ace 생성하는 메소드 생성

--- src/main/java/domain/card/CardFactory.java
+++ src/main/java/domain/card/CardFactory.java
@@ -8,19 +8,44 @@ import java.util.List;
  * 트럼프 카드 전체 생성을 담당하는 객체
  */
 public class CardFactory {
-    public static List<Card> create() {
-        List<Card> cards = new ArrayList<>();
-        Symbol[] symbols = Symbol.values();
-        for (Symbol symbol : symbols) {
-            createByType(cards, symbol);
-        }
-        return Collections.unmodifiableList(cards);
+
+  public static List<Card> create() {
+    List<Card> cards = new ArrayList<>();
+    Symbol[] symbols = Symbol.values();
+    for (Symbol symbol : symbols) {
+      createByType(cards, symbol);
+    }
+    return Collections.unmodifiableList(cards);
+  }
+
+  private static void createByType(List<Card> cards, Symbol symbol) {
+    Type[] types = Type.values();
+    for (Type type : types) {
+      cards.add(new Card(symbol, type));
+    }
+  }
+
+  public static List<Card> createAces() {
+    List<Card> cards = new ArrayList<>();
+    Type[] types = Type.values();
+
+    for (Type type : types) {
+      createAcesBySymbol(cards, type);
     }
 
-    private static void createByType(List<Card> cards, Symbol symbol) {
-        Type[] types = Type.values();
-        for (Type type : types) {
-            cards.add(new Card(symbol, type));
-        }
+    return Collections.unmodifiableList(cards);
+  }
+
+  private static void createAcesBySymbol(List<Card> cards, Type type) {
+    Symbol[] symbols = Symbol.values();
+
+    for (Symbol symbol : symbols) {
+      if (symbol.getScore() < 11) {
+        break;
+      }
+      cards.add(new Card(symbol, type));
     }
+
+
+  }
 }
