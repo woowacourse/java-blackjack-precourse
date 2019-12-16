c4b2232c7cdfd7d4fe10799c8badf8ec160ec9aa
aegis1920 <aegis1920@gmail.com>
2019-12-16 19:55:01 +0900

    [docs] add : 소유한 카드를 출력하는 메소드, [feat] add : 베팅 금액 입력 메소드, 베팅 금액 체크 메소드

--- src/main/java/domain/game/CardGame.java
+++ src/main/java/domain/game/CardGame.java
@@ -1,5 +1,10 @@
 package domain.game;
 
+import domain.card.Card;
+import domain.card.CardFactory;
+import domain.user.Player;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -9,8 +14,9 @@ public class CardGame {
 
     // 후에 rafactoring
     static Scanner sc = new Scanner(System.in);
+    static final int FIRST_CARD_COUNT = 2;
 
-    public static void insertPlayerNameStr(){
+    public static List<String> insertPlayerNameStr(){
 
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         String playerStr = sc.nextLine();
@@ -25,29 +31,62 @@ public class CardGame {
             playerNameList = deleteSpacePlayerNameArr(playerNameArr);
         }
 
-        System.out.println(playerNameList.toString());
+        return playerNameList;
 
     }
 
     public static String[] dividePlayerNameStrToRest(String playerStr){
-        String[] playerNameArr = playerStr.split(",");
-        return playerNameArr;
+        return playerStr.split(",");
     }
 
     public static List<String> deleteSpacePlayerNameArr(String[] playerNameArr){
-        List<String> playerNameList = Arrays.asList(playerNameArr).stream()
+        return Arrays.asList(playerNameArr).stream()
                 .map(element -> element.replace(" ", ""))
                 .collect(Collectors.toList());
-
-        return playerNameList;
     }
 
     public static boolean checkPlayerNameList(List<String> playerNameList){
-        return playerNameList.stream().allMatch(element -> !element.equals(""));
+        return playerNameList.stream()
+                .allMatch(element -> !element.equals(""));
+    }
+
+    public static List<Player> insertPlayerMoneyStr(List<String> playerNameList){
+
+        List<Player> playerList = new ArrayList<>();
+
+        // 여기 bettingMoney가 음수일 경우 처리해줘야 함. 함수로 빼면 되겠다. 그냥
+        playerNameList.stream().
+                forEach(element -> {
+                    System.out.println(element + "의 베팅 금액은?");
+                    int bettingMoney = insertBettingMoney();
+                    playerList.add(new Player(element, bettingMoney));
+                });
+
+        return playerList;
+    }
+
+    public static int insertBettingMoney(){
+        int bettingMoney = sc.nextInt();
+        while(checkBettingMoney(bettingMoney)){
+            System.out.println("베팅 금액이 너무 적습니다! 다시 입력해주세요!");
+            bettingMoney = sc.nextInt();
+        }
+        return bettingMoney;
+    }
+
+    public static boolean checkBettingMoney(int bettingMoney){
+        if(bettingMoney <= 0){
+            return true;
+        }
+        return false;
     }
 
     public static void run(){
-        insertPlayerNameStr();
+        List<String> playerNameList = insertPlayerNameStr();
+        System.out.println(playerNameList.toString());
+        List<Player> playerList = insertPlayerMoneyStr(playerNameList);
+        List<Card> cardList = CardFactory.create();
+        System.out.println(playerList.toString());
     }
 
 }
