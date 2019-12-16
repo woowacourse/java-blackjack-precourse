e1286b9233d276a04b1afcc33cc1cc24830a6d7a
김예림 <kiel0103@naver.com>
2019-12-16 16:29:34 +0900

    플레이어, 딜러의 카드 결과 얻기, 플레이어가 가지고있는 카드 전체 얻기 기능 구현

--- src/main/java/domain/user/User.java
+++ src/main/java/domain/user/User.java
@@ -2,20 +2,17 @@ package domain.user;
 
 import domain.card.Card;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public abstract class User {
-    protected final List<Card> cards = new ArrayList<>();
+public class User {
+    private final List<Card> cards = new ArrayList<>();
 
     public void addCard(Card card) {
         cards.add(card);
     }
 
-    public int getSumOfAllCardNumbers() {
-        return 0;
-    }
-
-    public String[] getCardsStrings() {
-        return null;
+    public List<Card> openAllCards() {
+        return Collections.unmodifiableList(cards);
     }
 }
