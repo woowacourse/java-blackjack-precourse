70444daa7dd8394a44ea16ed2b1df442d0a7abf5
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-16 15:52:55 +0900

    feat: 딜러가 공개할 카드 장수 분리, 정리

--- src/main/java/domain/game/Rule.java
+++ src/main/java/domain/game/Rule.java
@@ -4,6 +4,7 @@ public class Rule {
 	static final int BASIC_DRAW = 2;
 	static final int BLACKJACK_POINT = 21;
 	static final int DEALER_DRAW_POINT = 17;
+	static final int DEALER_OPEN_COUNT=1;
 	private static final String OUT_OF_CARDS_MESSAGE = "카드가 부족합니다.";
 
 	public static int getBasicDraw() {
@@ -20,5 +21,13 @@ public class Rule {
 
 	public static String getOutOfCardsMessage() {
 		return OUT_OF_CARDS_MESSAGE;
+	}
+
+	public static int getOpenCount() {
+		return DEALER_OPEN_COUNT;
+	}
+
+	public static void setOpenCount(int openCount) {
+		Rule.openCount = openCount;
 	}
 }
