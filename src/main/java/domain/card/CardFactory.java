62d24c6514183a09697ac1f7c629a14e11632b85
HWooKim97 <rgd5454@naver.com>
2019-12-16 23:04:32 +0900

    Fix method create()
    - Delete unmodifiableList() to use object

--- src/main/java/domain/card/CardFactory.java
+++ src/main/java/domain/card/CardFactory.java
@@ -14,7 +14,7 @@ public class CardFactory {
         for (Symbol symbol : symbols) {
             createByType(cards, symbol);
         }
-        return Collections.unmodifiableList(cards);
+        return cards;
     }
 
     private static void createByType(List<Card> cards, Symbol symbol) {
