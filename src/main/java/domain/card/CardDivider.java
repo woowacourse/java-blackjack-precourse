fd6b7f9f45f981af1eb21f9d9a79a0144ad571d3
김예림 <kiel0103@naver.com>
2019-12-16 19:04:13 +0900

    참여자 전원이 카드를 두장 씩 받기 기능 수정 : 수정할 수 없는 카드 리스트를 섞을 수 없어서, 카드 인덱스를 랜덤으로 얻도록 수정함

--- src/main/java/domain/card/CardDivider.java
+++ src/main/java/domain/card/CardDivider.java
@@ -6,26 +6,29 @@ import java.util.List;
 import java.util.Random;
 
 public class CardDivider {
+    private final int RANDOM_NUM_MULTIPLIER = 100;      /* (랜덤실수) x 100 */
     private final List<Card> cardList;
     private List<Card> usedCards = new ArrayList<Card>();
 
     public CardDivider(List<Card> cardList) {
         this.cardList = cardList;
-
     }
 
-    private void cardListRandomShuffle(List<Card> cardList) {
-        long seed = System.nanoTime();
-        Collections.shuffle(cardList, new Random(seed));
-    }
-
-    public Card getOneCard() {
+    public Card getOneRandomCard() {
+        Card ret;
 
         if (cardList == null || cardList.size() < 1) {
             return null;
         }
 
-        return cardList.get(0);
+        /* 랜덤하게 뽑을 카드의 인덱스 결정 */
+        ret = cardList.get((int)(Math.random() * RANDOM_NUM_MULTIPLIER) % cardList.size());
+
+        for (int idx = 1; idx < cardList.size() && isAlreadyUsed(ret); idx++) {
+            ret = cardList.get(idx);
+        }
+
+        return ret;
     }
 
     private boolean isAlreadyUsed(Card card) {
