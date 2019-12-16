7074ef14e637f6f5e12d8fd0acca349e625051da
junk-sound <wom2277@naver.com>
2019-12-16 13:49:42 +0900

    비기능: 상수 위치 조정.

--- src/main/java/common/BlackjackConfig.java
+++ src/main/java/common/BlackjackConfig.java
@@ -2,4 +2,8 @@ package common;
 
 public class BlackjackConfig {
     public static final String STANDARD_FOR_PARSE = ",";
+    public static final int BLACKJACK = 21;
+    public static final int SURPLUS_OF_ACE = 10;
+    public static final int STANDARD_TO_HIT_FOR_DEALER = 16;
+    public static final int DEFAULT_NUMBER = 2;
 }
