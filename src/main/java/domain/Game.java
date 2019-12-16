f2d17232a7c656001166d3c04eda05b4e557db60
Devhyein <jhn7121@naver.com>
2019-12-17 01:22:11 +0900

    convention 수정

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -30,7 +30,7 @@ public class Game {
         Game();
     }
 
-    public void inputPlayerName(){
+    public void inputPlayerName() {
         String totalInputName;
 
         System.out.println("참여할 플레이어의 이름을 입력하세요.(이름은 쉼표 기준으로 구분) : ");
@@ -42,7 +42,7 @@ public class Game {
     }
 
     private boolean checkFormAboutName() {
-        if(name.length < 2 || name.length > 8) {
+        if (name.length < 2 || name.length > 8) {
             System.out.println("player는 2~8명으로 설정해주세요.");
             return false;
         }
@@ -51,8 +51,8 @@ public class Game {
 
     private void inputBettingMoney() {
         bettingMoney = new double[name.length];
-        for(int i = 0; i < name.length; i++) {
-            System.out.println(name[i]+"의 배팅 금액은?");
+        for (int i = 0; i < name.length; i++) {
+            System.out.println(name[i] + "의 배팅 금액은?");
             bettingMoney[i] = sc.nextDouble();
         }
     }
@@ -66,11 +66,11 @@ public class Game {
 
     public void Game() {
         initDistributeCard();
-        if(dealer.ifBlackJack()) {
+        if (dealer.ifBlackJack()) {
             dealerIsBlackJack();
             return;
         }
-        for(index = 0; index < player.size(); index++){
+        for (index = 0; index < player.size(); index++) {
             askOneMoreOnlyForNotBJPlayer();
         }
         giveMoreCardToDealer();
@@ -78,20 +78,20 @@ public class Game {
     }
 
     public void dealerIsBlackJack() {
-        for(int i=0;i<player.size();i++) {
+        for (int i = 0; i < player.size(); i++) {
             checkIfPlayerIsBJWhenDealerIsBJ(i);
         }
     }
 
     public void checkIfPlayerIsBJWhenDealerIsBJ(int i) {
-        if(player.get(i).ifBlackJack()){
+        if (player.get(i).ifBlackJack()) {
             player.get(index).multipleInterestAndBettingMoney(0);
         }
         player.get(index).multipleInterestAndBettingMoney(-1);
     }
 
     public void askOneMoreOnlyForNotBJPlayer() {
-        if(!checkPlayerBlackJack()){
+        if (!checkPlayerBlackJack()) {
             askOneMoreCard();
         }
     }
@@ -110,14 +110,14 @@ public class Game {
     }
 
     public void distributeCard() {
-        for(int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) {
             distributeCardToPlayer();
             distributeCardToDealer();
         }
     }
 
     public void distributeCardToPlayer() {
-        for(int i = 0; i < player.size(); i++) {
+        for (int i = 0; i < player.size(); i++) {
             player.get(i).addCard(cards.remove(cards.size() - 1));
         }
     }
@@ -128,10 +128,10 @@ public class Game {
 
     public void printInfo() {
         System.out.print("\n딜러와 ");
-        for(int i = 0; i < player.size()-1; i++) {
-            System.out.print(player.get(i).getName()+", ");
+        for (int i = 0; i < player.size() - 1; i++) {
+            System.out.print(player.get(i).getName() + ", ");
         }
-        System.out.print(player.get(player.size()-1).getName());
+        System.out.print(player.get(player.size() - 1).getName());
         System.out.println("에게 2장의 카드를 나누었습니다.\n");
     }
 
@@ -141,14 +141,14 @@ public class Game {
     }
 
     public void printPlayerCard() {
-        for(int i = 0; i < player.size(); i++) {
-            System.out.print(player.get(i).getName()+"카드: ");
+        for (int i = 0; i < player.size(); i++) {
+            System.out.print(player.get(i).getName() + "카드: ");
             System.out.println(player.get(i).toString());
         }
     }
 
     public boolean checkPlayerBlackJack() {
-        if(player.get(index).ifBlackJack()) {
+        if (player.get(index).ifBlackJack()) {
             player.get(index).multipleInterestAndBettingMoney(1.5);
             dealer.minusCost(player.get(index).getReward());
             return true;
@@ -175,7 +175,7 @@ public class Game {
             actAddOneMoreCard();
             ifUnder21 = checkIfOver21();
         }
-        if(!Objects.equals(answer, "n") && ifUnder21){
+        if (!Objects.equals(answer, "n") && ifUnder21) {
             askOneMoreCard();
         }
 
@@ -183,12 +183,12 @@ public class Game {
 
     public void actAddOneMoreCard() {
         player.get(index).addCard(cards.remove(cards.size() - 1));
-        System.out.print(player.get(index).getName()+"카드: ");
+        System.out.print(player.get(index).getName() + "카드: ");
         System.out.println(player.get(index).toString());
     }
 
     public boolean checkIfOver21() {
-        if(player.get(index).sumScore() > BLACKJACK) {
+        if (player.get(index).sumScore() > BLACKJACK) {
             player.get(index).burstPlayer = true;
             System.out.println("21을 초과하여 게임에서 지셨습니다.\n");
             LoosePlayerBurst();
@@ -198,13 +198,13 @@ public class Game {
     }
 
     public void giveMoreCardToDealer() {
-        if(dealer.sumScore() <= DEALER_STANDARD) {
+        if (dealer.sumScore() <= DEALER_STANDARD) {
             System.out.println("딜러가 16 이하라 한장 더 받았습니다.\n");
             dealer.addCard(cards.remove(cards.size() - 1));
             giveMoreCardToDealer();
             return;
         }
-        if(dealer.sumScore() > BLACKJACK) {
+        if (dealer.sumScore() > BLACKJACK) {
             LooseDealerBurst();
         }
     }
@@ -212,7 +212,7 @@ public class Game {
     public void LooseDealerBurst() {
         System.out.println("딜러가 21을 초과하여 패하였습니다.\n");
         dealer.ifBurst = true;
-        for(int i = 0; i < player.size(); i++) {
+        for (int i = 0; i < player.size(); i++) {
             calculateLooseDealerBurst(i);
         }
     }
@@ -230,20 +230,20 @@ public class Game {
     }
 
     public void finishGame() {
-        for(int i = 0; i < player.size(); i++) {
+        for (int i = 0; i < player.size(); i++) {
             findWinner(i);
         }
         printFinalDealerCard();
         printFinalPlayerCard();
         System.out.println("\n최종 수익");
-        System.out.println("딜러: "+dealer.finalCost());
-        for(int i = 0; i < player.size(); i++) {
-            System.out.println(player.get(i).getName()+": "+player.get(i).getReward());
+        System.out.println("딜러: " + dealer.finalCost());
+        for (int i = 0; i < player.size(); i++) {
+            System.out.println(player.get(i).getName() + ": " + player.get(i).getReward());
         }
     }
 
     public void findWinner(int i) {
-        if(player.get(i).ifBlackJack() || player.get(i).burstPlayer || dealer.ifBurst){
+        if (player.get(i).ifBlackJack() || player.get(i).burstPlayer || dealer.ifBurst) {
             return;
         }
         comparePlayerAndDealer(i);
@@ -262,13 +262,13 @@ public class Game {
 
     public void printFinalDealerCard() {
         System.out.print("딜러: ");
-        System.out.println(dealer.toString()+" - 결과: "+dealer.sumScore());
+        System.out.println(dealer.toString() + " - 결과: " + dealer.sumScore());
     }
 
     public void printFinalPlayerCard() {
-        for(int i = 0; i < player.size(); i++) {
-            System.out.print(player.get(i).getName()+"카드: ");
-            System.out.println(player.get(i).toString()+" - 결과: "+player.get(i).sumScore());
+        for (int i = 0; i < player.size(); i++) {
+            System.out.print(player.get(i).getName() + "카드: ");
+            System.out.println(player.get(i).toString() + " - 결과: " + player.get(i).sumScore());
         }
     }
 }
