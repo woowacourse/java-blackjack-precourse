830867b40883790b1d77ce3d1c7592618e96cdbc
ikswary <ikswary@naver.com>
2019-12-16 11:44:56 +0900

    [REFACTOR] MVC 모델을 따르기 위한 메소드 재배치
    기존 InputService의 메소드들을 Controller로,
    Controller의 메소드들을 PlayService로 이동한 뒤
    InputService 클래스 삭제 및 UIService 생성

--- src/main/java/controller/BlackJackController.java
+++ src/main/java/controller/BlackJackController.java
@@ -1,32 +1,67 @@
 package controller;
 
 import domain.card.Card;
-import domain.card.CardFactory;
-import domain.user.Dealer;
 import domain.user.Player;
-import services.BlackJackInputService;
+import exceptions.EmptyInputException;
+import exceptions.NegativeNumberException;
 import utils.UtilityMethods;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class BlackJackController {
-	private List<Card> cardDeck;
-	private List<Player> players;
-	private Dealer dealer;
-
-	public BlackJackController() {
-		this.cardDeck = CardFactory.create();
-		this.dealer = new Dealer();
-		List<String> names = BlackJackInputService.getPlayersName();
+	public static List<String> getPlayersName() {
+		System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+		Scanner scanner = new Scanner(System.in);
+		String names = scanner.nextLine();
+		try {
+			validateNames(names);
+		} catch (EmptyInputException e) {
+			System.out.println("플레이어를 입력하여야 합니다");
+		}
+		scanner.close();
+		return UtilityMethods.sliceStringToList(names);
 	}
 
-	public void startBlackJack() {}
+	public static List<Double> getEachBettings() {
+	}
 
-	private void drawFromCardDeck() {
-		for ( Player player : this.players ) {
-			player.addCard(popRandomCard(this.cardDeck));
+	public static double getBetting(String player) {
+		System.out.println(player + "의 배팅 금액은?");
+		Scanner scanner = new Scanner(System.in);
+		double betting = scanner.nextDouble();
+		try {
+			validateBetting(betting);
+		} catch (NegativeNumberException e) {
+			System.out.println("양수를 입력해야 합니다");
 		}
-		dealer.addCard(popRandomCard(this.cardDeck));
+		scanner.close();
+		return betting;
+	}
+
+	public void playGames() {
+
+	}
+
+	private static String validateNames(String names) throws EmptyInputException {
+		if (names.equals("")) {
+			throw new EmptyInputException();
+		}
+		return names;
+	}
+
+	private static double validateBetting(double betting) throws NegativeNumberException {
+		if (betting < 0) {
+			throw new NegativeNumberException();
+		}
+		return betting;
+
+		private void drawFromCardDeck () {
+			for (Player player : this.players) {
+				player.addCard(popRandomCard(this.cardDeck));
+			}
+			dealer.addCard(popRandomCard(this.cardDeck));
+		}
 	}
 
 	public Card popRandomCard(List<Card> cards) {
