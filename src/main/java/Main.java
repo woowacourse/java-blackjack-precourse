84bb1bb516e1653c196181a5f4ca9caf689052dd
Min-92 <0201.mj.lee@gmail.com>
2019-12-16 16:49:28 +0900

    FEAT : Game 클래스 생성
    - domain.Game 클래스 생성
    - Game.play() 메소드 생성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import domain.blackjack.Game;
+
 public class Main {
 
   public static void main(String[] args) {
+    Game game = new Game();
+    game.play();
   }
 }
---
+++ src/main/java/domain/blackjack/Game.java
@@ -0,0 +1,8 @@
+package domain.blackjack;
+
+public class Game {
+
+  public void play() {
+
+  }
+}
