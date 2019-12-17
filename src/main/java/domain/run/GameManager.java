4e83ddddc59a42b1a99b39c87faf68c8d7be1bfd
hayoung298 <dudgk298@gmail.com>
2019-12-17 15:34:03 +0900

    이름 입력받고, 분리하는 함수 추가

--- src/main/java/domain/run/GameManager.java
+++ src/main/java/domain/run/GameManager.java
@@ -1,28 +1,48 @@
 package domain.run;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
+import com.sun.javafx.scene.control.skin.FXVK.Type;
+
+import domain.card.Card;
+import domain.card.CardFactory;
+import domain.card.Symbol;
+import domain.user.Dealer;
 import domain.user.Player;
 
 public class GameManager {
 
-	final Scanner sc=new Scanner(System.in);
-	ArrayList<Player> players=new ArrayList<Player>();
-	
+	final Scanner sc = new Scanner(System.in);
+	List<Card> cards = CardFactory.create();
+	ArrayList<Player> players = new ArrayList<Player>();
+	ArrayList<Card> playerCard = new ArrayList<Card>();
+	Integer[] scoreResult;
+	int maxScore=0;
+	int winner = 0;
+	Random random = new Random();
+
+	Dealer dealer = new Dealer();
+
 	public void printInputName() {
 		System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-		String names=sc.next();
+		String names = sc.next();
 		splitName(names);
 	}
 
 	private void splitName(String names) {
-		for(String name : names.split(",")) {
-			System.out.println(name+"의 배팅금액은?");
-			players.add(new Player(name,sc.nextDouble()));
+		int count=0;
+		for (String name : names.split(",")) {
+			System.out.println(name + "의 배팅금액은?");
+			players.add(new Player(name, sc.nextDouble()));
+			count++;
 		}
+		 scoreResult= new Integer[count+1];
+		drawCard();
+		printResult();
 	}
-
 
 }
