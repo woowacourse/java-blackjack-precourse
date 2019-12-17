f5366f0029abe2e8d9efd5f116b6634cacff8e82
hayoung298 <dudgk298@gmail.com>
2019-12-17 15:27:50 +0900

    symbol, type 값 get

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -7,7 +7,6 @@ import java.util.Objects;
  */
 public class Card {
     private final Symbol symbol;
-
     private final Type type;
 
     public Card(Symbol symbol, Type type) {
@@ -37,5 +36,11 @@ public class Card {
                 "symbol=" + symbol +
                 ", type=" + type +
                 '}';
+    }
+    public Symbol getSymbol() {
+    	return symbol;
+    }
+    public Type getType() {
+    	return type;
     }
 }
