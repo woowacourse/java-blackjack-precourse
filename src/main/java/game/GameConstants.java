306b2e0ddc2595a3f286f8932d1107395f197023
zion830 <zion830@honglab.org>
2019-12-17 23:37:31 +0900

    feat: 게임에서 사용할 모든 상수를 모아놓은 클래스 추가

---
+++ src/main/java/game/GameConstants.java
@@ -0,0 +1,16 @@
+package game;
+
+/**
+ * 게임 규칙과 연관된 모든 상수 정의
+ */
+public class GameConstants {
+    public static final int RANGE_MIN = 0;
+    public static final int ENTRY_MIN = 0;
+    public static final int FIRST_DIVIDE_COUNT = 2;
+    public static final int BLACKJACK = 21;
+    public static final int REQUEST_MORE_CARD_COUNT = 16;
+    public static final int ACE_TO_TEN_COUNT = 11;
+    public static final int ACE_COUNT = 10;
+    public static final String YES = "y";
+    public static final String NO = "n";
+}
