eac6fa4e61699e8ef2a3bdb5f7c9f8f68a978af3
KSKIM <kwangsoonkim@outlook.com>
2019-12-17 15:00:28 +0900

    feat: 게임을 실행하는 BlackjackApplication 추가

---
+++ src/main/java/com/woowacourse/blackjack/BlackjackApplication.java
@@ -0,0 +1,22 @@
+package com.woowacourse.blackjack;
+
+import com.woowacourse.blackjack.controller.BlackjackGame;
+import com.woowacourse.blackjack.domain.card.CardDeck;
+import com.woowacourse.blackjack.domain.card.CardFactory;
+import com.woowacourse.blackjack.domain.user.dealer.Dealer;
+
+/**
+ * 블랙잭 어플리케이션의 시작부
+ *
+ * @version 1.0.0
+ * @author KSKIM
+ * @since 2019-12-15
+ */
+public class BlackjackApplication {
+	public static void main(String[] args) {
+		CardDeck cardDeck = new CardDeck(CardFactory.create());
+		Dealer dealer = new Dealer();
+		BlackjackGame blackJackGame = new BlackjackGame(cardDeck, dealer);
+		blackJackGame.play();
+	}
+}
