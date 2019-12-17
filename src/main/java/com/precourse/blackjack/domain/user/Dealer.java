d79c68d047e0be8ceecddb79558bfda5ca6cf7d9
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-12-17 15:37:13 +0900

    refactor: 딜러 객체에 상수 추가

--- src/main/java/com/precourse/blackjack/domain/user/Dealer.java
+++ src/main/java/com/precourse/blackjack/domain/user/Dealer.java
@@ -4,13 +4,15 @@ import com.precourse.blackjack.domain.card.Card;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /**
  * 게임 딜러를 의미하는 객체
  */
 public class Dealer {
+	private static final int ACE_SCORE = 11;
+	private static final int MAXIMUM_SCORE = 21;
+	private static final int ACE_TO_ONE = 10;
+
 	private final List<Card> cards = new ArrayList<>();
 
 	public Dealer() {
@@ -34,14 +36,14 @@ public class Dealer {
 			.mapToInt(card -> card.getScore())
 			.sum();
 
-		while (totalScore > 21 && hasAce()) {
-			totalScore -= 10;
+		while (totalScore > MAXIMUM_SCORE && hasAce()) {
+			totalScore -= ACE_TO_ONE;
 		}
 		return totalScore;
 	}
 
 	private boolean hasAce() {
 		return cards.stream()
-			.anyMatch(card -> card.getScore() == 11);
+			.anyMatch(card -> card.getScore() == ACE_SCORE);
 	}
 }
