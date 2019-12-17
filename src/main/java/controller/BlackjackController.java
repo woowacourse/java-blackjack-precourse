6b92d95037f7cb6bc60a889d6a14984d51a11edf
sezikim <kdghj1002@gmail.com>
2019-12-17 17:32:23 +0900

    Add feat - 플레이어와 딜러에게 처음 두장의 카드를 주는 기능

--- src/main/java/controller/BlackjackController.java
+++ src/main/java/controller/BlackjackController.java
@@ -1,7 +1,38 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.card.Card;
+import domain.card.CardFactory;
+import domain.user.BlackjackMember;
+import domain.user.BlackjackEntry;
+
 public class BlackjackController {
 	public static final int MAX = 21;
 	public static final int ACE_TERM = 10;
 	public static final int ZERO = 0;
+
+	private List<Card> cards;
+
+	BlackjackController() {
+		cards = new ArrayList<>();
+
+	}
+
+	private List<Card> initCards() {
+		List<Card> cards = new ArrayList<>();
+
+		for (Card card : CardFactory.create()) {
+			cards.add(card);
+		}
+
+		return cards;
+	}
+
+	private void firstServe(BlackjackEntry blackjackEntry) {
+		for (BlackjackMember blackjackMember : blackjackEntry.getEntry()) {
+			blackjackMember.addCard(cards.remove(ZERO));
+		}
+	}
 }
