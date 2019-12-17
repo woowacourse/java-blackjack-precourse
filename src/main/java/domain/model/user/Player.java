7eb1c39c22149501268735c9724e15cf23f559fd
toneyparky <qkrtmddhks95@gmail.com>
2019-12-17 14:34:15 +0900

    Update: Player 클래스 자바독스 수정

--- src/main/java/domain/model/user/Player.java
+++ src/main/java/domain/model/user/Player.java
@@ -9,7 +9,7 @@ package domain.model.user;
  * @author Seungwan Park, github.com/toneyparky
  * @version 1.0
  * @apiNote 게임 참여자를 의미하는 객체입니다.
- * @since : %G%
+ * @since : 2019.12.17 화요일
  */
 public class Player extends BlackJackParticipant {
     private final String name;
