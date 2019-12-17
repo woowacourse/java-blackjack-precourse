822f851d36c529508fb36fa344e3809ac09d6b29
HWooKim97 <rgd5454@naver.com>
2019-12-17 11:57:14 +0900

    Fix Dealer class
    - Remove overlaps with Player class

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -1,16 +1,9 @@
 package domain.user;
 
-import domain.card.Card;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 게임 딜러를 의미하는 객체
  */
 public class Dealer extends Player {
-    private final List<Card> cards = new ArrayList<>();
-
     public Dealer() {
         super("딜러", 0);
     }
