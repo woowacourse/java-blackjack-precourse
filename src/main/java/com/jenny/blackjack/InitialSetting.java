039e45e53327b962119a37f9cbcf9f9d7ec7e14b
jenny <jennylee4517@gmail.com>
2019-12-17 19:22:55 +0900

    isValidName 예외처리

--- src/main/java/com/jenny/blackjack/InitialSetting.java
+++ src/main/java/com/jenny/blackjack/InitialSetting.java
@@ -30,10 +30,13 @@ public class InitialSetting {
         String[] splitedInput = input.split(",");
         int cnt = splitedInput.length;
 
+        if(cnt == 0){
+            return false;
+        }
         for(int compared = 0; compared < cnt; compared++){
             for(int each = compared+1; each < cnt; each++){
                 if(splitedInput[compared].equals(splitedInput[each])
-                    || splitedInput[compared] == null || splitedInput[each] == null){
+                    || splitedInput[compared].equals("") || splitedInput[each].equals("")){
                     return false;
                 }
             }
