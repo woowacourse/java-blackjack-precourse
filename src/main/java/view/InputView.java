a587972a005ec3902be97d1716d21cbb73c9ecbf
begaonnuri <begaonnuri@gmail.com>
2019-12-17 12:09:07 +0900

    feat: 한장의 카드를 받을지 선택을 입력받는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import domain.user.Player;
+
 import java.util.Scanner;
 
 public class InputView {
@@ -13,5 +15,10 @@ public class InputView {
     public static int inputPlayerMoney(String name) {
         System.out.println(name + "의 배팅 금액은?");
         return sc.nextInt();
+    }
+
+    public static Character inputChoiceGetCard(Player player) {
+        System.out.println(player.getName() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+        return sc.next().charAt(0);
     }
 }
