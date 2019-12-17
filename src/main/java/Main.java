0bfee7152c112808a6978aeae6edcc5b25f19bfa
barrett24 <barrett2440@gmail.com>
2019-12-17 19:27:14 +0900

    메인 클래스 구현

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import computer.Controller;
+
+public class Main {
+    public static void main(String args[]) {
+        Controller controller = new Controller();
+        controller.startGame();
+    }
+}
