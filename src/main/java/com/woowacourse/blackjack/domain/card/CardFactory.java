6cfde59b10c05b19929b244df6e32681098d5096
KSKIM <kwangsoonkim@outlook.com>
2019-12-17 14:39:34 +0900

    chore: CardFactory 경고 주석 추가
    create 메서드가 반환하는 값이 immutable함을 알리기 위함

--- src/main/java/com/woowacourse/blackjack/domain/card/CardFactory.java
+++ src/main/java/com/woowacourse/blackjack/domain/card/CardFactory.java
@@ -1,4 +1,4 @@
-package domain.card;
+package com.woowacourse.blackjack.domain.card;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -6,8 +6,13 @@ import java.util.List;
 
 /**
  * 트럼프 카드 전체 생성을 담당하는 객체
+ *
+ * @version 1.0.0
+ * @author KSKIM
+ * @since 2019-12-15
  */
 public class CardFactory {
+    // 다른 곳에서 리스트에 수정을 가하는 메서드 호출 불가. (읽기 전용)
     public static List<Card> create() {
         List<Card> cards = new ArrayList<>();
         Symbol[] symbols = Symbol.values();
