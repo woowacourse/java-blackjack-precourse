fb516284435641f387699d2d961fe5441cc340c5
jenny <jennylee4517@gmail.com>
2019-12-17 18:26:36 +0900

    getAceCnt로 기능 분리

--- src/main/java/com/jenny/blackjack/Result.java
+++ src/main/java/com/jenny/blackjack/Result.java
@@ -19,22 +19,39 @@ public class Result {
         }
     }
 
+    /*
+     * 카드 점수의 합을 계산하는 메서드
+     * 에이스의 경우, 1또는 11로 사용 가능하므로
+     * 상황에따라 10을 더하는것이 유리하면 그렇게 한다
+     * 블랙잭 조건이 되는 경우는 21이 아닌 22로 계산하여
+     * 금액 계산시 21과 구분짓도록 한다
+     */
     public int calcResultValue(User user){
         List<Card> cards = user.getCardsList();
-        int aceCnt = 0;
+        int aceCnt = getAceCnt(cards);
         int sum = user.getSumOfCards();
 
-        for(Card c : cards){
-            if(c.getSymbol().equals("ACE")){
-                aceCnt++;
-            }
-        }
         for(int a = 0; a < aceCnt; a++){
             if(sum + 10 <= 21){
                 sum += 10;
+            }else{
+                break;
             }
         }
+        if(sum == 21 && cards.size() == 2){
+            sum++;
+        }
         return sum;
+    }
+
+    public int getAceCnt(List<Card> cards){
+        int cnt = 0;
+        for(Card c : cards){
+            if(c.getSymbol().equals("ACE")){
+                cnt++;
+            }
+        }
+        return cnt;
     }
 
     public boolean isBurst(User user){
