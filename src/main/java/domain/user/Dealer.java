62402dbb43e2520616f05ffbf6fbebf8b7616e72
HWooKim97 <rgd5454@naver.com>
2019-12-17 17:27:50 +0900

    Add method showMoney()
    - Overload super.showMoney()
      Print dealer's money

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -1,5 +1,7 @@
 package domain.user;
 
+import java.util.List;
+
 /**
  * 게임 딜러를 의미하는 객체
  */
@@ -9,4 +11,15 @@ public class Dealer extends Player {
     }
 
     // TODO 추가 기능 구현
+    public void showMoney(List<Player> user) {
+        int money = 0;
+
+        user.remove(0);
+        for (Player player : user) {
+            money += player.getBettingMoney();
+        }
+
+        System.out.println(super.getName() + " : " + money);
+    }
+
 }
