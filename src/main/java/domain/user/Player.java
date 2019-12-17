2b6da94d24bd590a53a15591fe21707fd9fdbc24
pandahun <gnsgnsek@gmail.com>
2019-12-17 13:13:44 +0900

    feature: modify Player.java
    Player 객체 추가 기능 구현 및 수정
    -bettingMoney 객체를 변수로 지정
    -ace의 갯수 파악 후 점수 반환 기능 구현
    -Card 정보 String으로 반환하게 구현

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -1,27 +1,118 @@
 package domain.user;
 
+import domain.BettingMoney;
+import domain.Name;
 import domain.card.Card;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-/**
- * 게임 참여자를 의미하는 객체
- */
+import static io.InputView.DELIMITER;
+
 public class Player {
-    private final String name;
-    private final double bettingMoney;
+
+    public static final int BLACK_JACK = 21;
+    private static final int ACE = 1;
+    private static final int TEN = 10;
+    private static final double ANYTHING = 0;
+
+    private final Name name;
+    private BettingMoney bettingMoney;
     private final List<Card> cards = new ArrayList<>();
 
-    public Player(String name, double bettingMoney) {
+    public Player( Name name ) {
         this.name = name;
-        this.bettingMoney = bettingMoney;
     }
 
-    public void addCard(Card card) {
+    public void addCard( Card card ) {
         cards.add(card);
     }
 
-    // TODO 추가 기능 구현
+    public void setBettingMoney( double bettingMoney ) {
+        this.bettingMoney = new BettingMoney(bettingMoney);
+    }
 
+    public String getName() {
+        return name.getName();
+    }
+
+    public BettingMoney getBettingMoney() {
+        return bettingMoney;
+    }
+
+    public int getScore() {
+        int score = cards.stream()
+                .map(Card::getScore)
+                .reduce(Integer::sum)
+                .orElse(0);
+        if (score <= 11 && hasAce()) {
+            score = applyAce(score);
+        }
+        return score;
+    }
+
+    public String getCardToString() {
+        return cards.stream()
+                .map(Card::toString)
+                .collect(Collectors.joining(DELIMITER));
+    }
+
+    public boolean isBlackJack() {
+        return getScore() == BLACK_JACK;
+    }
+
+    private boolean hasAce() {
+        int aceCount = (int) cards.stream()
+                .filter(card -> card.getScore() == ACE)
+                .count();
+
+        return aceCount >= ACE;
+    }
+
+    private int applyAce( int score ) {
+        int aceCount = (int) cards.stream()
+                .filter(card -> card.getScore() == ACE)
+                .count();
+        if (aceCount == 3) {
+            return BLACK_JACK;
+        }
+        return score + TEN;
+    }
+
+    public boolean isBust() {
+        int score = cards.stream()
+                .map(Card::getScore)
+                .reduce(Integer::sum)
+                .orElse(0);
+        return score > BLACK_JACK;
+    }
+
+    public double getRewardMoney( boolean isDealerBlackJack, boolean isWin ) {
+        if (isDealerBlackJack) {
+            return getBlackJackMoney();
+        }
+        if (isBlackJack()) {
+            return bettingMoney.blackJack();
+        }
+        if (isWin) {
+            return bettingMoney.earn();
+        }
+        return bettingMoney.loseGame();
+    }
+
+    private double getBlackJackMoney() {
+        if (isBlackJack()) {
+            return ANYTHING;
+        }
+        return bettingMoney.loseGame();
+    }
+
+    public boolean isDealer() {
+        return false;
+    }
+
+    public boolean isPlayer() {
+        return true;
+    }
 }
