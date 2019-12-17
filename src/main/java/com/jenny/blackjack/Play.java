2c8de5ef4d22c6234c183f00381c953f73d86705
jenny <jennylee4517@gmail.com>
2019-12-17 15:58:59 +0900

    distributeInitialCards 구현

--- src/main/java/com/jenny/blackjack/Play.java
+++ src/main/java/com/jenny/blackjack/Play.java
@@ -1,5 +1,27 @@
 package com.jenny.blackjack;
 
-public class Play {
+import domain.card.Card;
+import domain.card.CardFactory;
+import domain.user.Dealer;
+import domain.user.Player;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Play {
+    private int TOTAL_CARDS = 13;
+    private int INITIAL_CARDS = 2;
+
+    private List<Card> cards;
+    private Set<Integer> alreadyUsedCards = new HashSet<>();
+
+    public void distributeInitialCards(Dealer dealer, List<Player> players){
+        cards = CardFactory.create();
+
+        getCardForDealer(INITIAL_CARDS, dealer);
+        for(Player p : player){
+            getCardForPlayer(INITIAL_CARDS, p);
+        }
+    }
 }
