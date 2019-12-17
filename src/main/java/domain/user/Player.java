92640dae42606829d52d1e8c04638d4b334ae59d
jenny <jennylee4517@gmail.com>
2019-12-17 19:34:13 +0900

    Player가 Dealer를 상속받도록 변경하여 중복코드 제거

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * 게임 참여자를 의미하는 객체
  */
-public class Player implements User {
+public class Player extends Dealer {
     private final String name;
     private final double bettingMoney;
     private final List<Card> cards = new ArrayList<>();
@@ -19,39 +19,10 @@ public class Player implements User {
         this.bettingMoney = bettingMoney;
     }
 
-    public void addCard(Card card) {
-        cards.add(card);
-    }
-
     // TODO 추가 기능 구현
 
     public String getName(){
         return name;
-    }
-
-    public String getCards(){
-        int size = cards.size();
-        StringBuilder msg = new StringBuilder();
-
-        for(int idx = 0; idx < size; idx++){
-            Card card = cards.get(idx);
-            msg.append(card.getSymbol() + " " + card.getType() + ",");
-        }
-        return msg.toString();
-    }
-
-    public List<Card> getCardsList(){
-        return cards;
-    }
-
-    public int getSumOfCards(){
-        int sum = 0;
-        int size = cards.size();
-
-        for(Card card : cards){
-            sum += card.getSymbol().getScore();
-        }
-        return sum;
     }
 
     public void setResultMoney(double money){
