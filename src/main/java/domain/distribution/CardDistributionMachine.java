ec189472462a7da84ac93e937d080fe7b157aa18
JunSeong <ebseud6135@naver.com>
2019-12-17 23:08:34 +0900

    카드 분배 기계에서 카드를 나눠 주는 기능 수정

--- src/main/java/domain/distribution/CardDistributionMachine.java
+++ src/main/java/domain/distribution/CardDistributionMachine.java
@@ -1,19 +1,33 @@
 package domain.distribution;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import domain.card.Card;
 import domain.card.Cards;
 
+/**
+ * 카드를 나눠주는 기계 역할을 담당
+ */
 public class CardDistributionMachine {
     private final Cards cards;
     private final DistributionSequence distributionSequence;
 
-    public CardDistributionMachine(Cards cards, DistributionSequence distributionSequence ) {
-        this.cards = cards;
-        this.distributionSequence = distributionSequence;
+    public CardDistributionMachine() {
+        this.cards = new Cards();
+        this.distributionSequence = new DistributionSequence();
     }
 
-    public Card distributeCard() {
+    public Card getCard() {
         return cards.getCardByIndex(distributionSequence.get());
     }
 
+    public List<Card> getCardsBySize(int size) {
+        List<Card> cardStore = new ArrayList<>();
+        for (int count = 0; count < size; count++) {
+            cardStore.add(cards.getCardByIndex(distributionSequence.get()));
+        }
+        return cardStore;
+
+    }
 }
