7502ec4ec0b6d4bc4c4b3a3f7354e7e06cd8c81e
JunSeong <ebseud6135@naver.com>
2019-12-17 23:43:02 +0900

    상황별 플레이어들을 찾아내서 리턴하는 기능들 추가

--- src/main/java/domain/user/Players.java
+++ src/main/java/domain/user/Players.java
@@ -1,7 +1,9 @@
 package domain.user;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import domain.card.Card;
 import exception.Validator;
 
 public class Players {
@@ -11,5 +13,44 @@ public class Players {
         Validator.validatePlayersSize(players.size());
         Validator.validateDuplicatedName(players);
         this.players = players;
+    }
+
+    public Player getPlayer(int index) {
+        return players.get(index);
+    }
+
+    public int size() {
+        return players.size();
+    }
+
+    public void addCardAllPlayer(List<Card> cards) {
+        Validator.validateCardSizeAndPlayerSize(cards.size(), this.size());
+        for (int index = 0; index < size(); index++) {
+            players.get(index).addCard(cards.get(index));
+        }
+    }
+
+    public List<Player> findByBust() {
+        return players.stream()
+                .filter(Player::isBust)
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> findByNotBust() {
+        return players.stream()
+                .filter(player -> !player.isBust())
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> findByBlackjack() {
+        return players.stream()
+                .filter(Player::isBlackjack)
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> findByNotBlackjack() {
+        return players.stream()
+                .filter(player -> !player.isBlackjack())
+                .collect(Collectors.toList());
     }
 }
