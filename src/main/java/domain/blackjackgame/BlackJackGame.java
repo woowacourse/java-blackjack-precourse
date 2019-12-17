3d38a9d820199187d33a95ea002fb15a0ad33c9b
sssssseungmi <tmdal9516@naver.com>
2019-12-17 13:02:04 +0900

    Add inputPlayer method and inputBettingMoney method in BlackJackGame.java

--- src/main/java/domain/blackjackgame/BlackJackGame.java
+++ src/main/java/domain/blackjackgame/BlackJackGame.java
@@ -15,4 +15,27 @@ public class BlackJackGame {
 		players = new ArrayList<>();
 	}
 
+	private boolean inputPlayer() {
+		String playerNames = scanner.nextLine();
+		String[] splitedPlayerNames = playerNames.split(",");
+		if (splitedPlayerNames.length == 0) {
+			return false;
+		}
+		for (String playerName : splitedPlayerNames) {
+			inputBettingMoney(playerName);
+		}
+	}
+
+	private void inputBettingMoney(String playerName) {
+		double bettingMoney = 0;
+		try {
+			bettingMoney = Double.parseDouble(scanner.nextLine());
+			Player player = new Player(playerName, bettingMoney);
+			players.add(player);
+		} catch (Exception e) {
+			System.out.println("잘못된 입력입니다. 다시 입력해 주세요");
+			inputBettingMoney(playerName);
+		}
+	}
+
 }
