93fd10c2178ffcde1e25a9b69f8655b038cec725
junk-sound <wom2277@naver.com>
2019-12-17 13:28:49 +0900

    비기능: '유저의 카드 숫자 총을 계산하는 기능'에 대한 단위 테스트 수정.

--- src/test/java/domain/user/UserTest.java
+++ src/test/java/domain/user/UserTest.java
@@ -1,5 +1,147 @@
+package domain.user;
+
+import domain.card.Card;
+import domain.card.Symbol;
+import domain.card.Type;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
+    private User user;
+
+
+    @BeforeEach
+    void init() {
+        user = new User() {};
+    }
+
+    @Test
+    void calculateScoreWithoutAce() {
+        //given
+        List<Card> cardsWithoutAce = Arrays.asList(
+                new Card(Symbol.EIGHT, Type.SPADE),
+                new Card(Symbol.KING, Type.CLUB)
+        );
+
+        for (Card card : cardsWithoutAce) {
+            user.addCard(card);
+        }
+
+        //when & then
+        assertEquals(8 + 10, user.calculateScore());
+    }
+
+    @Test
+    void calculateScoreWithAceWhenNotExceedsBlackjack() {
+        //given
+        List<Card> cardsWithAce = Arrays.asList(
+                new Card(Symbol.EIGHT, Type.SPADE),
+                new Card(Symbol.ACE, Type.CLUB)
+        );
+
+        for (Card card : cardsWithAce) {
+            user.addCard(card);
+        }
+
+        //when & then
+        assertEquals(8 + 11, user.calculateScore());
+    }
+
+    @Test
+    void calculateScoreWithAceWhenExceedsBlackjack() {
+        //given
+        List<Card> cardsWithAce = Arrays.asList(
+                new Card(Symbol.EIGHT, Type.SPADE),
+                new Card(Symbol.KING, Type.SPADE),
+                new Card(Symbol.ACE, Type.CLUB)
+        );
+
+        for (Card card : cardsWithAce) {
+            user.addCard(card);
+        }
+
+        //when & then
+        assertEquals(8 + 10 + 1, user.calculateScore());
+    }
+
+    @Test
+    void isBustWhenBust() {
+        List<Card> cardsWhichBust = Arrays.asList(
+                new Card(Symbol.EIGHT, Type.SPADE),
+                new Card(Symbol.KING, Type.SPADE),
+                new Card(Symbol.KING, Type.CLUB)
+        );
+
+        for (Card card : cardsWhichBust) {
+            user.addCard(card);
+        }
+
+        assertTrue(user.isBust());
+    }
+
+    @Test
+    void isBustWhenNotBust() {
+        //given
+        List<Card> cardsWhichNotBust = Arrays.asList(
+                new Card(Symbol.EIGHT, Type.SPADE),
+                new Card(Symbol.KING, Type.CLUB)
+        );
+
+        for (Card card : cardsWhichNotBust) {
+            user.addCard(card);
+        }
+
+        assertFalse(user.isBust());
+    }
+
+
+    @Test
+    void isBlackjackWhenBlackjack() {
+        List<Card> blackjack = Arrays.asList(
+                new Card(Symbol.ACE, Type.SPADE),
+                new Card(Symbol.KING, Type.CLUB)
+        );
+
+        for (Card card : blackjack) {
+            user.addCard(card);
+        }
+
+        assertTrue(user.isBlackjack());
+    }
+
+    @Test
+    void isBlackjackWhenNotBlackjackScore() {
+        List<Card> notBlackjack = Arrays.asList(
+                new Card(Symbol.EIGHT, Type.SPADE),
+                new Card(Symbol.KING, Type.CLUB)
+        );
+
+        for (Card card : notBlackjack) {
+            user.addCard(card);
+        }
+
+        assertFalse(user.isBlackjack());
+    }
+
+    @Test
+    void isBlackjackWhenNotBlackjackSize() {
+        List<Card> notBlackjack = Arrays.asList(
+                new Card(Symbol.EIGHT, Type.SPADE),
+                new Card(Symbol.KING, Type.CLUB),
+                new Card(Symbol.THREE, Type.SPADE)
+        );
+
+        for (Card card : notBlackjack) {
+            user.addCard(card);
+        }
+
+        assertEquals(21, user.calculateScore());
+        assertFalse(user.isBlackjack());
+    }
 }
