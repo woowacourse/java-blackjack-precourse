eff75011e753e242b16f37efba46ed6aec076867
YerinCho <forjustice9839@gmail.com>
2019-12-17 21:10:31 +0900

    메인 클래스 추가

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,14 @@
+package domain;
+
+import domain.BlackJackGame;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	BlackJackGame blackjackgame = new BlackJackGame();
+	blackjackgame.run();
+    }
+
+}
+
