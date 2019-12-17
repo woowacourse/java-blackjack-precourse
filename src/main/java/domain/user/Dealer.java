fdc0adfd7942e0308e00c9735c9516487ac99b4b
JunSeong <ebseud6135@naver.com>
2019-12-17 23:41:58 +0900

    16을 초과하는지 검사하는 기능 추가

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -8,14 +8,18 @@ import java.util.List;
 /**
  * 게임 딜러를 의미하는 객체
  */
-public class Dealer {
-    private final List<Card> cards = new ArrayList<>();
+public class Dealer extends User{
+    private static final String NAME ="딜러";
+    private static final int HIT_BOUNDARY_VALUE = 16;
 
     public Dealer() {}
 
-    public void addCard(Card card) {
-        cards.add(card);
+    public boolean isHaveToHit() {
+        return getTotalScore() <= HIT_BOUNDARY_VALUE;
     }
 
-    // TODO 추가 기능 구현
+    @Override
+    public String getName() {
+        return NAME;
+    }
 }
