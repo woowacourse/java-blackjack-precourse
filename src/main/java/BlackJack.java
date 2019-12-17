bda323ea33dc3c24e7711a1115e5ae241cd14197
ParkSanggi <roqkfwkehlwk@naver.com>
2019-12-17 20:55:13 +0900

    add playGame method

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -4,5 +4,6 @@ import domain.manager.PlayManager;
 public class BlackJack {
     public static void main(String[] args) {
         PlayManager playManager = new PlayManager(InputManager.getPlayerNameList(), InputManager.getBettingMoneyList());
+        playManager.playGame();
     }
 }
