b2dea6d0d3c9484e3b9c8275b7ac2e35a53367d8
jaeju.jang <jjj0611@gmail.com>
2019-12-17 14:52:10 +0900

    [ADD] 예외 출력을 위한 변수를 InvalidException클래스에 추가했습니다.

--- src/main/java/exception/InvalidException.java
+++ src/main/java/exception/InvalidException.java
@@ -2,7 +2,9 @@ package exception;
 
 public class InvalidException extends IllegalArgumentException {
 
-    public static String NOT_A_NUMBER_BATTING_EXCEPTION = "배팅금액은 숫자만 입력가능합니다.";
+    public static String NOT_A_NUMBER_BETTING_EXCEPTION = "배팅금액은 숫자만 입력가능합니다.";
+    public static String NEGATIVE_BETTING_EXCEPTION = "배팅금액은 0이하일 수 없습니다.";
+    public static String PLAYER_NAME_EMPTY_EXCEPTION = "플레이어 이름은 비어있을 수 없습니다.";
 
     public InvalidException() {
     }
