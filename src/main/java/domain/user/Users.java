ad718120d9b67cf47609c5e59d91a8b145e908ab
Tak Hyungmin <milkho11@naver.com>
2019-12-17 18:02:10 +0900

    리팩토링: Users
    Users에서 중복되는 decideOutcome 부분을 수정하고
    접근지정자를 알맞게 고쳤다.
    그외 자바 컨벤션을 위반하는 코드를 수정하였다.

--- src/main/java/domain/user/Users.java
+++ src/main/java/domain/user/Users.java
@@ -1,14 +1,12 @@
 package domain.user;
 
-import domain.BenefitMultipleType;
-import domain.outcome.Outcome;
 import domain.outcome.Outcomes;
 import domain.view.InputUtil;
 import domain.view.OutputUtil;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -16,130 +14,101 @@ public class Users {
     private static List<User> users;
     private static final int EMPTY = 0;
 
-    public Users(List<User> users) {
+    private Users(List<User> users) {
         valid(users);
-        this.users = users;
+        Users.users = users;
     }
 
     private void valid(List<User> users) {
-        if(users.size() == EMPTY){
+        if (users.size() != EMPTY) {
             return;
         }
         throw new IllegalArgumentException("이름을 채워주세요!");
     }
 
-    public List<User> getUsers() {
-        return Collections.unmodifiableList(users);
-    }
-
     public static Users initUsers(String[] names) {
         List<User> userList = new ArrayList<>();
-        userList.add(new Dealer());
+        addDealer(userList);
         for (String name : names) {
-            // TODO : OutputUtil과의 의존성이 생기는데... 더 나은 방법이 없나 고민해보기
-            OutputUtil.printBettingMoneyDemand(name);
+            printBettingMoneyDemand(name);
             userList.add(new Player(name, InputUtil.inputBettingMoney()));
         }
         return new Users(userList);
     }
 
+    private static void addDealer(List<User> userList) {
+        userList.add(new Dealer());
+    }
+
+    private static void printBettingMoneyDemand(String name) {
+        OutputUtil.printBettingMoneyDemand(name);
+    }
+
     public void receiveBeginningCard() {
-        users.forEach(user -> {
-            user.addRandomCard();
-            user.addRandomCard();
-        });
-    }
-
-    public void printFinalOutput() {
-        users.forEach(user -> {
-            user.printFinalOutput();
-        });
-    }
-
-    public void printInitUserCard() {
-        users.forEach(user -> {
-            user.printUserInfo();
-        });
+        users.forEach(User::addInitRandomCard);
     }
 
     public void startAddCardQuestion(Outcomes outcomes) {
         startUserAddCardQuestion(outcomes);
-        startDealerAddCardQuestion();
+        startDealerAddCard();
     }
 
     private void startUserAddCardQuestion(Outcomes outcomes) {
-        getPlayers()
+        getPlayer()
                 .filter(player -> !outcomes.isHavePlayer(player.getName()))
-                .forEach(player -> player.startAddCardLoop());
+                .forEach(Player::startAddCardLoop);
     }
 
-    private void startDealerAddCardQuestion() {
-        users.stream()
-                .filter(user -> user.isDealer())
-                .map(user -> (Dealer) user)
-                .forEach(dealer ->
-                        dealer.hit());
+    private void startDealerAddCard() {
+        getDealer().hit();
     }
 
     public void decideOutcome(int dealderScore, Outcomes outcomes) {
-        decideExcessLoswer(outcomes);
+        decideExcessOutcome(true, false, outcomes);
         if (dealderScore > 21) {
-            decideAllAlivePlayerWinner(outcomes);
+            decideExcessOutcome(false, true, outcomes);
             return;
         }
-        decideWinner(dealderScore, outcomes);
-        decideLoswer(dealderScore, outcomes);
+        decideWinOrLose(true, dealderScore, outcomes);
+        decideWinOrLose(false, dealderScore, outcomes);
     }
 
-    public void addOutcomes(Player player, Outcomes outcomes, Double benefit) {
+    private void decideExcessOutcome(
+            boolean checkExcessFlag,
+            boolean winFlag,
+            Outcomes outcomes
+    ) {
+        getPlayer()
+                .filter(player -> !outcomes.isHavePlayer(player.getName()))
+                .filter(player -> player.checkExcess() == checkExcessFlag)
+                .forEach(player -> {
+                    addOutcome(player, outcomes,
+                            player.calcureateBenefit(winFlag));
+                });
+    }
+
+    private void decideWinOrLose(
+            boolean winFlag,
+            int dealerScore,
+            Outcomes outcomes
+    ) {
+        getPlayer()
+                .filter(player -> !outcomes.isHavePlayer(player.getName()))
+                .filter(player -> player.isWinBy(dealerScore) == winFlag)
+                .filter(player -> !player.checkExcess())
+                .forEach(player -> {
+                    addOutcome(player, outcomes,
+                            player.calcureateBenefit(winFlag));
+                });
+    }
+
+    private void addOutcome(Player player, Outcomes outcomes, Double benefit) {
         outcomes.addOutcomes(
                 player.getName(),
                 benefit, player.getCards());
     }
 
-    private void decideAllAlivePlayerWinner(Outcomes outcomes) {
-        getPlayers()
-                .filter(player -> !player.checkExcess())
-                .filter(player -> !outcomes.isHavePlayer(player.getName()))
-                .forEach(player -> {
-                    addOutcomes(player, outcomes,
-                            player.getBenefit(BenefitMultipleType.USER_WIN));
-                });
-    }
-
-    private void decideWinner(int dealerScore, Outcomes outcomes) {
-        getPlayers()
-                .filter(player -> player.isWinBy(dealerScore))
-                .filter(player -> !player.checkExcess())
-                .filter(player -> !outcomes.isHavePlayer(player.getName()))
-                .forEach(player -> {
-                    addOutcomes(player, outcomes,
-                            player.getBenefit(BenefitMultipleType.USER_WIN));
-                });
-    }
-
-    private void decideExcessLoswer(Outcomes outcomes) {
-        getPlayers()
-                .filter(player -> player.checkExcess())
-                .filter(player -> !outcomes.isHavePlayer(player.getName()))
-                .forEach(player -> {
-                    addOutcomes(player, outcomes,
-                            player.getBenefit(BenefitMultipleType.USER_LOSE));
-                });
-    }
-
-    private void decideLoswer(int dealerScore, Outcomes outcomes) {
-        getPlayers()
-                .filter(player -> !player.isWinBy(dealerScore))
-                .filter(player -> !player.checkExcess())
-                .filter(player -> !outcomes.isHavePlayer(player.getName()))
-                .forEach(player -> {
-                    addOutcomes(player, outcomes,
-                            player.getBenefit(BenefitMultipleType.USER_LOSE));
-                });
-    }
-
-    public Stream<Player> getPlayers() {
+    public Stream<Player> getPlayer() {
         return users.stream()
                 .filter(User::isPlayer)
                 .map(player -> (Player) player);
@@ -147,20 +116,33 @@ public class Users {
 
     public String getUsersName() {
         return users.stream()
-                .filter(user -> user instanceof Player)
+                .filter(User::isPlayer)
                 .map(user -> ((Player) user).getName())
                 .collect(Collectors.joining(","));
     }
 
     public Dealer getDealer() {
-        return users.stream()
-                .filter(user -> user instanceof Dealer)
+        Optional<Dealer> dealer = users.stream()
+                .filter(User::isDealer)
                 .map(user -> (Dealer) user)
-                .findFirst()
-                .get();
+                .findFirst();
+        if (dealer.isPresent()) {
+            return dealer.get();
+        }
+        throw new IllegalStateException("딜러 없는 경우가 발생하였습니다.");
     }
 
     public int getInitBlackJackPlayer() {
-        return (int) getPlayers().count();
+        return (int) getPlayer()
+                .filter(User::isBlackJack)
+                .count();
+    }
+
+    public void printBeginningUserCard() {
+        users.forEach(User::printUserInfo);
+    }
+
+    public void printFinalOutput() {
+        users.forEach(User::printFinalOutput);
     }
 }
