969abb3c5a7044b075949e7d8175e73e82ad41d1
jiwon_lala <s26788761@naver.com>
2019-12-17 16:12:43 +0900

    fix : CardDeck 클래스의 getRandomCard() 함수에서 java.lang.Unsurpported OpertaionException 에러 나는 버그 수정
    기존의 코드는 cards에 있는 randomCard를 remove() 함수를 사용하여 제거하는 코드여서 Collections.unmodifiableList(cards)인 cards에 에러가 남.
    
    그래서 필드에 usedCards라는 ArrayList를 새로 만들고 사용한(플레이어에게 제공된) Card를 add() 하여 사용된 카드와 사용되지 않은 카드를 관리
    
    private boolean isUsed(Card card);
    //입력받은 Card 인스턴스가 이미 사용된 usedCards에 포함되어 있는지 체크하여 포함되어 있으면 true 리턴, 아니면 false 리턴

--- src/main/java/domain/card/CardDeck.java
+++ src/main/java/domain/card/CardDeck.java
@@ -11,19 +11,34 @@
 
 package domain.card;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CardDeck {
     private List<Card> cards;
+    private List<Card> usedCards;
 
     public CardDeck() {
         cards = CardFactory.create();
+        usedCards = new ArrayList<>();
     }
 
     public Card getRandomCard() {
-        Card randomCard = cards.get(getRandomIndex());
-        cards.remove(randomCard);
+        Card randomCard;
+
+        do {
+            randomCard = cards.get(getRandomIndex());
+        } while (!isUsed(randomCard));
+
+        usedCards.add(randomCard);
         return randomCard;
+    }
+
+    private boolean isUsed(Card card) {
+        if (usedCards.contains(card)) {
+            return true;
+        }
+        return false;
     }
 
     private int getRandomIndex() {
