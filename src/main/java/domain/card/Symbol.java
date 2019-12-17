b8dc8513d23fe23f44a609395eedec03a6693cf0
hayoung298 <dudgk298@gmail.com>
2019-12-17 15:27:19 +0900

    symbol, type 값 toString으로 출력

--- src/main/java/domain/card/Symbol.java
+++ src/main/java/domain/card/Symbol.java
@@ -1,20 +1,60 @@
 package domain.card;
 
 public enum Symbol {
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
-
+    ACE(1){
+    	@Override
+    	public String toString() {return "11";}
+    },
+    TWO(2){
+    	@Override
+    	public String toString() {return "2";}
+    },
+    THREE(3){
+    	@Override
+    	public String toString() {return "3";}
+    },
+    FOUR(4){
+    	@Override
+    	public String toString() {return "4";}
+    },
+    FIVE(5){
+    	@Override
+    	public String toString() {return "5";}
+    },
+    SIX(6){
+    	@Override
+    	public String toString() {return "6";}
+    },
+    SEVEN(7){
+    	@Override
+    	public String toString() {return "7";}
+    },
+    EIGHT(8){
+    	@Override
+    	public String toString() {return "8";}
+    },
+    NINE(9){
+    	@Override
+    	public String toString() {return "9";}
+    },
+    TEN(10){
+    	@Override
+    	public String toString() {return "10";}
+    },
+    JACK(10){
+    	@Override
+    	public String toString() {return "J";}
+    },
+    QUEEN(10){
+    	@Override
+    	public String toString() {return "Q";}
+    },
+    KING(10){
+    	@Override
+    	public String toString() {return "K";}
+    };
+	
+	
     private int score;
 
     Symbol(int score) {
--- src/main/java/domain/card/Type.java
+++ src/main/java/domain/card/Type.java
@@ -1,8 +1,20 @@
 package domain.card;
 
 public enum Type {
-    SPADE,
-    DIAMOND,
-    HEART,
-    CLUB
+    SPADE{
+    	@Override
+    	public String toString() {return "스페이드";}
+    },
+    DIAMOND{
+    	@Override
+    	public String toString() {return "다이아몬드";}
+    },
+    HEART{
+    	@Override
+    	public String toString() {return "하트";}
+    },
+    CLUB{
+    	@Override
+    	public String toString() {return "클로버";}
+    }
 }
