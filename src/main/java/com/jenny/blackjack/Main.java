95deb3fe722c49a2ec3ecc872d8074c3ed080688
jenny <jennylee4517@gmail.com>
2019-12-17 20:13:12 +0900

    main 메서드 주석 추가

--- src/main/java/com/jenny/blackjack/Main.java
+++ src/main/java/com/jenny/blackjack/Main.java
@@ -7,8 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    /*
+     * 블랙잭 게임 실행 메서드
+     * 1. init : 사용자를 입력받고 배팅금액을 설정하는 단계
+     * 2. play : Hit or Stay를 결정하는 단계
+     * 3. result : 결과를 계산하는 단계
+     */
     public static void main(String[] args) {
-
         InitialSetting init = new InitialSetting();
         Play play = new Play();
         Result result = new Result();
