4f339f84caa70e0bcea1060ef2f907522a341032
JunSeong <ebseud6135@naver.com>
2019-12-17 23:23:00 +0900

    Player 객체 생성 시 예외 상황 검사 기능 추가

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -1,27 +1,24 @@
 package domain.user;
 
-import domain.card.Card;
-
-import java.util.ArrayList;
-import java.util.List;
+import exception.Validator;
 
 /**
  * 게임 참여자를 의미하는 객체
  */
-public class Player {
+public class Player extends User{
+    private static final String YES = "y";
+
     private final String name;
     private final double bettingMoney;
-    private final List<Card> cards = new ArrayList<>();
 
     public Player(String name, double bettingMoney) {
+        checkCorrectPlayer(name, bettingMoney);
         this.name = name;
         this.bettingMoney = bettingMoney;
     }
 
-    public void addCard(Card card) {
-        cards.add(card);
+    private void checkCorrectPlayer(String name, double bettingMoney) {
+        Validator.checkPlayerName(name);
+        Validator.validateBettingMoney(bettingMoney);
     }
-
-    // TODO 추가 기능 구현
-
 }
