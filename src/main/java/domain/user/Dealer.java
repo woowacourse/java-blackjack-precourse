56a43dba011c1c077ef3ab5bb7326a9e89f17f38
joi0104 <joi0104@naver.com>
2019-12-17 18:50:10 +0900

    feat : 딜러의 카드팩이 모두 소진시, 새로운 카드팩 생성하기

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -11,30 +11,35 @@ import java.util.Random;
  * 게임 딜러를 의미하는 객체
  */
 
-public class Dealer extends GameParticipant{
+public class Dealer extends GameParticipant {
     private final static int NUMBER_OF_CARD_GIVEN_FIRST = 2;
-    private final List<Card> cardpack = new CardFactory().create();
+    private List<Card> cardpack = new CardFactory().create();
 
     public Dealer() {
         super("dealer");
     }
 
-    public void giveInitCard(GameParticipant participant){
-        for(int i=0; i < NUMBER_OF_CARD_GIVEN_FIRST; i++){
+    public void giveInitCard(GameParticipant participant) {
+        for (int i = 0; i < NUMBER_OF_CARD_GIVEN_FIRST; i++) {
             giveCard(participant);
         }
     }
 
-   private void giveCard(GameParticipant participant){
+    private void giveCard(GameParticipant participant) {
         Card pickedCard = pickOneOfCardInCardpack();
         participant.addCard(pickedCard);
-   }
+    }
 
-   private Card pickOneOfCardInCardpack(){
-       Random random = new Random();
-       Card pickedCard = cardpack.get(random.nextInt(cardpack.size()));
-       cardpack.remove(pickedCard);
-       return pickedCard;
-   }
+    private Card pickOneOfCardInCardpack() {
+        Random random = new Random();
+        Card pickedCard = cardpack.get(random.nextInt(cardpack.size()));
+        cardpack.remove(pickedCard);
+
+        if (cardpack.size() == 0) {
+            cardpack = new CardFactory().create();
+        }
+
+        return pickedCard;
+    }
 
 }
