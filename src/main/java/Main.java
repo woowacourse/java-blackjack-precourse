6c302bfa8e3e3e6d2cf582cf0f236e500e652bc3
jiwon_lala <s26788761@naver.com>
2019-12-17 15:31:35 +0900

    feat : Main 함수 구현

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Main.java
+ *
+ * version 1.0
+ *
+ * 20191217
+ *
+ * copyright by jiwonSong(s26788761@naver.com)
+ *
+ */
+
+import domain.game.GameManager;
+
+public class Main {
+    public static void main(String[] args) {
+        GameManager gameManager = new GameManager();
+        gameManager.start();
+    }
+}
+
