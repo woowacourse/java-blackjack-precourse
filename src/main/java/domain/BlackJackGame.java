74c2e58d99434ef1b35231c31a052abbd40c1454
YerinCho <forjustice9839@gmail.com>
2019-12-17 17:11:50 +0900

    처음 두장의 카드를 받는경우와 이후 한 장만 받는 경우로 메소드  분리

--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -1,15 +1,12 @@
 package domain;
 
-import domain.io.Message;
-import domain.io.MoneyInput;
-import domain.io.NameInput;
-
+import domain.Score;
+import domain.Money;
+import domain.io.*;
 import domain.user.Player;
 import domain.user.Dealer;
-
 import domain.card.Card;
 import domain.card.CardFactory;
-
 import java.util.List;
 import java.util.ArrayList;
 
@@ -17,6 +14,8 @@ public class BlackJackGame {
     Message message = new Message();
     MoneyInput moneyinput = new MoneyInput();
     NameInput nameinput = new NameInput();
+    MoreCardInput morecardinput = new MoreCardInput();
+    Score score = new Score();
     private List<Card> cardList = new ArrayList<>(CardFactory.create());
     private Player[] player;
     private Dealer dealer;
@@ -30,6 +29,35 @@ public class BlackJackGame {
 	    player[i] = new Player(name[i], moneyinput.inputMoney(name[i]));
 	}
     }
+    
+    //
+    public void getCardFirst() {
+	for(int i=0;i<2;i++) {
+	    getCard(dealer);
+	}
+	for(int i=0;i<player.length;i++) {
+	    getCard(player[i]);
+	    getCard(player[i]);
+	}
+    }
+    
+    public void getPlayerMoreCard(Player player) {
+	while(score.getSum(player.getCards())<=21&&morecardinput.getMoreCardOrNot(player.getName())){
+	    getCard(player);
+	    message.showCardStatus(player);
+	}
+    }
+    
+    public void getDealerMoreCard() {
+	if(score.isLessthanSeventeen(dealer.getCards())) {
+	    getCard(dealer);
+	    message.showDealerMoreCard();
+	    return;
+	}
+	message.showDealerNotMoreCard();
+	return;
+    }
+    //
     
     public void getCard(Dealer dealer) {
 	dealer.addCard(selectCard());
