c5fabc3a291349a58946fc53441337dc7f0675dc
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-17 21:19:27 +0900

    feat: 기본 게임 흐름 정리

--- src/main/java/domain/game/BlackJack.java
+++ src/main/java/domain/game/BlackJack.java
@@ -3,22 +3,30 @@ package domain.game;
 import java.util.List;
 
 import controller.InputController;
-import controller.OutputController;
+import domain.game.round.BlackJackRound;
+import domain.game.round.HitRound;
 
 public class BlackJack {
 	private Table table;
+	private BlackJackRound blackJackRound;
+	private HitRound hitRound;
 
 	public BlackJack() {
 		setUp();
+		blackJackRound = new BlackJackRound();
+		hitRound = new HitRound();
 	}
 
 	public void run() {
-
-
+		blackJackRound.run(table);
+		if (table.hasWinner()) {
+			return;
+		}
+		hitRound.run(table);
 	}
 
 	private void setUp() {
-		InputController inputController=InputController.getInputController();
+		InputController inputController = InputController.getInputController();
 		List<String> playerNames = inputController.getPlayerNames();
 		List<Double> bettings = inputController.getBettings(playerNames);
 		table = new Table(playerNames, bettings);
